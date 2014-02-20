5c4ef4f3f5c9f447ad75a4ce7d800be2f503796f
Lucas Bartholemy <lucas@bartholemy.com>
2014-02-20 11:27:20 +0100

    Fixed spelling.

--- protected/modules_core/activity/views/activities/ActivitySpaceCreated.php
+++ protected/modules_core/activity/views/activities/ActivitySpaceCreated.php
@@ -1,16 +1,16 @@
-<?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>                    
+<?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>
 
 <strong><?php echo $user->displayName; ?></strong>
 
 <?php if ($workspace != null && Wall::$currentType != Wall::TYPE_SPACE): ?>
-    <?php echo Yii::t('ActivityModule.base', 'created a new workspace'); ?>
+n    <?php echo Yii::t('ActivityModule.base', 'created a new space'); ?>
 
     <strong>
-        <?php echo Helpers::truncateText($workspace->name, 25); ?> - 
+        <?php echo Helpers::truncateText($workspace->name, 25); ?> -
     </strong>
 
 <?php else: ?>
-    <?php echo Yii::t('ActivityModule.base', 'created this workspace.'); ?>
+    <?php echo Yii::t('ActivityModule.base', 'created this space.'); ?>
 <?php endif; ?>
 
 <?php $this->endContent(); ?>
