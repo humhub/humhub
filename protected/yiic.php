cc58ad3c6f787f2aa411a224ba8b8cdee5298f9f
Lucas Bartholemy <lucas@bartholemy.com>
2014-02-20 18:13:14 +0100

    Added WebRoot Alias for console applications

--- protected/yiic.php
+++ protected/yiic.php
@@ -16,8 +16,9 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 require_once(dirname(__FILE__).'/vendors/yii/yii.php');
 require_once(dirname(__FILE__).'/components/HConsoleApplication.php');
 
-
 $app=Yii::createApplication('HConsoleApplication', $config);
+
+Yii::setPathOfAlias('webroot', realpath(dirname(__FILE__). '/..'));
 
 // Add Yii Commands
 $app->commandRunner->addCommands(YII_PATH.'/cli/commands');
