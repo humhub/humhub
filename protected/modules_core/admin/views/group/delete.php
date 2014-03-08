168b980a4570ffb44aeb323c388bd1dd30db17ca
Andy Strobel <andreas.strobel@googlemail.com>
2014-03-08 19:04:34 +0100

    Fixing the layout of group delete form

--- protected/modules_core/admin/views/group/delete.php
+++ protected/modules_core/admin/views/group/delete.php
@@ -12,54 +12,47 @@
  * @since 0.5
  */
 ?>
-<div id="middle">
-    <div class="panel_middle">
 
-        <div class="account_title"><?php echo Yii::t('AdminModule.base', 'Delete group: {group}', array('group' => $group->name)); ?></div>
-
-        <div class="content m20">
-
-            <p class="note"><?php echo Yii::t('AdminModule.base', 'You need to set an alternative group for existing users'); ?></p>
-
-        </div>
-
-        <div class="form">
-
-            <?php
-            $form = $this->beginWidget('CActiveForm', array(
-                'id' => 'admin-deleteGroup-form',
-                'enableAjaxValidation' => true,
-                'htmlOptions' => array('enctype' => 'multipart/form-data'),
-            ));
-            ?>
-
-            <?php echo $form->errorSummary($model); ?>
-
-            <div class="row">
-                <?php
-                $groupModels = Group::model()->findAll('id !=' . $group->id);
-                $list = CHtml::listData($groupModels, 'id', 'name');
-                ?>
-
-                <?php echo $form->labelEx($model, 'group_id'); ?><br>
-                <?php echo $form->dropDownList($model, 'group_id', $list, array('class' => 'dropdown-select')); ?>
-                <?php echo $form->error($model, 'group_id'); ?>
-            </div>
-
-            <div class="clearFloats"></div>
-
-            <div class="p20">
-                <?php echo CHtml::submitButton(Yii::t('AdminModule.base', 'Delete group'), array('class' => 'input_button')); ?>
-
-            </div>
-
-            <?php $this->endWidget(); ?>
-
-        </div><!-- form -->
+<p>
+<?php echo Yii::t('AdminModule.base', 'To delete the group <strong>"{group}"</strong> you need to set an alternative group for existing users:', array('{group}' => $group->name)); ?>
+</p>
 
 
 
 
-    </div>
+<?php
+$form = $this->beginWidget('CActiveForm', array(
+    'id' => 'admin-deleteGroup-form',
+    'enableAjaxValidation' => true,
+    'htmlOptions' => array('enctype' => 'multipart/form-data'),
+));
+?>
+
+<?php echo $form->errorSummary($model); ?>
+
+
+    <?php
+    $groupModels = Group::model()->findAll('id !=' . $group->id);
+    $list = CHtml::listData($groupModels, 'id', 'name');
+    ?>
+
+<div class="form-group">
+    <?php echo $form->dropDownList($model, 'group_id', $list, array('class' => 'form-control')); ?>
+    <?php echo $form->error($model, 'group_id'); ?>
 </div>
+
+
+
+
+<hr>
+<?php echo CHtml::submitButton(Yii::t('AdminModule.base', 'Delete group'), array('class' => 'btn btn-danger')); ?>
+
+
+
+<?php $this->endWidget(); ?>
+
+
+
+
+
 
