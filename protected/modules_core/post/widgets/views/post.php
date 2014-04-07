98b1a9f04b45ad6d56a08c9beaf2c6a2dc2a3291
Andy Strobel <andreas.strobel@googlemail.com>
2014-04-07 19:05:49 +0200

    Integration of an expand/collapse feature to cut long posts

--- protected/modules_core/post/widgets/views/post.php
+++ protected/modules_core/post/widgets/views/post.php
@@ -10,10 +10,64 @@
  * @since 0.5
  */
 ?>
-<div class="panel panel-default">
+<div class="panel panel-default post" id="post-<?php echo $post->id; ?>">
     <div class="panel-body">
         <?php $this->beginContent('application.modules_core.wall.views.wallLayout', array('object' => $post)); ?>
-        <?php print HHtml::enrichText($post->message); ?>
+        <div id="post-content-<?php echo $post->id; ?>" style="overflow: hidden; margin-bottom: 5px;">
+            <?php print HHtml::enrichText($post->message); ?>
+        </div>
+        <a class="more-link-post hidden" id="more-link-post-<?php echo $post->id; ?>" data-state="down"
+           style="margin: 20px 0 20px 0;" href="javascript:showMore(<?php echo $post->id; ?>);"><i
+                class="icon-arrow-down"></i> <?php echo Yii::t('PostModule.base', 'Read full post...'); ?></a>
         <?php $this->endContent(); ?>
     </div>
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+        // save the count of characters
+        var _words = '<?php echo strlen(HHtml::enrichText($post->message)); ?>';
+
+
+        if (_words > 1100) {
+            // show more-button
+            $('#more-link-post-<?php echo $post->id; ?>').removeClass('hidden');
+            // set limited height
+            $('#post-content-<?php echo $post->id; ?>').css('max-height', '310px');
+        }
+    });
+
+    function showMore(post_id) {
+
+        // set current state
+        var _state = $('#more-link-post-' + post_id).attr('data-state');
+
+        if (_state == "down") {
+            // show full content
+            $('#post-content-' + post_id).animate({
+                'max-height': '100%'
+            }, 800);
+
+            // set new link content
+            $('#more-link-post-' + post_id).html('<i class="icon-arrow-up"></i> <?php echo Yii::t('PostModule.base', 'Collapse'); ?>');
+
+            // update link state
+            $('#more-link-post-' + post_id).attr('data-state', 'up');
+
+        } else {
+            // set back to limited length
+            $('#post-content-' + post_id).css('max-height', '310px');
+
+            // set new link content
+            $('#more-link-post-' + post_id).html('<i class="icon-arrow-down"></i> <?php echo Yii::t('PostModule.base', 'Read full post...'); ?>');
+
+            // update link state
+            $('#more-link-post-' + post_id).attr('data-state', 'down');
+
+        }
+
+    }
+
+</script>
