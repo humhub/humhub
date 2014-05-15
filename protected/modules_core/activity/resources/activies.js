55de7fce81b1f77b6e48b708bb4c36bc738aff48
Andy Strobel <andreas.strobel@googlemail.com>
2014-05-15 22:26:13 +0200

    Prevent more ajax requests, if all activities already loaded #67

--- protected/modules_core/activity/resources/activies.js
+++ protected/modules_core/activity/resources/activies.js
@@ -18,10 +18,10 @@ $(document).ready(function () {
     // set the ID for the last loaded activity entry to 1
     var activityLastLoadedEntryId = 1;
 
+    // save if the last entries are already loaded
+    var activityLastEntryReached = false;
 
     $('#activityContents').scroll(function () {
-
-
 
         // save height of the overflow container
         var _containerHeight = $("#activityContents").height();
@@ -35,11 +35,14 @@ $(document).ready(function () {
         // load more activites if current scroll position is near scroll height
         if (_currentScrollPosition >= (_scrollHeight - _containerHeight - 1)) {
 
-            console.log('last id: ' + activityLastLoadedEntryId);
-            loadMore();
+            // checking if ajax loading is necessary or the last entries are already loaded
+            if (activityLastEntryReached == false) {
+
+                // load more activities
+                loadMore();
+            }
 
         }
-
 
     });
 
@@ -72,13 +75,16 @@ $(document).ready(function () {
 
             } else {
 
-
-
                 // add new activities
                 $('#activityContents').prepend(json.output);
 
                 // save the last activity id for the next reload
                 activityLastLoadedEntryId = json.lastEntryId;
+
+                if (json.counter < 10) {
+                    // prevent the next ajax calls, if there are no more entries
+                    activityLastEntryReached = true;
+                }
 
             }
 
