120a3a1266b10fc9ca7233943348ee575c2dd4e1
Andy Strobel <andreas.strobel@googlemail.com>
2014-05-20 12:23:44 +0200

    Added bootstrap construction check at jQuery Flatelements Plugin #63

--- js/jquery.flatelements.js
+++ js/jquery.flatelements.js
@@ -17,68 +17,115 @@
         // build main options before element iteration
         var opts = $.extend({}, $.fn.flatelements.defaults, options);
 
+        //
+        // check if an element follow the bootstrap html construction
+        //
+        function checkBootstrapStructure($obj) {
+
+            if ($obj.attr('type') == "checkbox" && $obj.parent().prop("tagName") == "LABEL" && $obj.parent().parent().attr('class') == "checkbox") {
+                return true;
+
+            } else if ($obj.attr('type') == "radio" && $obj.parent().prop("tagName") == "LABEL" && $obj.parent().parent().attr('class') == "radio") {
+                return true
+            } else {
+                return false;
+            }
+        }
+
         // iterate and reformat each matched element
         return this.each(function () {
 
             // save object in a variable
             $this = $(this);
 
-            // modify elements for checkboxes
-            if ($this.attr('type') == "checkbox") {
+            // Only modify this element, if it follow the bootstrap html construction
+            if (checkBootstrapStructure($this) == true) {
 
-                // make checkbox to a slider element
-                if ($this.attr('data-view') == 'slider') {
+                // modify elements for checkboxes
+                if ($this.attr('type') == "checkbox") {
 
-                    // check if the standard bootstrap container <div> and <label> exists
-                    if ($this.parent().parent().attr('class') == "checkbox" && $this.parent().prop("tagName") == "LABEL") {
+                    // make checkbox to a slider element
+                    if ($this.attr('data-view') == 'slider') {
 
-                        // remove existing container class
-                        $this.parent().parent().removeClass('checkbox');
+                        // check if the standard bootstrap container <div> and <label> exists
+                        if ($this.parent().parent().attr('class') == "checkbox" && $this.parent().prop("tagName") == "LABEL") {
 
-                        // add new container class
-                        $this.parent().parent().addClass('onoffswitch');
+                            // remove existing container class
+                            $this.parent().parent().removeClass('checkbox');
 
-                        // add new input class
-                        $this.addClass('onoffswitch-checkbox');
+                            // add new container class
+                            $this.parent().parent().addClass('onoffswitch');
 
-                        // save label text
-                        var _label = $.trim($this.parent().parent().text());
+                            // add new input class
+                            $this.addClass('onoffswitch-checkbox');
 
-                        // remove label text
-                        $this.parent().html($this.parent().find('input'));
+                            // save label text
+                            var _label = $.trim($this.parent().parent().text());
 
-                        // build new slider construct
-                        var _newHTML = '<label class="onoffswitch-label" for="' + $this.attr('id') + '">' +
-                            '<div class="onoffswitch-inner"></div>' +
-                            '<div class="onoffswitch-switch"></div>' +
-                            '</label>';
+                            // remove label text
+                            $this.parent().html($this.parent().find('input'));
 
-                        // add new slider construckt
-                        $this.parent().append(_newHTML);
+                            // build new slider construct
+                            var _newHTML = '<label class="onoffswitch-label" for="' + $this.attr('id') + '">' +
+                                '<div class="onoffswitch-inner"></div>' +
+                                '<div class="onoffswitch-switch"></div>' +
+                                '</label>';
 
-                        // build closing slider construct
-                        _newHTML = '<label class="onoffswitch-label" for="' + $this.attr('id') + '">' + _label + '</label>' +
-                            '<div class="onoffswitch-clear"></div>';
+                            // add new slider construckt
+                            $this.parent().append(_newHTML);
 
-                        // add closing slider contruct
-                        $this.parent().parent().after(_newHTML);
+                            // build closing slider construct
+                            _newHTML = '<label class="onoffswitch-label" for="' + $this.attr('id') + '">' + _label + '</label>' +
+                                '<div class="onoffswitch-clear"></div>';
 
-                        // remove the enclosing label tag
-                        $this.parent().replaceWith($this.parent().html());
+                            // add closing slider contruct
+                            $this.parent().parent().after(_newHTML);
 
+                            // remove the enclosing label tag
+                            $this.parent().replaceWith($this.parent().html());
+
+                        }
+
+
+                    } else {
+
+                        // check if the standard bootstrap container <div> exists
+                        if ($this.parent().parent().attr('class') == "checkbox") {
+
+                            // add new class
+                            $this.parent().parent().addClass('regular-checkbox-container');
+
+                            // add a new <div> at the end to clear floats
+                            $this.parent().parent().append('<div class="regular-checkbox-clear"></div>');
+                        }
+
+                        // check if the standard bootstrap <label> exists
+                        if ($this.parent().prop("tagName") == "LABEL") {
+
+                            // if there is no assignment
+                            if ($this.parent().attr('for') == undefined) {
+
+                                // assign label to checkbox
+                                $this.parent().attr('for', $this.attr('id'));
+
+                                // add new checkbox element
+                                $this.parent().append('<div class="regular-checkbox-box"></div>');
+                            }
+                        }
+
+                        // add new class to checkbox
+                        $this.addClass('regular-checkbox');
                     }
 
-
-                } else {
+                    // modify elements for radio buttons
+                } else if ($this.attr('type') == "radio") {
 
                     // check if the standard bootstrap container <div> exists
-                    if ($this.parent().parent().attr('class') == "checkbox") {
+                    if ($this.parent().parent().attr('class') == "radio") {
 
                         // add new class
-                        $this.parent().parent().addClass('regular-checkbox-container');
+                        $this.parent().parent().addClass('regular-radio-container');
 
-                        // add a new <div> at the end to clear floats
-                        $this.parent().parent().append('<div class="regular-checkbox-clear"></div>');
                     }
 
                     // check if the standard bootstrap <label> exists
@@ -87,49 +134,22 @@
                         // if there is no assignment
                         if ($this.parent().attr('for') == undefined) {
 
-                            // assign label to checkbox
+                            // assign label to radio element
                             $this.parent().attr('for', $this.attr('id'));
 
-                            // add new checkbox element
-                            $this.parent().append('<div class="regular-checkbox-box"></div>');
+                            // add new radio element
+                            $this.parent().append('<div class="regular-radio-button"></div>');
                         }
                     }
 
-                    // add new class to checkbox
-                    $this.addClass('regular-checkbox');
-                }
-
-                // modify elements for radio buttons
-            } else if ($this.attr('type') == "radio") {
-
-                // check if the standard bootstrap container <div> exists
-                if ($this.parent().parent().attr('class') == "radio") {
-
-                    // add new class
-                    $this.parent().parent().addClass('regular-radio-container');
+                    // add new class to radio element
+                    $this.addClass('regular-radio');
 
                 }
-
-                // check if the standard bootstrap <label> exists
-                if ($this.parent().prop("tagName") == "LABEL") {
-
-                    // if there is no assignment
-                    if ($this.parent().attr('for') == undefined) {
-
-                        // assign label to radio element
-                        $this.parent().attr('for', $this.attr('id'));
-
-                        // add new radio element
-                        $this.parent().append('<div class="regular-radio-button"></div>');
-                    }
-                }
-
-                // add new class to radio element
-                $this.addClass('regular-radio');
 
             }
-
         });
+
     };
 
 
