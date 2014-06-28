4b3b0fcc5f0d8710c23c1ffd5eceae18b2d62db2
Andy Strobel <andreas.strobel@googlemail.com>
2014-06-29 00:41:08 +0200

    Fixing layout issues at admin area

--- protected/modules_core/admin/views/module/list.php
+++ protected/modules_core/admin/views/module/list.php
@@ -1,53 +1,54 @@
-<h1><?php echo Yii::t('AdminModule.base', 'Modules'); ?></h1>
-
-<ul class="nav nav-pills" id="moduleTabs">
-    <li class="active"><a href="#extensions">Installed</a></li>
-    <!--<li><?php echo CHtml::link('Browse online', $this->createUrl('listOnline')); ?></li>-->
-</ul>
-<br>
-
-<h2>Installed Modules</h2>
+<div class="panel panel-default">
+    <div class="panel-heading"><?php echo Yii::t('AdminModule.base', 'Modules'); ?></div>
+    <div class="panel-body">
 
 
-<?php foreach ($installedModules as $moduleId => $module) : ?>
-    <div class="media">
-        <img class="media-object img-rounded pull-left" data-src="holder.js/64x64" alt="64x64"
-             style="width: 64px; height: 64px;"
-             src="<?php echo $module->getImage(); ?>">
+        <h2>Installed Modules</h2>
 
-        <div class="media-body">
-            <h4 class="media-heading"><?php echo $module->getName(); ?>
-                <small>
-                    <?php if ($module->isEnabled()) : ?>
-                        <span
-                            class="label label-success"><?php echo Yii::t('SpaceModule.base', 'Activated'); ?></span>
-                    <?php endif; ?>
-                </small>
-            </h4>
 
-            <p><?php echo $module->getDescription(); ?></p>
+        <?php foreach ($installedModules as $moduleId => $module) : ?>
+            <div class="media">
+                <img class="media-object img-rounded pull-left" data-src="holder.js/64x64" alt="64x64"
+                     style="width: 64px; height: 64px;"
+                     src="<?php echo $module->getImage(); ?>">
 
-            <p><small>
-                    <?php if ($module->isEnabled()) : ?>
-                        <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Disable'), array('//admin/module/disable', 'moduleId' => $moduleId)); ?>
+                <div class="media-body">
+                    <h4 class="media-heading"><?php echo $module->getName(); ?>
+                        <small>
+                            <?php if ($module->isEnabled()) : ?>
+                                <span
+                                    class="label label-success"><?php echo Yii::t('SpaceModule.base', 'Activated'); ?></span>
+                            <?php endif; ?>
+                        </small>
+                    </h4>
 
-                        <?php if ($module->getConfigUrl()) : ?>
-                            &middot; <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Configure'), $module->getConfigUrl()); ?>
-                        <?php endif; ?>
-                    <?php else: ?> 
-                        <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Enable'), array('//admin/module/enable', 'moduleId' => $moduleId)); ?>
-                    <?php endif; ?>
-                    
-                    <!--
-                    &middot; <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Uninstall'), array('//admin/module/uninstall', 'moduleId' => $moduleId)); ?>
-                    -->
-                    
-                    <!--
-                    &middot; <a href="#"> Details</a>
+                    <p><?php echo $module->getDescription(); ?></p>
+
+                    <p>
+                        <small>
+                            <?php if ($module->isEnabled()) : ?>
+                                <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Disable'), array('//admin/module/disable', 'moduleId' => $moduleId)); ?>
+
+                                <?php if ($module->getConfigUrl()) : ?>
+                                    &middot; <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Configure'), $module->getConfigUrl()); ?>
+                                <?php endif; ?>
+                            <?php else: ?>
+                                <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Enable'), array('//admin/module/enable', 'moduleId' => $moduleId)); ?>
+                            <?php endif; ?>
+
+                            <!--
+                            &middot; <?php echo CHtml::link(Yii::t('AdminModule.modules', 'Uninstall'), array('//admin/module/uninstall', 'moduleId' => $moduleId)); ?>
+                            -->
+
+                            <!--
+                            &middot; <a href="#"> Details</a>
                     &middot; <a href="#"> Uninstall</a>
                     -->
-                </small></p>
+                        </small>
+                    </p>
 
-        </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
-<?php endforeach; ?>
+</div>
