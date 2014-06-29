f07d5daa8bbee60ed8b12fad550462c077d850dd
Andy Strobel <andreas.strobel@googlemail.com>
2014-06-29 15:30:54 +0200

    Fixed File Upload Button at ContentForm to work together with space image uploader

--- protected/modules_core/file/widgets/views/fileUploadButton.php
+++ protected/modules_core/file/widgets/views/fileUploadButton.php
@@ -39,87 +39,92 @@
 <span class="btn btn-info fileinput-button tt" data-toggle="tooltip" data-placement="top" title=""
       data-original-title="<?php echo Yii::t('FileModule.base', 'Upload files'); ?>">
     <i class="fa fa-cloud-upload"></i>
-    <input id="<?php echo $uploaderId; ?>" type="file" name="files[]"
+
+    <input id="<?php echo $uploaderId; ?>" class="postfileupload" type="file" name="files[]"
            data-url="<?php echo Yii::app()->createUrl('//file/file/upload'); ?>" multiple>
 </span>
 
 <script>
     $(function () {
 
-        $('#<?php echo $uploaderId; ?>').fileupload({
-            dataType: 'json',
+        $('.postfileupload').each(function () {
 
-            // After File is uploaded
-            done: function (e, data) {
+            $('#<?php echo $uploaderId; ?>').fileupload({
+                dropZone: $(this),
+                dataType: 'json',
 
-                // Parses the given JSON Array returned by FileUpload Controller
-                // See application.modules_core.file.controllers.FileController
-                //      Method: handleFileUpload  for all available json informations.
-                $.each(data.result.files, function (index, file) {
+                // After File is uploaded
+                done: function (e, data) {
 
-                    // No Upload Error
-                    if (!file.error) {
+                    // Parses the given JSON Array returned by FileUpload Controller
+                    // See application.modules_core.file.controllers.FileController
+                    //      Method: handleFileUpload  for all available json informations.
+                    $.each(data.result.files, function (index, file) {
 
-                        // Hidden Value Field, which holds a comma separetet list of
-                        // all guids of uploaded files
-                        hiddenValueField = $('#<?php echo $bindToFormFieldId; ?>');
-                        hiddenValueField.val(hiddenValueField.val() + "," + file.guid);
+                        // No Upload Error
+                        if (!file.error) {
 
-                        // Attach a simple Li Entry
-                        $('#<?php echo $uploaderId;?>_list').append('<li style="padding-left: 24px;" class="mime ' + file.mimeIcon + '">' + file.name + '</li>');
+                            // Hidden Value Field, which holds a comma separetet list of
+                            // all guids of uploaded files
+                            hiddenValueField = $('#<?php echo $bindToFormFieldId; ?>');
+                            hiddenValueField.val(hiddenValueField.val() + "," + file.guid);
 
-                    } else {
-                        alert("Could not upload File: " + file.name + "\nReason:\n" + file.errorMessage);
-                    }
+                            // Attach a simple Li Entry
+                            $('#<?php echo $uploaderId;?>_list').append('<li style="padding-left: 24px;" class="mime ' + file.mimeIcon + '">' + file.name + '</li>');
 
-                });
+                        } else {
+                            alert("Could not upload File: " + file.name + "\nReason:\n" + file.errorMessage);
+                        }
 
-            },
+                    });
 
-            progressall: function (e, data) {
+                },
 
-                var progress = parseInt(data.loaded / data.total * 100, 10);
+                progressall: function (e, data) {
 
-                // Fix: remove focus from upload button to hide tooltip
-                $('#post_submit_button').focus();
+                    var progress = parseInt(data.loaded / data.total * 100, 10);
 
-                // hide form buttons
-                $('.btn_container').hide();
+                    // Fix: remove focus from upload button to hide tooltip
+                    $('#post_submit_button').focus();
 
-                // show progress bar
-                $('#<?php echo $uploaderId; ?>_progress').show();
-
-                if (progress == 100) {
-
-                    // set upload status to 100
-                    $('#<?php echo $uploaderId; ?>_progress').children().css('width', 100 + "%");
-
-                    // hide progress bar
-                    $('#<?php echo $uploaderId; ?>_progress').hide();
-
-                    // show form buttons
-                    $('.btn_container').show();
-
-                    // show attached files
-                    $('#<?php echo $uploaderId; ?>_list').fadeIn('slow');
-
-                } else {
+                    // hide form buttons
+                    $('.btn_container').hide();
 
                     // show progress bar
                     $('#<?php echo $uploaderId; ?>_progress').show();
 
-                    // update upload status
-                    $('#<?php echo $uploaderId; ?>_progress').children().css('width', progress + "%");
+                    if (progress == 100) {
+
+                        // set upload status to 100
+                        $('#<?php echo $uploaderId; ?>_progress').children().css('width', 100 + "%");
+
+                        // hide progress bar
+                        $('#<?php echo $uploaderId; ?>_progress').hide();
+
+                        // show form buttons
+                        $('.btn_container').show();
+
+                        // show attached files
+                        $('#<?php echo $uploaderId; ?>_list').fadeIn('slow');
+
+                    } else {
+
+                        // show progress bar
+                        $('#<?php echo $uploaderId; ?>_progress').show();
+
+                        // update upload status
+                        $('#<?php echo $uploaderId; ?>_progress').children().css('width', progress + "%");
+                    }
+
+                    // Show Uploaded FileUploadList Widget if exists
+                    $('#<?php echo $uploaderId; ?>_details').show();
+
                 }
 
-                // Show Uploaded FileUploadList Widget if exists
-                $('#<?php echo $uploaderId; ?>_details').show();
+            }).prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
 
-            }
-
-        }).prop('disabled', !$.support.fileInput).parent().addClass($.support.fileInput ? undefined : 'disabled');
-        ;
-    });
+        })
+    })
 
 
     /**
