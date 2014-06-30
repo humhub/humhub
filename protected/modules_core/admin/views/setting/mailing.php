ee4c74fdc9595eca7787f0fab25613a89988a447
Lucas Bartholemy <lucas@bartholemy.com>
2014-07-01 00:01:33 +0200

    Mailing View Optimization (Hide SMTP Option when PHP is selected)

--- protected/modules_core/admin/views/setting/mailing.php
+++ protected/modules_core/admin/views/setting/mailing.php
@@ -2,10 +2,12 @@
     <div class="panel-heading"><?php echo Yii::t('AdminModule.base', '<strong>Mailing</strong> settings'); ?></div>
     <div class="panel-body">
 
-        <?php $form = $this->beginWidget('CActiveForm', array(
+        <?php
+        $form = $this->beginWidget('CActiveForm', array(
             'id' => 'mailing-settings-form',
             'enableAjaxValidation' => false,
-        )); ?>
+        ));
+        ?>
 
         <?php echo $form->errorSummary($model); ?>
 
@@ -25,35 +27,36 @@
             <?php echo $form->labelEx($model, 'transportType'); ?>
             <?php echo $form->dropDownList($model, 'transportType', $transportTypes, array('class' => 'form-control', 'readonly' => HSetting::IsFixed('transportType', 'mailing'))); ?>
         </div>
+        
+        <div id="smtpOptions">
+            <hr>
+            <h4> <?php echo Yii::t('AdminModule.base', 'SMTP Options'); ?> </h4>
 
-        <hr>
-        <h4> <?php echo Yii::t('AdminModule.base', 'SMTP Options'); ?> </h4>
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'hostname'); ?>
+                <?php echo $form->textField($model, 'hostname', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('hostname', 'mailing'))); ?>
+            </div>
 
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'hostname'); ?>
-            <?php echo $form->textField($model, 'hostname', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('hostname', 'mailing'))); ?>
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'username'); ?>
+                <?php echo $form->textField($model, 'username', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('username', 'mailing'))); ?>
+            </div>
+
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'password'); ?>
+                <?php echo $form->passwordField($model, 'password', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('password', 'mailing'))); ?>
+            </div>
+
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'port'); ?>
+                <?php echo $form->textField($model, 'port', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('port', 'mailing'))); ?>
+            </div>
+
+            <div class="form-group">
+                <?php echo $form->labelEx($model, 'encryption'); ?>
+                <?php echo $form->dropDownList($model, 'encryption', $encryptionTypes, array('class' => 'form-control', 'readonly' => HSetting::IsFixed('encryption', 'mailing'))); ?>
+            </div>
         </div>
-
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'username'); ?>
-            <?php echo $form->textField($model, 'username', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('username', 'mailing'))); ?>
-        </div>
-
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'password'); ?>
-            <?php echo $form->passwordField($model, 'password', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('password', 'mailing'))); ?>
-        </div>
-
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'port'); ?>
-            <?php echo $form->textField($model, 'port', array('class' => 'form-control', 'readonly' => HSetting::IsFixed('port', 'mailing'))); ?>
-        </div>
-
-        <div class="form-group">
-            <?php echo $form->labelEx($model, 'encryption'); ?>
-            <?php echo $form->dropDownList($model, 'encryption', $encryptionTypes, array('class' => 'form-control', 'readonly' => HSetting::IsFixed('encryption', 'mailing'))); ?>
-        </div>
-
         <hr>
         <?php echo CHtml::submitButton(Yii::t('AdminModule.base', 'Save'), array('class' => 'btn btn-primary')); ?>
 
@@ -65,6 +68,22 @@
     </div>
 </div>
 
+<script>
+    
+    if ($("#MailingSettingsForm_transportType option:selected").val() != 'smtp') {
+        $("#smtpOptions").hide();
+    }
+    
+    $('#MailingSettingsForm_transportType').on('change', function() {
+        if ($("#MailingSettingsForm_transportType option:selected").val() != 'smtp') {
+            $("#smtpOptions").hide();
+        } else {
+            $("#smtpOptions").show();
+        }
+    });
+
+    
+</script>    
 
 
 
