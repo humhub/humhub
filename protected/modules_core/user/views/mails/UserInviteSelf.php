aaa46eae9793c6c9e26643d01c9e3525c6592889
Andy Strobel <andreas.strobel@googlemail.com>
2014-07-17 16:46:16 +0200

    Integrated app name in  welcome email template

--- protected/modules_core/user/views/mails/UserInviteSelf.php
+++ protected/modules_core/user/views/mails/UserInviteSelf.php
@@ -64,7 +64,7 @@ a{
 
 
 
-/* 
+/*
 
 main color = #7191a8
 
@@ -683,7 +683,7 @@ td[class="image-124px"] img{
                                                 <tr>
                                                     <td  style="font-size: 14px; line-height: 22px; font-family:Open Sans,Arial,Tahoma, Helvetica, sans-serif; color:#777777; font-weight:300; text-align:center; ">
 
-                                                        <?php echo Yii::t('UserModule.views_mails_UserInviteSelf', 'We are happy that you want to use HumHub. The Open Source Social Network to increase your communication and teamwork. Please click on the button below to proceed with your registration.'); ?>
+                                                        <?php echo Yii::t('UserModule.views_mails_UserInviteSelf', 'We are happy that you want to use ' .Yii::app()->name. '. The Open Source Social Network to increase your communication and teamwork. Please click on the button below to proceed with your registration.'); ?>
 
                                                     </td>
                                                 </tr>
