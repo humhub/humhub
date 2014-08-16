73935fafff301e61c1e498bdbae433c51b627ce0
Lucas Bartholemy <lucas@bartholemy.com>
2014-08-16 12:31:30 +0200

    Added default access rules for content container controllers

--- protected/components/ContentContainerController.php
+++ protected/components/ContentContainerController.php
@@ -52,6 +52,33 @@ class ContentContainerController extends Controller
     public $autoCheckContainerAccess = true;
 
     /**
+     * @return array action filters
+     */
+    public function filters()
+    {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+        );
+    }
+
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules()
+    {
+        return array(
+            array('allow', // allow authenticated user to perform 'create' and 'update' actions
+                'users' => array('@'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    /**
      * Automatically loads the underlying contentContainer (User/Space) by using
      * the uguid/sguid request parameter
      * 
