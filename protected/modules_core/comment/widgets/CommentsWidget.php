dd3267fc4126df3c6bbe25d356fc731baffe2d8d
allyraza <saytoally@hotmail.com>
2014-08-18 07:22:38 -0700

    clean up comment widgets

--- protected/modules_core/comment/widgets/CommentsWidget.php
+++ protected/modules_core/comment/widgets/CommentsWidget.php
@@ -9,46 +9,41 @@
  * @package humhub.modules_core.comment
  * @since 0.5
  */
-class CommentsWidget extends HWidget
-{
+class CommentsWidget extends HWidget {
 
-    /**
-     * Content Object
-     */
-    public $object;
+	/**
+	 * Content Object
+	 */
+	public $object;
 
-    /**
-     * Executes the widget.
-     */
-    public function run()
-    {
+	/**
+	 * Executes the widget.
+	 */
+	public function run()
+	{
 
-        $modelName = $this->object->content->object_model;
-        $modelId = $this->object->content->object_id;
-        
-        // Indicates that the number of comments was limited
-        $isLimited = false;
+		$modelName = $this->object->content->object_model;
+		$modelId = $this->object->content->object_id;
+		
+		// Indicates that the number of comments was limited
+		$isLimited = false;
 
-        // Count all Comments
-        $commentCount = Comment::GetCommentCount($modelName, $modelId);
-        $comments = Comment::GetCommentsLimited($modelName, $modelId, 2);
+		// Count all Comments
+		$commentCount = Comment::GetCommentCount($modelName, $modelId);
+		$comments = Comment::GetCommentsLimited($modelName, $modelId, 2);
 
-        if ($commentCount > 2)
-            $isLimited = true;
+		if ($commentCount > 2)
+			$isLimited = true;
 
-        $this->render('comments', array(
-            'object' => $this->object,
-            
-            'comments' => $comments,
-            'modelName' => $modelName,
-            'modelId' => $modelId,
-            'id' => $modelName . "_" . $modelId,
-            'isLimited' => $isLimited,
-            'total' => $commentCount
-                )
-        );
-    }
+		$this->render('comments', array(
+			'object' => $this->object,
+			'comments' => $comments,
+			'modelName' => $modelName,
+			'modelId' => $modelId,
+			'id' => $modelName . "_" . $modelId,
+			'isLimited' => $isLimited,
+			'total' => $commentCount
+		));
+	}
 
 }
-
-?>
