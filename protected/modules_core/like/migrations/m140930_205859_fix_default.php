e6b62107a4da1040e00e30fd1c8a4d97a491c20f
Lucas Bartholemy <lucas@bartholemy.com>
2014-09-30 23:31:41 +0200

    Added missing table columns default value to support stricter sql modes (#223)

---
+++ protected/modules_core/like/migrations/m140930_205859_fix_default.php
@@ -0,0 +1,27 @@
+<?php
+
+class m140930_205859_fix_default extends EDbMigration
+{
+
+    public function up()
+    {
+        $this->alterColumn('like', 'target_user_id', 'int(11) DEFAULT NULL');
+    }
+
+    public function down()
+    {
+        echo "m140930_205859_fix_default does not support migration down.\n";
+        return false;
+    }
+
+    /*
+      // Use safeUp/safeDown to do migration with transaction
+      public function safeUp()
+      {
+      }
+
+      public function safeDown()
+      {
+      }
+     */
+}
---
+++ protected/modules_core/notification/migrations/m140930_212528_fix_default.php
@@ -0,0 +1,29 @@
+<?php
+
+class m140930_212528_fix_default extends EDbMigration
+{
+
+    public function up()
+    {
+        $this->alterColumn('notification', 'emailed', 'tinyint(4) NOT NULL DEFAULT \'0\'');
+        $this->alterColumn('notification', 'updated_by', "int(11) DEFAULT NULL");
+        $this->alterColumn('notification', 'created_by', "int(11) DEFAULT NULL");
+    }
+
+    public function down()
+    {
+        echo "m140930_212528_fix_default does not support migration down.\n";
+        return false;
+    }
+
+    /*
+      // Use safeUp/safeDown to do migration with transaction
+      public function safeUp()
+      {
+      }
+
+      public function safeDown()
+      {
+      }
+     */
+}
