39519fd72b31d35a97848043d8dd640322f49f4e
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-06 19:48:32 +0200

    Fixed notify users broken on user profiles (#247)

--- protected/modules_core/wall/widgets/views/contentForm.php
+++ protected/modules_core/wall/widgets/views/contentForm.php
@@ -1,27 +1,28 @@
-<div class="panel panel-default <?php if (Yii::app()->getController()->id == 'dashboard') { echo 'hidden'; } ?>">
+<div class="panel panel-default <?php if (Yii::app()->getController()->id == 'dashboard') {
+    echo 'hidden';
+} ?>">
     <div class="panel-body" id="contentFormBody">
 
-        <?php echo CHtml::form('', 'POST'); ?>
+<?php echo CHtml::form('', 'POST'); ?>
 
         <ul id="contentFormError">
         </ul>
 
-        <?php echo $form; ?>
+<?php echo $form; ?>
 
         <div id="notifyUserContainer" class="form-group hidden" style="margin-top: 15px;">
             <input type="text" value="" id="notifyUserInput" name="notifyUserInput"/>
 
             <?php
-
-            $user_url = '//space/space/searchMemberJson';
-            if (get_class($contentContainer) == Wall::TYPE_USER) {
-                $user_url = '//user/search/json';
+            $user_url = $this->createUrl('//user/search/json', array('keyword' => '-keywordPlaceholder-'));
+            if (get_class($contentContainer) == 'Space') {
+                $user_url = $this->createUrl('//space/space/searchMemberJson', array('sguid' => $this->contentContainer->guid, 'keyword' => '-keywordPlaceholder-'));
             }
 
             /* add UserPickerWidget to notify members */
             $this->widget('application.modules_core.user.widgets.UserPickerWidget', array(
                 'inputId' => 'notifyUserInput',
-                'userSearchUrl' => $this->createUrl($user_url, array('sguid' => $this->contentContainer->guid, 'keyword' => '-keywordPlaceholder-')),
+                'userSearchUrl' => $user_url,
                 'maxUsers' => 10,
                 'userGuid' => Yii::app()->user->guid,
                 'placeholderText' => Yii::t('WallModule.widgets_views_archiveLink', 'Add a member to notify'),
@@ -45,14 +46,14 @@
                 <?php
                 $url = CHtml::normalizeUrl(Yii::app()->createUrl($submitUrl));
                 echo HHtml::ajaxSubmitButton($submitButtonText, $url, array(
-                        'type' => 'POST',
-                        'dataType' => 'json',
-                        'beforeSend' => "function() {
+                    'type' => 'POST',
+                    'dataType' => 'json',
+                    'beforeSend' => "function() {
                     $('.contentForm').removeClass('error');
                     $('#contentFormError').hide();
                     $('#contentFormError').empty();
                 }",
-                        'success' => "function(response) {
+                    'success' => "function(response) {
                     if (response.success) {
 
                         // application.modules_core.wall function
@@ -90,7 +91,7 @@
 
                     }
              }",
-                    ), array('id' => "post_submit_button", 'class' => 'btn btn-info')
+                        ), array('id' => "post_submit_button", 'class' => 'btn btn-info')
                 );
                 ?>
                 <?php
@@ -102,7 +103,7 @@
                 ?>
 
                 <!-- public checkbox -->
-                <?php echo CHtml::checkbox("visibility", "", array('id'=>'contentForm_visibility', 'class' => 'contentForm hidden')); ?>
+<?php echo CHtml::checkbox("visibility", "", array('id' => 'contentForm_visibility', 'class' => 'contentForm hidden')); ?>
 
                 <!-- content sharing -->
                 <div class="pull-right">
@@ -118,13 +119,13 @@
                                             class="fa fa-bell"></i> <?php echo Yii::t('WallModule.widgets_views_archiveLink', 'Notify members'); ?>
                                     </a>
                                 </li>
-                                <?php if (get_class($this->contentContainer) == 'Space' && $this->contentContainer->canShare()): /* can create public content */ ?>
+<?php if (get_class($this->contentContainer) == 'Space' && $this->contentContainer->canShare()): /* can create public content */ ?>
                                     <li>
                                         <a id="contentForm_visibility_entry" href="javascript:changeVisibility();"><i
                                                 class="fa fa-unlock"></i> <?php echo Yii::t('WallModule.widgets_views_archiveLink', 'Make public'); ?>
                                         </a>
                                     </li>
-                                <?php endif; ?>
+<?php endif; ?>
                             </ul>
                         </li>
                     </ul>
@@ -146,7 +147,7 @@
 
         </div>
         <!-- /contentForm_Options -->
-        <?php echo CHtml::endForm(); ?>
+<?php echo CHtml::endForm(); ?>
     </div>
     <!-- /panel body -->
 </div> <!-- /panel -->
@@ -161,7 +162,7 @@
 
 
     // Remove info text from the textinput
-    jQuery('#contentFormBody').click(function () {
+    jQuery('#contentFormBody').click(function() {
 
         // Hide options by default
         jQuery('.contentForm_options').fadeIn();
