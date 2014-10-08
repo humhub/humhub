81a76bc811b2e69a3f9264e5e2b0166fc2780679
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-08 10:20:24 +0200

    Updated translations (translate.humhub.org)

--- protected/modules_core/admin/messages/ru/controllers_SettingController.php
+++ protected/modules_core/admin/messages/ru/controllers_SettingController.php
@@ -1,12 +1,12 @@
 <?php
 return array (
   'APC' => 'APC',
-  'Could not load LDAP! - Check PHP Extension' => 'Не удалось загрузить LDAP! - Проверьте PHP Extension',
+  'Could not load LDAP! - Check PHP Extension' => 'Не удалось загрузить LDAP! - Проверьте расширения PHP',
   'Database' => 'База данных',
   'File' => 'Файл',
   'No caching (Testing only!)' => 'Нет кэширования (только тестирование!)',
   'No theme' => 'Нет темы',
   'None - shows dropdown in user registration.' => 'Нет - показывает в выпадающем регистрации пользователя.',
-  'Saved' => 'Сохранить',
-  'Saved and flushed cache' => 'Сохранено и сброшен кеш',
+  'Saved' => 'Сохранено',
+  'Saved and flushed cache' => 'Сохранено со сбросом кеша',
 );
