9fffcf3222af326a95d66647a3f27a447547e2c7
Lucas Bartholemy <lucas@bartholemy.com>
2014-10-11 16:58:36 +0200

    Updated translations (translate.humhub.org)

--- protected/modules_core/directory/messages/fr/widgets_views_spaceStats.php
+++ protected/modules_core/directory/messages/fr/widgets_views_spaceStats.php
@@ -1,8 +1,8 @@
 <?php
 return array (
   '<strong>New</strong> spaces' => '<strong>Nouvel</strong> espace',
-  '<strong>Space</strong> stats' => 'Statistiques d\'<strong>Espaces</strong>',
+  '<strong>Space</strong> stats' => '<strong>Statistiques</strong> des espaces',
   'Most members' => 'Plus fréquenté',
   'Private spaces' => 'Espaces privés',
-  'Total spaces' => 'Total des espaces',
+  'Total spaces' => 'Nombre d\'espaces',
 );
