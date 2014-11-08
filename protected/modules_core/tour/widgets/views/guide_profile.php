90a4576c99eeb6f31e8f601e672d0d7472b5168e
andystrobel <andreas.strobel@googlemail.com>
2014-11-08 13:34:47 +0100

    Escaped content in TourModule #323

--- protected/modules_core/tour/widgets/views/guide_profile.php
+++ protected/modules_core/tour/widgets/views/guide_profile.php
@@ -18,46 +18,46 @@
         {
             orphan: true,
             backdrop: true,
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>User profile</strong>'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', "This is your public user profile, which can be seen by any registered user."); ?>"
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>User profile</strong>')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', "This is your public user profile, which can be seen by any registered user.")); ?>
         },
         {
             element: "#user-profile-image",
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> photo'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', 'Upload a new profile photo by simply clicking here or by drag&drop. Do just the same for updating your cover photo.'); ?>",
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> photo')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', 'Upload a new profile photo by simply clicking here or by drag&drop. Do just the same for updating your cover photo.')); ?>,
             placement: "right"
         },
         {
             element: ".controls-account",
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Edit</strong> account'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', 'Click on this button to update your profile and account settings. You can also add more information to your profile.'); ?>",
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Edit</strong> account')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', 'Click on this button to update your profile and account settings. You can also add more information to your profile.')); ?>,
             placement: "left"
         },
         {
             element: ".profile-nav-container",
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> menu'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', 'Just like in the space, the user profile can be personalized with various modules.<br><br>You can see which modules are available for your profile by looking them in “Modules” in the account settings menu.'); ?>",
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> menu')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', 'Just like in the space, the user profile can be personalized with various modules.<br><br>You can see which modules are available for your profile by looking them in “Modules” in the account settings menu.')); ?>,
             placement: "right"
         },
         {
             element: "#contentFormBody",
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> stream'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', 'Each profile has its own pin board. Your posts will also appear on the dashboards of those users who are following you.'); ?>",
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Profile</strong> stream')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', 'Each profile has its own pin board. Your posts will also appear on the dashboards of those users who are following you.')); ?>,
             placement: "bottom"
         },
         <?php if (Yii::app()->user->isAdmin() == true) : ?>
         {
             orphan: true,
             backdrop: true,
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Hurray!</strong> You\'re done!'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', 'You\'ve completed the user profile guide!<br><br>To carry on with the administration guide, click here:<br /><br />'); ?> <a href='javascript:gotoAdministration = true; tourCompleted();'><?php echo Yii::t("TourModule.widgets_views_guide_profile", "<strong>Administration (Modules)</strong>"); ?></a><br><br>"
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Hurray!</strong> You\'re done!')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', 'You\'ve completed the user profile guide!<br><br>To carry on with the administration guide, click here:<br /><br />')); ?> + "<a href='javascript:gotoAdministration = true; tourCompleted();'><?php echo Yii::t("TourModule.widgets_views_guide_profile", "<strong>Administration (Modules)</strong>"); ?></a><br><br>"
         }
         <?php else : ?>
         {
             orphan: true,
             backdrop: true,
-            title: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', '<strong>Hurray!</strong> The End.'); ?>",
-            content: "<?php echo Yii::t('TourModule.widgets_views_guide_profile', "You've completed the user profile guide!"); ?>"
+            title: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', '<strong>Hurray!</strong> The End.')); ?>,
+            content: <?php echo json_encode(Yii::t('TourModule.widgets_views_guide_profile', "You've completed the user profile guide!")); ?>
         }
         <?php endif; ?>
 
