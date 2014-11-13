b5b11385ec04144068ecb724ffc77417972700a4
Lucas Bartholemy <lucas@bartholemy.com>
2014-11-13 09:47:15 +0100

    Added missing translations

--- protected/modules_core/wall/widgets/views/notificationSwitchLink.php
+++ protected/modules_core/wall/widgets/views/notificationSwitchLink.php
@@ -3,7 +3,7 @@
     $offLinkId = 'notification_off_' . $content->getUniqueId();
     $onLinkId = 'notification_on_' . $content->getUniqueId();
 
-    echo HHtml::ajaxLink('<i class="fa fa-bell-slash-o"></i> Turn off notifications', Yii::app()->createUrl('//wall/content/notificationSwitch', array(
+    echo HHtml::ajaxLink('<i class="fa fa-bell-slash-o"></i> '.Yii::t('WallModule.widgets_views_notificationSwitchLink', 'Turn off notifications'), Yii::app()->createUrl('//wall/content/notificationSwitch', array(
                 'id' => $content->id,
                 'className' => get_class($content),
                 'switch' => 0
@@ -18,7 +18,7 @@
     ));
     ?>
     <?php
-    echo HHtml::ajaxLink('<i class="fa fa-bell-o"></i> Turn on notifications', Yii::app()->createUrl('//wall/content/notificationSwitch', array(
+    echo HHtml::ajaxLink('<i class="fa fa-bell-o"></i> '.Yii::t('WallModule.widgets_views_notificationSwitchLink', 'Turn on notifications'), Yii::app()->createUrl('//wall/content/notificationSwitch', array(
                 'id' => $content->id,
                 'className' => get_class($content),
                 'switch' => 1
