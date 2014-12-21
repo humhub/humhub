b331777064bad7f3663303ae049cc7ca242a9208
Lucas Bartholemy <lucas@bartholemy.com>
2014-12-21 08:45:58 +0100

    Updated translations (translate.humhub.org)

--- protected/modules_core/admin/messages/cs/views_setting_index.php
+++ protected/modules_core/admin/messages/cs/views_setting_index.php
@@ -2,6 +2,6 @@
 return array (
   '<strong>Basic</strong> settings' => '<strong>Základní</strong> nastavení',
   'E.g. http://example.com/humhub' => 'Např. http://example.com/humhub',
-  'New users will automatically added to these space(s).' => 'Do tohoto jednoho či více míst budou automaticky přidání noví uživatelé.',
+  'New users will automatically added to these space(s).' => 'Do těchto prostorů budou automaticky přidání noví uživatelé.',
   'Save' => 'Uložit',
 );
--- protected/modules_core/user/messages/cs/views_account_editModules.php
+++ protected/modules_core/user/messages/cs/views_account_editModules.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   '<strong>User</strong> modules' => '<strong>Moduly</strong> uživatele',
-  'Are you really sure? *ALL* module data for your profile will be deleted!' => 'Jste si opravdu jistý(á)? *VŠECHNA* data modulu vázaná k vašemu profilu budou smazána!',
+  'Are you really sure? *ALL* module data for your profile will be deleted!' => 'Jste si opravdu jistí? *VŠECHNA* data modulu vázaná k vašemu profilu budou smazána!',
   'Configure' => 'Nastavit',
   'Disable' => 'Vypnout',
   'Enable' => 'Zapnout',
