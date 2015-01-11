d93e5653f5efdb018661bd608dc46d1c3846a598
Jordan Thompson <jordanslost@gmail.com>
2015-01-11 09:51:53 -0800

    Added Ekko Lightbox to User Profile Image
    Added Ekko Lightbox to User Profile Image to view larger version.

--- protected/modules_core/user/widgets/views/profileHeader.php
+++ protected/modules_core/user/widgets/views/profileHeader.php
@@ -82,10 +82,34 @@
 
         <div class="image-upload-container profile-user-photo-container" style="width: 140px; height: 140px;">
 
-            <!-- profile image output-->
-            <img class="img-rounded profile-user-photo" id="user-profile-image"
-                 src="<?php echo $user->getProfileImage()->getUrl(); ?>"
-                 data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;"/>
+            <?php
+                           
+                /* Get original profile image URL */
+                                   
+                $profileImageExt = pathinfo($user->getProfileImage()->getUrl(), PATHINFO_EXTENSION);
+                                   
+                $profileImageOrig = preg_replace('/.[^.]*$/', '', $user->getProfileImage()->getUrl());
+                $defaultImage = (basename($user->getProfileImage()->getUrl()) == 'default_user.jpg' || basename($user->getProfileImage()->getUrl()) == 'default_user.jpg?cacheId=0') ? true : false;
+                $profileImageOrig = $profileImageOrig .'_org.' . $profileImageExt;
+                                   
+                if (!$defaultImage) {
+                                   
+            ?>
+                   
+                <!-- profile image output-->
+                <a data-toggle="lightbox" data-gallery="" href="<?php echo $profileImageOrig; ?>#.jpeg"  data-footer='<button type="button" class="btn btn-primary" data-dismiss="modal"><?php echo Yii::t('FileModule.widgets_views_showFiles', 'Close'); ?></button>'>
+                    <img class="img-rounded profile-user-photo" id="user-profile-image"
+                        src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+                            data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;"/>
+                </a>
+                           
+            <?php } else { ?>
+                           
+                <img class="img-rounded profile-user-photo" id="user-profile-image"
+                    src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+                        data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;"/>
+                                             
+            <?php } ?>
 
             <!-- check if the current user is the profile owner and can change the images -->
             <?php if ($isProfileOwner) { ?>
