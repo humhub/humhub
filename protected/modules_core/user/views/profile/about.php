04d3dd40c82d3e590cb49bca489edc07108a1625
andystrobel <andreas.strobel@googlemail.com>
2015-01-13 13:14:38 +0100

    Removed unnecessary closing DIV tag

--- protected/modules_core/user/views/profile/about.php
+++ protected/modules_core/user/views/profile/about.php
@@ -1,44 +1,47 @@
 <div class="panel panel-default">
-   <div class="panel-heading"><?php echo Yii::t('UserModule.views_profile_about', '<strong>About</strong> this user'); ?></div>
+    <div
+        class="panel-heading"><?php echo Yii::t('UserModule.views_profile_about', '<strong>About</strong> this user'); ?></div>
 
     <div class="panel-body">
 
-        <?php $firstClass="active";?>
+        <?php $firstClass = "active"; ?>
 
 
         <ul id="tabs" class="nav nav-tabs" data-tabs="tabs">
             <?php foreach ($user->profile->getProfileFieldCategories() as $category): ?>
-                <li class="<?php echo $firstClass; $firstClass = ""; ?>"><a href="#profile-category-<?php echo $category->id; ?>"
-                                                                            data-toggle="tab"><?php echo  Yii::t($category->getTranslationCategory(), $category->title); ?></a></li>
+                <li class="<?php echo $firstClass;
+                $firstClass = ""; ?>"><a href="#profile-category-<?php echo $category->id; ?>"
+                                         data-toggle="tab"><?php echo Yii::t($category->getTranslationCategory(), $category->title); ?></a>
+                </li>
             <?php endforeach; ?>
         </ul>
 
-        <?php $firstClass="active";?>
+        <?php $firstClass = "active"; ?>
 
         <div class="tab-content">
             <?php foreach ($user->profile->getProfileFieldCategories() as $category): ?>
 
-                <div class="tab-pane <?php echo $firstClass; $firstClass = ""; ?>" id="profile-category-<?php echo $category->id; ?>">
+                <div class="tab-pane <?php echo $firstClass;
+                $firstClass = ""; ?>" id="profile-category-<?php echo $category->id; ?>">
                     <form class="form-horizontal" role="form">
                         <?php foreach ($user->profile->getProfileFields($category) as $field) : ?>
 
                             <div class="form-group">
-                                <label class="col-sm-3 control-label"><?php echo Yii::t($field->getTranslationCategory(), $field->title); ?></label>
+                                <label
+                                    class="col-sm-3 control-label"><?php echo Yii::t($field->getTranslationCategory(), $field->title); ?></label>
 
 
-								<?php if ( strtolower($field->title) == 'about' ) { ?>
-                                <div class="col-sm-9">
-                                    <p class="form-control-static"><?php echo HHtml::enrichText($field->getUserValue($user, false)); ?></p>
-                                </div>
-								<?php } else { ?>
-                                <div class="col-sm-9">
-                                    <p class="form-control-static"><?php echo $field->getUserValue($user, false); ?></p>
-                                </div>
-								<?php } ?>
+                                <?php if (strtolower($field->title) == 'about') { ?>
+                                    <div class="col-sm-9">
+                                        <p class="form-control-static"><?php echo HHtml::enrichText($field->getUserValue($user, false)); ?></p>
+                                    </div>
+                                <?php } else { ?>
+                                    <div class="col-sm-9">
+                                        <p class="form-control-static"><?php echo $field->getUserValue($user, false); ?></p>
+                                    </div>
+                                <?php } ?>
                             </div>
-                            </div>
-
-
+                            
                         <?php endforeach; ?>
 
                     </form>
