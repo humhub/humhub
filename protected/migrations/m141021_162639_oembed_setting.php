ced24d40bd87fb5d51d98d1d9a48baefde2dd942
Andres Gomez <agomez@igalia.com>
2015-02-04 15:32:07 +0200

    [oembed] Added Flickr support to OEmbed

--- protected/migrations/m141021_162639_oembed_setting.php
+++ protected/migrations/m141021_162639_oembed_setting.php
@@ -7,7 +7,7 @@ class m141021_162639_oembed_setting extends EDbMigration
     {
         $this->insert('setting', array(
             'name' => 'oembedProviders',
-            'value_text' => '{"vimeo.com":"http:\/\/vimeo.com\/api\/oembed.json?scheme=https&url=%url%&format=json&maxwidth=450","youtube.com":"http:\/\/www.youtube.com\/oembed?scheme=https&url=%url%&format=json&maxwidth=450","youtu.be":"http:\/\/www.youtube.com\/oembed?scheme=https&url=%url%&format=json&maxwidth=450","soundcloud.com":"https:\/\/soundcloud.com\/oembed?url=%url%&format=json&maxwidth=450","slideshare.net":"https:\/\/www.slideshare.net\/api\/oembed\/2?url=%url%&format=json&maxwidth=450"}'
+            'value_text' => '{"vimeo.com":"http:\/\/vimeo.com\/api\/oembed.json?scheme=https&url=%url%&format=json&maxwidth=450","youtube.com":"http:\/\/www.youtube.com\/oembed?scheme=https&url=%url%&format=json&maxwidth=450","youtu.be":"http:\/\/www.youtube.com\/oembed?scheme=https&url=%url%&format=json&maxwidth=450","soundcloud.com":"https:\/\/soundcloud.com\/oembed?url=%url%&format=json&maxwidth=450","slideshare.net":"https:\/\/www.slideshare.net\/api\/oembed\/2?url=%url%&format=json&maxwidth=450","flickr.com":"https:\/\/flickr.com\/services\/oembed?url=%url%&format=json&maxwidth=450","flic.kr":"https:\/\/flickr.com\/services\/oembed?url=%url%&format=json&maxwidth=450"}'
         ));
     }
 
