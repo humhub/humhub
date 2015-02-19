5dccadaf77f61ccb7ceb49992780ea57824bf74c
Lucas Bartholemy <lucas@bartholemy.com>
2015-02-19 15:41:23 +0100

    Shorten max length for profile fields (firstname, lastname, title) #314

--- protected/modules_core/installer/controllers/ConfigController.php
+++ protected/modules_core/installer/controllers/ConfigController.php
@@ -362,7 +362,7 @@ class ConfigController extends Controller
         $field->required = 1;
         $field->show_at_registration = 1;
         if ($field->save()) {
-            $field->fieldType->maxLength = 100;
+            $field->fieldType->maxLength = 20;
             $field->fieldType->save();
         } else {
             throw new CHttpException(500, print_r($field->getErrors(), true));
@@ -379,7 +379,7 @@ class ConfigController extends Controller
         $field->required = 1;
         $field->is_system = 1;
         if ($field->save()) {
-            $field->fieldType->maxLength = 100;
+            $field->fieldType->maxLength = 30;
             $field->fieldType->save();
         }
 
@@ -392,7 +392,7 @@ class ConfigController extends Controller
         $field->field_type_class = 'ProfileFieldTypeText';
         $field->is_system = 1;
         if ($field->save()) {
-            $field->fieldType->maxLength = 100;
+            $field->fieldType->maxLength = 50;
             $field->fieldType->save();
         }
 
