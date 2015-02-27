b38eaefe06e8fd9a19f7ab882bd73b042b58e8f8
Lucas Bartholemy <lucas@bartholemy.com>
2015-02-27 11:51:33 +0100

    Updated translations (translate.humhub.org)

--- protected/modules_core/wall/messages/ru/widgets_views_contentForm.php
+++ protected/modules_core/wall/messages/ru/widgets_views_contentForm.php
@@ -1,9 +1,9 @@
 <?php
 return array (
   'Add a member to notify' => 'Добавить участника, чтобы уведомить',
-  'Make private' => 'Сделать приватным',
-  'Make public' => 'Сделать публичным',
+  'Make private' => 'Сделать личным',
+  'Make public' => 'Обнародовать',
   'Notify members' => 'Уведомлять пользователей',
-  'Public' => 'Публичное',
+  'Public' => 'Обнародованное',
   'Whats on your mind?' => 'О чем вы думаете?',
 );
