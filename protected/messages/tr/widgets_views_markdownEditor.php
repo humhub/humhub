9d07839454e0367e57895bed234e9e0a92342930
Çağatay <JeXawoW@users.noreply.github.com>
2015-03-30 14:00:44 +0300

    Update widgets_views_markdownEditor.php

--- protected/messages/tr/widgets_views_markdownEditor.php
+++ protected/messages/tr/widgets_views_markdownEditor.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'Add image/file' => '',
-  'Add link' => '',
+  'Add image/file' => 'Resim/Dosya Ekle',
+  'Add link' => 'Link Ekle',
   'Close' => 'Kapat',
-  'Enter a url (e.g. http://example.com)' => '',
-  'Please wait while uploading...' => '',
-  'Target' => '',
+  'Enter a url (e.g. http://example.com)' => 'Url girin (Örnek: http://example.com)',
+  'Please wait while uploading...' => 'Yüklenirken lütfen bekleyin...',
+  'Target' => 'Hedef',
   'Title' => 'Başlık',
 );
