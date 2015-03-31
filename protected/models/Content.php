2282ea9d6463285665fa80268b23735716b0182c
Lucas Bartholemy <lucas@bartholemy.com>
2015-03-31 02:18:20 +0200

    Fixed phpdoc header

--- protected/models/Content.php
+++ protected/models/Content.php
@@ -3,7 +3,7 @@
 /**
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licenses/
+ * @license https://www.humhub.com/licences
  */
 
 /**
