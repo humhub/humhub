3c4fba7c30332cc978e8fd0172e34d860e3abe0c
Lucas Bartholemy <lucas@bartholemy.com>
2015-04-01 14:50:27 +0200

    Fixed OEmbed error on when open_basedir is set

--- protected/models/UrlOembed.php
+++ protected/models/UrlOembed.php
@@ -179,7 +179,10 @@ class UrlOembed extends HActiveRecord
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_TIMEOUT, 15);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        
+        // Not available when open_basedir is set.
+        @curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+        
         if (HSetting::Get('enabled', 'proxy')) {
             curl_setopt($curl, CURLOPT_PROXY, HSetting::Get('server', 'proxy'));
             curl_setopt($curl, CURLOPT_PROXYPORT, HSetting::Get('port', 'proxy'));
