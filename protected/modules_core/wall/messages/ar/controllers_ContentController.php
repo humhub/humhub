57430fbcf3fe0aa5fbecad42c13ed0910a5f1424
Renoa <renoa@users.noreply.github.com>
2015-04-10 22:40:58 +0300

    Update controllers_ContentController.php

--- protected/modules_core/wall/messages/ar/controllers_ContentController.php
+++ protected/modules_core/wall/messages/ar/controllers_ContentController.php
@@ -1,8 +1,8 @@
 <?php
 return array (
-  'Could not load requested object!' => '',
+  'Could not load requested object!' => 'لم نتمكن من عرض الشيء المطلوب!',
   'Maximum number of sticked items reached!
 
 You can stick only two items at once.
-To however stick this item, unstick another before!' => '',
+To however stick this item, unstick another before!' => 'لقد تعديت الحد الأقصى من المحتويات المثبتة, الرجاء إلغاء التثبيت عن أحد المحتويات.',
 );
