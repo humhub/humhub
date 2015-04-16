85380d8a64c6615dcd1e0f04650096502fc2a031
Chris Tembreull <chris@tembreull.org>
2015-04-15 17:03:35 -0700

    re-add flyout stuff

--- themes/CoMotion/views/layouts/head.php
+++ themes/CoMotion/views/layouts/head.php
@@ -1,5 +1,9 @@
 <?php $ver = HVersion::VERSION; ?>
 
+<link href="<?php echo Yii::app()->baseUrl; ?>/css/jasny-bootstrap.min.css" rel="stylesheet">
+<script type="text/javascript"
+        src="<?php echo Yii::app()->baseUrl; ?>/js/jasny-bootstrap.js"></script>
+
 <!-- start: CSS -->
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/datepicker.css?ver=<?php echo $ver; ?>" rel="stylesheet">
 <link href="<?php echo Yii::app()->theme->baseUrl; ?>/css/theme.css?ver=<?php echo $ver; ?>" rel="stylesheet">
