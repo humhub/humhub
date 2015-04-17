04ee15f59059554800021458de22d288a692301d
Bryan Smith <brysmi@gmail.com>
2015-04-17 16:47:02 -0700

    add theme.css.map (for dev/chrome) to gitignore | Max size user portraits for larger resolutions (out of scope but distracting) | remove inline styling where possible in favor of CSS

--- protected/modules_core/user/widgets/views/profileHeader_comotion.php
+++ protected/modules_core/user/widgets/views/profileHeader_comotion.php
@@ -1,20 +1,14 @@
-
-<!-- This makes theme dev/debugging very difficult
-  TODO: refactor styling so we don't have to repeat this in all modules -->
-
-<style type="text/css">
-  body {margin: 0; padding: 0; }
-  div.container { width: 100%; margin: 0; padding: 0; }
-</style>
-
 <div class="comotion-profile comotion-profile-header">
-
-  <img src="<?php echo $user->getProfileImage()->getUrl(); ?>" style="width: 100%;">
+  <!-- inline style! TODO: refactor to css -->
+  <!-- TODO: port user-photo upload so user can add photo! -->
+  <img  class="profile-user-photo" id="user-profile-image"
+        src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+        />
   <?php if (!$isProfileOwner) { ?>
     <div>Match</div>
   <?php } ?>
   <div class="comotion-profile-data">
     <h1><?php echo CHtml::encode($user->displayName); ?></h1>
-    <h3><?php echo CHtml::encode($user->profile->title); ?></h3>
+    <h2><?php echo CHtml::encode($user->profile->title); ?></h2>
   </div>
 </div>
