5689050943ba16575aa117c48a1a7ce19949bd27
Lucas Bartholemy <lucas@bartholemy.com>
2015-06-24 16:48:52 +0200

    Replaced index.php - added composer.json

--- index.php
+++ index.php
@@ -1,41 +1,20 @@
 <?php
 
 /**
- * HumHub
- * Copyright © 2014 The HumHub Project
- *
- * The texts of the GNU Affero General Public License with an additional
- * permission and of our proprietary license can be found at and
- * in the LICENSE file you have received along with this program.
- *
- * According to our dual licensing model, this program can be used either
- * under the terms of the GNU Affero General Public License, version 3,
- * or under a proprietary license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
  */
-$yii = dirname(__FILE__) . '/protected/vendors/yii/yii.php';
-$config = dirname(__FILE__) . '/protected/config/main.php';
-$appClass = dirname(__FILE__) . '/protected/components/WebApplication.php';
-
-// Disable these 3 lines when in production mode
+// comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 5);
-ini_set('error_reporting', E_ALL);
+defined('YII_ENV') or define('YII_ENV', 'dev');
 
-require_once($yii);
-require_once($appClass);
+require(__DIR__ . '/protected/vendor/autoload.php');
+require(__DIR__ . '/protected/vendor/yiisoft/yii2/Yii.php');
+$config = require(__DIR__ . '/protected/config/web.php');
 
-$app = Yii::createApplication('WebApplication', $config);
+Yii::setAlias('@webroot', __DIR__);
+Yii::setAlias('@app', __DIR__ . DIRECTORY_SEPARATOR . 'protected');
+Yii::setAlias('@humhub', '@app/humhub');
 
-Yii::setPathOfAlias('Zend',Yii::getPathOfAlias('application.vendors.Zend2'));
-        
-Yii::import('application.vendors.*');
-EZendAutoloader::$prefixes = array('Zend', 'Custom');
-Yii::import("ext.yiiext.components.zendAutoloader.EZendAutoloader", true);
-Yii::registerAutoloader(array("EZendAutoloader", "loadClass"), true);
-
-$app->run();
+(new yii\web\Application($config))->run();
