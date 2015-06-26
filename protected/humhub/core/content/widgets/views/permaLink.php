8a92649b919e8f6f046ddc6d8566dbb9f905fc8e
Lucas Bartholemy <lucas@bartholemy.com>
2015-06-26 18:38:23 +0200

    Removed ZendSearch

--- protected/humhub/core/content/widgets/views/permaLink.php
+++ protected/humhub/core/content/widgets/views/permaLink.php
@@ -11,7 +11,7 @@ use yii\helpers\Url;
         'uniqueID' => 'modal_permalink_' . $id,
         'linkOutput' => 'a',
         'title' => Yii::t('ContentModule.widgets_views_permaLink', '<strong>Permalink</strong> to this post'),
-        'message' => '<textarea rows="3" id="permalink-txt-' . $id . '" class="form-control permalink-txt">' . Url::to(['//wall/perma/content', 'model' => $model, 'id' => $id]) . '</textarea><p class="help-block">Copy to clipboard: Ctrl+C, Enter</p>',
+        'message' => '<textarea rows="3" id="permalink-txt-' . $id . '" class="form-control permalink-txt">' . Url::to(['//wall/perma/content', 'model' => $model, 'id' => $id], true) . '</textarea><p class="help-block">Copy to clipboard: Ctrl+C, Enter</p>',
         'buttonFalse' => Yii::t('ContentModule.widgets_views_permaLink', 'Close'),
         'linkContent' => '<i class="fa fa-link"></i> ' . Yii::t('ContentModule.widgets_views_permaLink', 'Permalink'),
         'linkHref' => '',
