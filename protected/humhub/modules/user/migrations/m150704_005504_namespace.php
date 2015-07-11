80c1568622d4ee0858a03b453da118b932a16d50
Lucas Bartholemy <lucas@bartholemy.com>
2015-07-11 11:01:55 +0200

    Change fieldType case

--- protected/humhub/modules/user/migrations/m150704_005504_namespace.php
+++ protected/humhub/modules/user/migrations/m150704_005504_namespace.php
@@ -10,14 +10,14 @@ class m150704_005504_namespace extends Migration
 
     public function up()
     {
-        
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\Text'], ['field_type_class' => 'ProfileFieldTypeText']);
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\Birthday'], ['field_type_class' => 'ProfileFieldTypeBirthday']);
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\DateTime'], ['field_type_class' => 'ProfileFieldTypeDateTime']);
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\Number'], ['field_type_class' => 'ProfileFieldTypeNumber']);
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\Select'], ['field_type_class' => 'ProfileFieldTypeSelect']);
-        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldType\TextArea'], ['field_type_class' => 'ProfileFieldTypeTextArea']);
-                
+
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\Text'], ['field_type_class' => 'ProfileFieldTypeText']);
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\Birthday'], ['field_type_class' => 'ProfileFieldTypeBirthday']);
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\DateTime'], ['field_type_class' => 'ProfileFieldTypeDateTime']);
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\Number'], ['field_type_class' => 'ProfileFieldTypeNumber']);
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\Select'], ['field_type_class' => 'ProfileFieldTypeSelect']);
+        $this->update('profile_field', ['field_type_class' => 'humhub\modules\user\models\fieldtype\TextArea'], ['field_type_class' => 'ProfileFieldTypeTextArea']);
+
         $this->renameClass('User', User::className());
         $this->renameClass('UserFollow', User::className());
     }
