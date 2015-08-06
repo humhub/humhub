7896f22f99a2b9826d7c3ccbb42b821f510fbd9b
andystrobel <andreas.strobel@googlemail.com>
2015-08-06 19:24:28 +0200

    Styled new datepicker component

--- protected/humhub/assets/AppAsset.php
+++ protected/humhub/assets/AppAsset.php
@@ -18,7 +18,6 @@ class AppAsset extends AssetBundle
     public $css = [
         'css/animate.min.css',
         'css/bootstrap.min.css',
-        'css/datepicker.css',
         'css/temp.css',
         'resources/font-awesome/css/font-awesome.min.css',
         'css/bootstrap-wysihtml5.css',
