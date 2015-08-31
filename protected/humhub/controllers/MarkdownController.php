1f06acdc722113030c1fc0afe5a6a8c447414012
Lucas Bartholemy <lucas@bartholemy.com>
2015-08-31 15:06:51 +0200

    Fixed markdown preview action

--- protected/humhub/controllers/MarkdownController.php
+++ protected/humhub/controllers/MarkdownController.php
@@ -1,22 +1,15 @@
 <?php
 
 /**
- * HumHub
- * Copyright © 2014 The HumHub Project
- *
- * The texts of the GNU Affero General Public License with an additional
- * permission and of our proprietary license can be found at and
- * in the LICENSE file you have received along with this program.
- *
- * According to our dual licensing model, this program can be used either
- * under the terms of the GNU Affero General Public License, version 3,
- * or under a proprietary license.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
  */
+
+namespace humhub\controllers;
+
+use Yii;
+use humhub\components\Controller;
 
 /**
  * MarkdownController provides preview for MarkdownEditorWidget
@@ -39,27 +32,10 @@ class MarkdownController extends Controller
         ];
     }
 
-    /**
-     * Specifies the access control rules.
-     * This method is used by the 'accessControl' filter.
-     * @return array access control rules
-     */
-    public function accessRules()
-    {
-        return array(
-            array('allow', // allow authenticated user to perform 'create' and 'update' actions
-                'users' => array('@'),
-            ),
-            array('deny', // deny all users
-                'users' => array('*'),
-            ),
-        );
-    }
-
     public function actionPreview()
     {
         $this->forcePostRequest();
-        return $this->widget('application.widgets.MarkdownViewWidget', array('markdown' => Yii::app()->request->getParam('markdown')));
+        return \humhub\widgets\MarkdownView::widget(['markdown' => Yii::$app->request->post('markdown')]);
     }
 
 }
