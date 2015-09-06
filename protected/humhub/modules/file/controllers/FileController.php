b378bd9113d16688b4e91ded8d0c197e49c00816
Lucas Bartholemy <lucas@bartholemy.com>
2015-09-06 22:06:03 +0200

    Fixed: File downloads throu X-Sendfile

--- protected/humhub/modules/file/controllers/FileController.php
+++ protected/humhub/modules/file/controllers/FileController.php
@@ -166,7 +166,7 @@ class FileController extends \humhub\components\Controller
                     $filePath = substr($filePath, strlen($docroot));
                 }
             }
-            Yii::$app->response->xSendFile($filePath . DIRECTORY_SEPARATOR . $fileName, $options);
+            Yii::$app->response->xSendFile($filePath . DIRECTORY_SEPARATOR . $fileName, null, $options);
         }
     }
 
