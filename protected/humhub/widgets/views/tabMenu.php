3443ef11282566913651511d7d8bcc2c896e0f20
Sebastian Stumpf <s.stumpf@zeros.ones.de>
2015-09-22 18:25:26 +0200

    Reworked Style of TabMenu.

--- protected/humhub/widgets/views/tabMenu.php
+++ protected/humhub/widgets/views/tabMenu.php
@@ -4,14 +4,14 @@
  *
  * @package humhub.widgets
  * @since 0.5 */
+
+use \yii\helpers\Html
 ?>
-<ul class="nav nav-tabs">
-    <?php foreach ($this->getItems() as $item) : ?>
-        <li class="<?php if ($item['isActive']): ?>active<?php endif; ?>">
-            <a href="<?php echo $item['url']; ?>" target="<?php echo $item['target']; ?>">
-                <?php //echo $item['icon']; ?>
-                <?php echo $item['label']; ?>
-            </a>
-        </li>
-    <?php endforeach; ?>
+<ul class="nav nav-pills">
+
+    <?php foreach ($this->context->getItems() as $item) {?>
+        <li <?php echo Html::renderTagAttributes($item['htmlOptions'])?>>
+        <?php echo Html::a($item['label'], $item['url']); ?>
+    </li>
+    <?php }; ?>
 </ul>
