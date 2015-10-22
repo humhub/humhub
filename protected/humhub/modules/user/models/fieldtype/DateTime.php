fdeb5b87e1e7098b90bf46fda57da846a06df939
Lucas Bartholemy <lucas@bartholemy.com>
2015-10-22 15:38:34 +0200

    Fixed #1214: Use 'short' date format for better compatiblity

--- protected/humhub/modules/user/models/fieldtype/DateTime.php
+++ protected/humhub/modules/user/models/fieldtype/DateTime.php
@@ -83,7 +83,7 @@ class DateTime extends BaseType
      */
     public function getFieldRules($rules = array())
     {
-        $rules[] = array($this->profileField->internal_name, 'date', 'timestampAttribute' => $this->profileField->internal_name);
+        $rules[] = array($this->profileField->internal_name, 'date', 'format' => 'short', 'timestampAttribute' => $this->profileField->internal_name);
         return parent::getFieldRules($rules);
     }
 
@@ -94,6 +94,7 @@ class DateTime extends BaseType
     {
         return array($this->profileField->internal_name => array(
                 'type' => 'datetime',
+                'format' => 'short',
                 'class' => 'form-control',
                 'dateTimePickerOptions' => array(
                     'pickTime' => ($this->showTimePicker)
