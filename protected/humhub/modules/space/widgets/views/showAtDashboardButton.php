476383cf54144086342ecb2468014fb35f331f15
andystrobel <andreas.strobel@googlemail.com>
2015-11-02 17:28:41 +0100

    Removed ShowAtDashbaordButton

--- protected/humhub/modules/space/widgets/views/showAtDashboardButton.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use yii\helpers\Html;
-
-if ($showAtDashboard) {
-    print Html::a(Yii::t('SpaceModule.widgets_views_followButton', "Hide at dashboard"), $space->createUrl('/space/membership/switch-dashboard-display', ['show' => 0]), array('data-method' => 'POST', 'class' => 'btn btn-primary'));
-} else {
-    print Html::a(Yii::t('SpaceModule.widgets_views_followButton', "Show at dashboard"), $space->createUrl('/space/membership/switch-dashboard-display', ['show' => 1]), array('data-method' => 'POST', 'class' => 'btn btn-primary'));
-}
