8713cea7c7a622777ae7afcd4615cd4f622d0678
andystrobel <andreas.strobel@googlemail.com>
2015-11-04 17:50:14 +0100

    Changed description in public space view

--- protected/humhub/modules/space/views/space/index.php
+++ protected/humhub/modules/space/views/space/index.php
@@ -6,7 +6,7 @@ echo \humhub\modules\content\widgets\Stream::widget(array(
     'streamAction' => '/space/space/stream',
     'messageStreamEmpty' => ($space->canWrite()) ?
             Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b><br>Start by posting something here...') :
-            Yii::t('SpaceModule.views_space_index', '<b>This space is still empty!</b>'),
+            Yii::t('SpaceModule.views_space_index', '<b>You are not member of this space and there is no public content, yet!</b>'),
     'messageStreamEmptyCss' => ($space->canWrite()) ?
             'placeholder-empty-stream' :
             '',
