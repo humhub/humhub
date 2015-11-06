3a7433351a6fe75a0bb00ea00bfa8b432b01e39c
Lucas Bartholemy <lucas@bartholemy.com>
2015-11-06 11:29:49 +0100

    Fixed #1288: Removed gii module from default console config

--- protected/humhub/config/console.php
+++ protected/humhub/config/console.php
@@ -5,10 +5,6 @@ Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
 return [
     'id' => 'humhub-console',
     'controllerNamespace' => 'humhub\commands',
-    'bootstrap' => ['gii'],
-    'modules' => [
-        'gii' => 'yii\gii\Module',
-    ],
     'components' => [
         'user' => [
             'class' => 'humhub\modules\user\components\User',
