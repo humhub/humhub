6b3f2583fae9306bc67c18332778f026ac51b70d
Lucas Bartholemy <lucas@bartholemy.com>
2015-11-20 15:11:39 +0100

    Dynamic top account menu

--- protected/humhub/modules/user/widgets/views/accountTopMenu.php
+++ protected/humhub/modules/user/widgets/views/accountTopMenu.php
@@ -10,54 +10,28 @@ use \yii\helpers\Url;
         <li class="dropdown account">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                 <div class="user-title pull-left hidden-xs">
-                    <strong><?php echo Html::encode($user->displayName); ?></strong><br/><span class="truncate"><?php echo Html::encode($user->profile->title); ?></span>
+                    <strong><?php echo Html::encode(Yii::$app->user->getIdentity()->displayName); ?></strong><br/><span class="truncate"><?php echo Html::encode(Yii::$app->user->getIdentity()->profile->title); ?></span>
                 </div>
 
                 <img id="user-account-image" class="img-rounded"
-                     src="<?php echo $user->getProfileImage()->getUrl(); ?>"
+                     src="<?php echo Yii::$app->user->getIdentity()->getProfileImage()->getUrl(); ?>"
                      height="32" width="32" alt="32x32" data-src="holder.js/32x32"
                      style="width: 32px; height: 32px;"/>
 
                 <b class="caret"></b>
             </a>
             <ul class="dropdown-menu pull-right">
-                <li>
-                    <a href="<?php echo $user->createUrl('/user/profile'); ?>"><i
-                            class="fa fa-user"></i> <?php echo Yii::t('base', 'My profile'); ?>
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo Url::toRoute('/user/account/edit'); ?>"><i
-                            class="fa fa-edit"></i> <?php echo Yii::t('base', 'Account settings'); ?>
-                    </a>
-                </li>
-
-                <?php if (Yii::$app->user->isAdmin()) : ?>
-                    <li class="divider"></li>
-                    <li>
-                        <a href="<?php echo Url::toRoute('/admin'); ?>"><i
-                                class="fa fa-cogs"></i> <?php echo Yii::t('base', 'Administration'); ?>
-                        </a>
-                    </li>
-                <?php endif; ?>
-
-
-                <!-- if the current user has admin rights -->
-                <?php if ($showUserApprovals) : ?>
-                    <li>
-                        <a href="<?php echo Url::toRoute('/admin/approval'); ?>"><i
-                                class="fa fa-check-circle"></i> <?php echo Yii::t('base', 'User Approvals'); ?>
-                        </a>
-                    </li>
-                <?php endif; ?>
-
-
-                <li class="divider"></li>
-                <li>
-                    <a href="<?php echo Url::toRoute('/user/auth/logout'); ?>"><i
-                            class="fa fa-sign-out"></i> <?php echo Yii::t('base', 'Logout'); ?>
-                    </a>
-                </li>
+                <?php foreach ($this->context->getItems() as $item): ?>
+                    <?php if ($item['label'] == '---'): ?>
+                        <li class="divider"></li>
+                        <?php else: ?>
+                        <li>
+                            <a href="<?php echo $item['url']; ?>">
+                                <?php echo $item['icon'] . ' ' .$item['label']; ?>
+                            </a>
+                        </li>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </ul>
         </li>
     </ul>
