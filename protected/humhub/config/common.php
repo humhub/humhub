f745ef5d5461db1330fbd996660a4ac89157338b
Lucas Bartholemy <lucas@bartholemy.com>
2015-11-23 15:41:43 +0100

    Changed version number to 1.0.0.beta.1

--- protected/humhub/config/common.php
+++ protected/humhub/config/common.php
@@ -7,7 +7,7 @@ Yii::setAlias('@humhub', '@app/humhub');
 
 $config = [
     'name' => 'HumHub',
-    'version' => '0.20.1',
+    'version' => '1.0.0-beta.1',
     'basePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
     'bootstrap' => ['log', 'humhub\components\bootstrap\ModuleAutoLoader'],
     'sourceLanguage' => 'en',
