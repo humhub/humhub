c3daa47fc679994b3e58df5fb98d816f92133149
Lucas Bartholemy <lucas@bartholemy.com>
2015-12-13 22:04:45 +0100

    Fixed: Prevent daily update available notification spam

--- protected/humhub/modules/admin/Events.php
+++ protected/humhub/modules/admin/Events.php
@@ -58,9 +58,25 @@ class Events extends \yii\base\Object
         $controller->stdout("Checking for new HumHub version... ");
 
         $latestVersion = libs\HumHubAPI::getLatestHumHubVersion();
-        if ($latestVersion != "" && version_compare($latestVersion, Yii::$app->version, ">")) {
-            $notification = new notifications\NewVersionAvailable();
-            $notification->sendBulk(User::find()->where(['super_admin' => 1]));
+        if ($latestVersion != "") {
+            $adminUserQuery = User::find()->where(['super_admin' => 1]);
+            $latestNotifiedVersion = Setting::Get('lastVersionNotify', 'admin');
+            $adminsNotified = !($latestNotifiedVersion == "" || version_compare($latestVersion, $latestNotifiedVersion, ">"));
+            $newVersionAvailable = (version_compare($latestVersion, Yii::$app->version, ">"));
+            $updateNotification = new notifications\NewVersionAvailable();
+
+            // Cleanup existing notifications
+            if (!$newVersionAvailable || ($newVersionAvailable && !$adminsNotified)) {
+                foreach ($adminUserQuery->all() as $admin) {
+                    $updateNotification->delete($admin);
+                }
+            }
+
+            // Create new notification
+            if ($newVersionAvailable && !$adminsNotified) {
+                $updateNotification->sendBulk($adminUserQuery);
+                Setting::Set('lastVersionNotify', $latestVersion, 'admin');
+            }
         }
 
         $controller->stdout('done. ' . PHP_EOL, \yii\helpers\Console::FG_GREEN);
