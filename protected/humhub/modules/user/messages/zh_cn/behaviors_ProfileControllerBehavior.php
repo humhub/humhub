45436583c8b8090b419e25398c51fe77780686ec
xiaomalover <xiaomalover@gmail.com>
2015-12-31 10:03:08 +0800

    update language file

--- protected/humhub/modules/user/messages/zh_cn/behaviors_ProfileControllerBehavior.php
+++ protected/humhub/modules/user/messages/zh_cn/behaviors_ProfileControllerBehavior.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'This user account is not approved yet!' => '',
-  'You need to login to view this user profile!' => '',
+  'This user account is not approved yet!' => '用户还未通过审核！',
+  'You need to login to view this user profile!' => '你需要登录才能查看用户信息！',
   'User not found!' => '没找到用户',
 );
