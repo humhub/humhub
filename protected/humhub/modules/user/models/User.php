a1177bcb22eb27c73e345c93b9c4990a198ea357
Lucas Bartholemy <lucas@bartholemy.com>
2016-02-02 02:56:35 +0100

    Fixed #1408: Make authMode user attribute write protected for admins

--- protected/humhub/modules/user/models/User.php
+++ protected/humhub/modules/user/models/User.php
@@ -122,7 +122,7 @@ class User extends ContentContainerActiveRecord implements \yii\web\IdentityInte
     {
         $scenarios = parent::scenarios();
         $scenarios['login'] = ['username', 'password'];
-        $scenarios['editAdmin'] = ['username', 'email', 'group_id', 'super_admin', 'auth_mode', 'status'];
+        $scenarios['editAdmin'] = ['username', 'email', 'group_id', 'super_admin', 'status'];
         $scenarios['registration'] = ['username', 'email', 'group_id'];
         return $scenarios;
     }
