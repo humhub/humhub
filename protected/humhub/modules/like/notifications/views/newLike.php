8e650330f8863843e20cbaeb2c2939751959f796
helnokaly <hany.elnokaly@nextechnology.me>
2016-02-24 14:55:30 +0200

    fix issue #1459 Notification of Comment like indicates Post instead of Comment

--- protected/humhub/modules/like/notifications/views/newLike.php
+++ protected/humhub/modules/like/notifications/views/newLike.php
@@ -4,6 +4,6 @@ use yii\helpers\Html;
 
 echo Yii::t('LikeModule.views_notifications_newLike', "%displayName% likes %contentTitle%.", array(
     '%displayName%' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
-    '%contentTitle%' => $this->context->getContentInfo($source->content->getPolymorphicRelation())
+    '%contentTitle%' => $this->context->getContentInfo($source->getSource())
 ));
 ?>
