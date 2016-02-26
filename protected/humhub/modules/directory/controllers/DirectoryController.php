9f0655a909db9c9e6dfd7f996c9bbbbe3c302cb5
Lucas Bartholemy <lucas@bartholemy.com>
2016-02-26 07:49:17 +0100

    Enh: Added directory page size setting

--- protected/humhub/modules/directory/controllers/DirectoryController.php
+++ protected/humhub/modules/directory/controllers/DirectoryController.php
@@ -82,7 +82,7 @@ class DirectoryController extends \humhub\modules\directory\components\Controlle
         $searchOptions = [
             'model' => \humhub\modules\user\models\User::className(),
             'page' => $page,
-            'pageSize' => Setting::Get('paginationSize'),
+            'pageSize' => $this->module->pageSize,
         ];
 
         if ($this->module->memberListSortField != "") {
@@ -126,7 +126,7 @@ class DirectoryController extends \humhub\modules\directory\components\Controlle
             'model' => \humhub\modules\space\models\Space::className(),
             'page' => $page,
             'sortField' => ($keyword == '') ? 'title' : null,
-            'pageSize' => Setting::Get('paginationSize'),
+            'pageSize' => $this->module->pageSize,
         ]);
 
         $pagination = new \yii\data\Pagination(['totalCount' => $searchResultSet->total, 'pageSize' => $searchResultSet->pageSize]);
