5e5c24fde5d538b81602ffe804facd1f1a0f332f
chha25 <chriz.hammerbacher@gmail.com>
2016-03-30 13:46:56 +0200

    Revert "Revert "Revert "Generate code files for the administration of the field assignment groups"""
    This reverts commit f7bb9969bb7931a1b5d7c70699322cae3d2820fc.

--- protected/humhub/modules/admin/views/user-profile/fieldAssignmentGroup.php
+++
@@ -1,9 +0,0 @@
-<div class="panel panel-default">
-
-    <div class="panel-heading"> <?php echo Yii::t('AdminModule.views_userprofile_fieldAssignment', '<strong>Assignment</strong> of profile field groups');   ?></div>
-        
-    <div class="panel-body">
-        
-    </div>
-    
-</div>
