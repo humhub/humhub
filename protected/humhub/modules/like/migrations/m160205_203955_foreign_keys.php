c7ac9c3ee179f15d8b6c4ddb74f43ef0f94b9f56
Vasile Gabriel <bunconta@live.com>
2016-04-07 23:51:38 +0300

    Add foreign keys

---
+++ protected/humhub/modules/like/migrations/m160205_203955_foreign_keys.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m160205_203955_foreign_keys extends Migration
+{
+    public function up()
+    {
+        $this->addForeignKey('fk_like-created_by', 'like', 'created_by', 'user', 'id', 'CASCADE', 'CASCADE');
+        $this->addForeignKey('fk_like-target_user_id', 'like', 'target_user_id', 'user', 'id', 'CASCADE', 'CASCADE');
+    }
+
+    public function down()
+    {
+        $this->dropForeignKey('fk_like-created_by', 'like');
+        $this->dropForeignKey('fk_like-target_user_id', 'like');
+
+        return true;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
