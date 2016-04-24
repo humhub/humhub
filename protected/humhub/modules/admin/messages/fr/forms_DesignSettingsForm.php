2166dc2a0adf3c1cdf854110012d4a8b90b26133
HumHub Translate <info@humhub.com>
2016-04-24 19:58:52 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/admin/messages/fr/forms_DesignSettingsForm.php
+++ protected/humhub/modules/admin/messages/fr/forms_DesignSettingsForm.php
@@ -2,6 +2,6 @@
 return array (
   'Default pagination size (Entries per page)' => 'Taille de pagination par défaut (entrées par page)',
   'Display Name (Format)' => 'Nom affiché (format)',
-  'Dropdown space order' => '',
+  'Dropdown space order' => 'Ordre des espaces dans la liste déroulante',
   'Theme' => 'Thème',
 );
--- protected/humhub/modules/space/messages/de/widgets_SpaceMembersMenu.php
+++ protected/humhub/modules/space/messages/de/widgets_SpaceMembersMenu.php
@@ -2,7 +2,7 @@
 return array (
   'Members' => 'Mitglieder',
   'Owner' => 'Eigentümer',
-  'Pending Approvals' => '',
-  'Pending Invites' => '',
+  'Pending Approvals' => 'Ausstehende Freigaben',
+  'Pending Invites' => 'Ausstehende Einladungen',
   'Permissions' => 'Berechtigungen',
 );
