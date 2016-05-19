018d7b9a27efa6bd8faeaad8bde95faa687a72ef
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2016-05-19 15:21:32 +0200

    Added BaseSidebar class for various sidebars

--- protected/humhub/modules/directory/widgets/NewMembers.php
+++ protected/humhub/modules/directory/widgets/NewMembers.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
 namespace humhub\modules\directory\widgets;
 
 use Yii;
