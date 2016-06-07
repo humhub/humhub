73c2c6afc6fb11ae5b8a7be4c9b1f19fc9d6c03e
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2016-06-07 11:36:36 +0200

    Improved space membership listing (sort order)

--- protected/humhub/modules/user/components/ActiveQueryUser.php
+++ protected/humhub/modules/user/components/ActiveQueryUser.php
@@ -2,7 +2,7 @@
 
 /**
  * @link https://www.humhub.org/
- * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  */
 
@@ -18,14 +18,26 @@ use yii\db\ActiveQuery;
 class ActiveQueryUser extends ActiveQuery
 {
 
-    public function init()
-    {
-        parent::init();
-    }
-
+    /**
+     * Limit to active users
+     * 
+     * @return \humhub\modules\user\components\ActiveQueryUser
+     */
     public function active()
     {
         $this->andWhere(['user.status' => \humhub\modules\user\models\User::STATUS_ENABLED]);
+        return $this;
+    }
+
+    /**
+     * Adds default user order (e.g. by lastname)
+     * 
+     * @return \humhub\modules\user\components\ActiveQueryUser
+     */
+    public function defaultOrder()
+    {
+        $this->joinWith('profile');
+        $this->addOrderBy(['profile.lastname' => SORT_ASC]);
         return $this;
     }
 
