fbca6e4c0f219802dbacf51f0ba5b2d75472938e
Kharlova Daria <kharlovad@sima-land.ru>
2016-06-09 17:45:15 +0500

    move ico to themes head

--- themes/HumHub/views/layouts/head.php
+++ themes/HumHub/views/layouts/head.php
@@ -1,2 +1,32 @@
+<!-- start: Favicon and Touch Icons -->
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16"
+      href="<?php echo Yii::getAlias("@web"); ?>/ico/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+<meta charset="<?= Yii::$app->charset ?>">
+<!-- end: Favicon and Touch Icons -->
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/font/open_sans/open-sans.css'; ?>" rel="stylesheet">
