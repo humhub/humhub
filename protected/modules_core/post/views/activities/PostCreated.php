f14eed021655bd4b6be15cac23f1549fa57e0557
Den Volkov <volkovdenya@gmail.com>
2016-06-20 10:19:42 +0300

    TC-10 fixed class color

--- protected/modules_core/post/views/activities/PostCreated.php
+++ protected/modules_core/post/views/activities/PostCreated.php
@@ -1,6 +1,6 @@
 <?php $this->beginContent('application.modules_core.activity.views.activityLayout', array('activity' => $activity)); ?>                    
 <?php
-echo Yii::t('PostModule.views_activities_PostCreated', "<i class='fa fa-dot-circle-o color-circle-mentorship' style='margin-right: 5px;vertical-align: middle;color:blue !important'></i> %displayName% created a new post.", array(
+echo Yii::t('PostModule.views_activities_PostCreated', "<i class='fa fa-dot-circle-o color-circle-mentorship' style='margin-right: 5px;vertical-align: middle;'></i> %displayName% created a new post.", array(
     '%displayName%' => '<strong>' . CHtml::encode($user->displayName) . '</strong>'
 ));
 ?>
