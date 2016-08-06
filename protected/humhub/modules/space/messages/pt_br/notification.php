aa972f2173ec24f98a1507c7a0283853421a72fc
HumHub Translate <info@humhub.com>
2016-08-06 13:20:10 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/space/messages/pt_br/notification.php
+++ protected/humhub/modules/space/messages/pt_br/notification.php
@@ -5,5 +5,5 @@ return array (
   '{displayName} declined your invite for the space {spaceName}' => '{displayName} recusou seu convite para o espaço {spaceName}',
   '{displayName} declined your membership request for the space {spaceName}' => '{displayName} recusou sua adesão ao espaço {spaceName}',
   '{displayName} invited you to the space {spaceName}' => '{displayName} convidou você para o espaço {spaceName}',
-  '{displayName} requests membership for the space {spaceName}' => '',
+  '{displayName} requests membership for the space {spaceName}' => '{displayName} pediu convite para o espaço {spaceName}',
 );
