ec8a425dc6a56e794dd3494418382f2164bb754a
buddha87 <mail@jharrer.de>
2016-10-28 23:05:07 +0200

    Small text fix in german language.

--- protected/humhub/modules/admin/messages/de/views_setting_index.php
+++ protected/humhub/modules/admin/messages/de/views_setting_index.php
@@ -5,7 +5,7 @@ return array (
   'Dashboard' => 'Übersicht',
   'Delete' => 'Löschen',
   'E.g. http://example.com/humhub' => 'z.B. http://example.com/humhub',
-  'Friendship' => 'Freunschaften',
+  'Friendship' => 'Freundschaften',
   'New users will automatically be added to these space(s).' => 'Neue Benutzer werden automatisch zu diesen Space(s) hinzugefügt. ',
   'Save' => 'Speichern',
   'You\'re using no logo at the moment. Upload your logo now.' => 'Du benutzt momentan kein eigenes Logo. Lade dein eigenes Logo hoch.',
