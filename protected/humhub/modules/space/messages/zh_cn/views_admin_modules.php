0996021ce30318ceb6c52e968a42731251b5ff87
xiaomalover <xiaomalover@gmail.com>
2016-11-25 16:04:24 +0800

    update

--- protected/humhub/modules/space/messages/zh_cn/views_admin_modules.php
+++ protected/humhub/modules/space/messages/zh_cn/views_admin_modules.php
@@ -4,7 +4,7 @@ return array (
   'Activated' => '启用',
   'Are you sure? *ALL* module data for this space will be deleted!' => '你确定？这个模块的所有的数据将会被删除！',
   'Configure' => '配置',
-  'Currently there are no modules available for this space!' => '该空间当前没有可能模块',
+  'Currently there are no modules available for this space!' => '该空间当前没有生效模块',
   'Disable' => '失效',
   'Enable' => '生效',
   'Enhance this space with modules.' => '用模块来增强这个空间。',
