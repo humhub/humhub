0a86d58f0a2fb5adc4eea6c4632e45a64021722e
Hj Ahmad Rasyid Hj Ismail <ahrasis@gmail.com>
2016-11-30 21:06:32 +0800

    Create error.php

---
+++ protected/humhub/messages/my/error.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  '<strong>Login</strong> required' => 'Es necesario <strong>iniciar sesión</strong>',
+  'An internal server error occurred.' => 'Un error interno ha ocurrido',
+  'You are not allowed to perform this action.' => 'Usted no tiene permisos para ejecutar esta acción',
+);
