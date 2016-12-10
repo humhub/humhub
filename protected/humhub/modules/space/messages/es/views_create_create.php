d15ff4eb81c9371e32cb347037d4197c6aa45c70
HumHub Translations <info@humhub.com>
2016-12-10 12:20:24 +0100

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/space/messages/es/views_create_create.php
+++ protected/humhub/modules/space/messages/es/views_create_create.php
@@ -3,6 +3,6 @@ return array (
   '<strong>Create</strong> new space' => '<strong>Crear</strong> nuevo espacio',
   'Advanced access settings' => 'Ajustes avanzados de acceso',
   'Next' => 'Siguiente',
-  'Space name' => '',
+  'Space name' => 'Nombre del Espacio',
   'space description' => 'descripción del espacio',
 );
