2a246acfdc7cc1168def0975f13ba3601691c4ce
buddha87 <mail@jharrer.de>
2016-12-20 20:26:44 +0100

    Tests + New theming structure.

--- protected/humhub/modules/space/tests/codeception/acceptance/CreateSpaceCest.php
+++ protected/humhub/modules/space/tests/codeception/acceptance/CreateSpaceCest.php
@@ -25,41 +25,46 @@ class CreateSpaceCest
         $I->waitForText('Create new space', 30, '#globalModal');
         $I->fillField('Space[name]', 'Space 1');
         $I->fillField('Space[description]', 'SpaceDescription');
-        
+
         $I->click('#access-settings-link');
         $I->waitForElementVisible('.field-space-join_policy');
-        
+
         // Only by invite
         $I->jsClick('#space-join_policy [value="0"]');
-        
+
         // Private visibility
         $I->jsClick('#space-visibility [value="0"]');
-        
+
         $I->click('Next', '#globalModal');
-        
+
         $I->waitForText('Name "Space 1" has already been taken.', 20, '#globalModal');
         $I->fillField('Space[name]', 'MySpace');
         $I->click('Next', '#globalModal');
-        
-        $I->waitForText('Add Modules', 20, '#globalModal');
-        
-        $I->click('Next', '#globalModal');
-        
+
+        // Fresh test environments (travis) won't have any preinstalled modules.
+        // Perhaps we should fetch an module manually by default.
+        try {
+            $I->waitForText('Add Modules', 5, '#globalModal');
+            $I->click('Next', '#globalModal');
+        } catch (Exception $e) {
+            // Do this if it's not present.
+        }
+
         $I->waitForText('Invite members', 10, '#globalModal');
         $I->selectUserFromPicker('#space-invite-user-picker', 'User1');
         $I->wait(1);
-        
+
         $I->click('Done', '#globalModal');
         $I->waitForText('MySpace');
         $I->waitForText('This space is still empty!');
-        
+
         $I->amUser1(true);
         $I->seeInNotifications('invited you to the space MySpace');
-        
+
         //TODO: Test private space
         // User Approval
     }
-    
+
     // User Approval
     // Space settings
 }
