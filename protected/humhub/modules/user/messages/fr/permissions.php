88511df1b06237e5f68d1bbca7b731de35425ab1
Rominou <befire@hotmail.fr>
2017-01-07 03:30:16 +0100

    Update permissions.php

--- protected/humhub/modules/user/messages/fr/permissions.php
+++ protected/humhub/modules/user/messages/fr/permissions.php
@@ -17,6 +17,6 @@
  * NOTE: this file must be saved in UTF-8 encoding.
  */
 return [
-    'About page' => '',
-    'Allows access to your about page' => '',
+    'About page' => 'Page À propos',
+    'Allows access to your about page' => 'Autoriser l\'accès à votre page À propos',
 ];
