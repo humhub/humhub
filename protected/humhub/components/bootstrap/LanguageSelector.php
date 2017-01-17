a57b25161f7cb6b8a41c24cf6a0dd8e2853eef81
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2017-01-17 12:16:00 +0100

    Improved I18N language handling

--- protected/humhub/components/bootstrap/LanguageSelector.php
+++ protected/humhub/components/bootstrap/LanguageSelector.php
@@ -2,7 +2,7 @@
 
 /**
  * @link https://www.humhub.org/
- * @copyright Copyright (c) 2015 HumHub GmbH & Co. KG
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  */
 
@@ -11,71 +11,21 @@ namespace humhub\components\bootstrap;
 use yii\base\BootstrapInterface;
 
 /**
- * Description of LanguageSelector
+ * LanguageSelector automatically sets the language of the i18n component
  *
+ * @see \humhub\components\i18n\I18N
  * @author luke
  */
 class LanguageSelector implements BootstrapInterface
 {
 
+    /**
+     * @inheritdoc
+     */
     public function bootstrap($app)
     {
         mb_internal_encoding('UTF-8');
-        
-        $isGuest = (!$app->params['installed'] || $app->user->isGuest);
-
-        if ($isGuest) {
-
-            if (isset($_POST['ChooseLanguage'])) {
-                /**
-                 * Use language form submitted language
-                 */
-                $languageModel = new \humhub\models\forms\ChooseLanguage();
-                if ($languageModel->load($app->request->post()) && $languageModel->validate()) {
-                    $cookie = new \yii\web\Cookie([
-                        'name' => 'language',
-                        'value' => $languageModel->language,
-                        'expire' => time() + 86400 * 365,
-                    ]);
-                    $app->getResponse()->getCookies()->add($cookie);
-                    $app->language = $languageModel->language;
-                }
-            } else {
-                /**
-                 * Use cookie or preferred language
-                 */
-                $allowedLanguages = $app->i18n->getAllowedLanguages();
-                $language = $app->request->getPreferredLanguage(array_keys($allowedLanguages));
-
-                if (isset($app->request->cookies['language'])) {
-                    $language = (string) $app->request->cookies['language'];
-
-                    // Check cookie given language is available
-                    if (!array_key_exists($language, $allowedLanguages)) {
-                        $cookie = new \yii\web\Cookie([
-                            'name' => 'language',
-                            'value' => 'en',
-                            'expire' => time() + 86400 * 365,
-                        ]);
-                        $app->getResponse()->getCookies()->add($cookie);
-                        $language = 'en';
-                    }
-                }
-                $app->language = $language;
-            }
-        } else {
-            if ($app->user->language) {
-                $app->language = $app->user->language;
-            }
-            if ($app->user->timeZone) {
-                $app->formatter->timeZone = $app->user->timeZone;
-            }
-            $app->formatter->defaultTimeZone = $app->timeZone;
-        }
-        
-        if ($app->language == 'en') {
-            $app->language = 'en-US';
-        }
+        $app->i18n->autosetLocale();
     }
 
 }
