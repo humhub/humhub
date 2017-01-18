b94b77484fc92dbab1a7bd23d550f668d59a7fee
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2017-01-18 19:14:11 +0100

    Added stream channels instead of show in stream flag

---
+++ protected/humhub/modules/stream/Module.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\stream;
+
+/**
+ * Stream Module provides stream (wall) backend and frontend
+ *
+ * @author Luke
+ * @since 1.2
+ */
+class Module extends \humhub\components\Module
+{
+    
+}
