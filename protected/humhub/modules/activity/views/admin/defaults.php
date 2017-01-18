43374aaf3bf3ebb42c46f425e7857a028666a5eb
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2017-01-18 13:12:50 +0100

    Admin panel wording fix

--- protected/humhub/modules/activity/views/admin/defaults.php
+++ protected/humhub/modules/activity/views/admin/defaults.php
@@ -6,7 +6,7 @@
     <h4><?= Yii::t('ActivityModule.base', 'E-Mail Summaries'); ?></h4>
     <div class="help-block">
         <?= Yii::t('ActivityModule.base', 'E-Mail summaries are sent to users to inform them about recent activities in your network.'); ?><br />
-        <?= Yii::t('ActivityModule.base', 'On this page you can define the default behavior for your users. These settings can be overwritten by the users in their account settings page.'); ?>
+        <?= Yii::t('ActivityModule.base', 'On this page you can define the default behavior for your users. These settings can be overwritten by users in their account settings page.'); ?>
         <br />
     </div>
     <br />
