3165fa22d114a2532d191986982d3c7833a4ea6b
buddh4 <mail@jharrer.de>
2017-01-26 14:48:40 +0100

    Added icons to user profile menu.

--- protected/humhub/modules/user/widgets/ProfileMenu.php
+++ protected/humhub/modules/user/widgets/ProfileMenu.php
@@ -10,6 +10,7 @@ namespace humhub\modules\user\widgets;
 
 use Yii;
 use humhub\modules\user\models\User;
+use humhub\modules\user\permissions\ViewAboutPage;
 
 /**
  * ProfileMenuWidget shows the (usually left) navigation on user profiles.
@@ -41,28 +42,30 @@ class ProfileMenu extends \humhub\widgets\BaseMenu
      */
     public function init()
     {
-        $this->addItemGroup(array(
+        $this->addItemGroup([
             'id' => 'profile',
             'label' => Yii::t('UserModule.widgets_ProfileMenuWidget', '<strong>Profile</strong> menu'),
             'sortOrder' => 100,
-        ));
+        ]);
 
-        $this->addItem(array(
+        $this->addItem([
             'label' => Yii::t('UserModule.widgets_ProfileMenuWidget', 'Stream'),
             'group' => 'profile',
+            'icon' => '<i class="fa fa-bars"></i>',
             'url' => $this->user->createUrl('//user/profile/home'),
             'sortOrder' => 200,
             'isActive' => (Yii::$app->controller->id == "profile" && (Yii::$app->controller->action->id == "index" || Yii::$app->controller->action->id == "home")),
-        ));
+        ]);
 
-        if ($this->user->permissionManager->can(new \humhub\modules\user\permissions\ViewAboutPage())) {
-            $this->addItem(array(
+        if ($this->user->permissionManager->can(new ViewAboutPage())) {
+            $this->addItem([
                 'label' => Yii::t('UserModule.widgets_ProfileMenuWidget', 'About'),
                 'group' => 'profile',
+                'icon' => '<i class="fa fa-info-circle"></i>',
                 'url' => $this->user->createUrl('//user/profile/about'),
                 'sortOrder' => 300,
                 'isActive' => (Yii::$app->controller->id == "profile" && Yii::$app->controller->action->id == "about"),
-            ));
+            ]);
         }
         parent::init();
     }
