ccd310662477c0c7443cfabeb52cf2917411dc5d
Daniel Kesselberg <mail@danielkesselberg.de>
2017-02-04 14:44:23 +0100

    update spelling for internalAllowAnonymousRegistration

--- protected/humhub/modules/installer/messages/da/forms_SecurityForm.php
+++ protected/humhub/modules/installer/messages/da/forms_SecurityForm.php
@@ -19,7 +19,7 @@
 return [
     'Allow friendships between members' => '',
     'Allow access for non-registered users to public content (guest access)' => 'Tillad adgang for ikke-registrerede bruger til offentligt indhold (gæste adgang)',
-    'External user can register (The registration form will be displayed at Login))' => 'Ekstern bruger kan registrere sig (Registreringen formularen vil blive vist på login siden)',
+    'External users can register (show registration form on login)' => 'Ekstern bruger kan registrere sig (Registreringen formularen vil blive vist på login siden)',
     'Newly registered users have to be activated by an admin first' => 'Nylig registrerede brugere skal aktiveres af en admin først',
     'Registered members can invite new users via email' => 'Registrerede brugere kan invitere nye brugere via email',
 ];
--- protected/humhub/modules/installer/messages/pt_br/forms_SecurityForm.php
+++ protected/humhub/modules/installer/messages/pt_br/forms_SecurityForm.php
@@ -2,7 +2,7 @@
 return array (
   'Allow access for non-registered users to public content (guest access)' => 'Permitir o acesso de usuários não registrados ao conteúdo público (acesso de convidado)',
   'Allow friendships between members' => 'Permitir amizades entre membros',
-  'External user can register (The registration form will be displayed at Login))' => 'O usuário externo pode se inscrever (O formulário de registro será exibido no Login)',
+  'External users can register (show registration form on login)' => 'O usuário externo pode se inscrever (O formulário de registro será exibido no Login)',
   'Newly registered users have to be activated by an admin first' => 'Usuários recém-registrados precisam ser ativados por um administrador primeiro',
   'Registered members can invite new users via email' => 'Os membros registrados podem convidar novos usuários por e-mail',
 );
