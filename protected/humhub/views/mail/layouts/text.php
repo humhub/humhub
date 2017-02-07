b3df50b07310a7d995e5e1f1ad6e9ddfac1ebe4b
Lucas Bartholemy <lucas.bartholemy@humhub.com>
2017-02-07 18:05:44 +0100

    Removed spaces in plaintext mail layout

--- protected/humhub/views/mail/layouts/text.php
+++ protected/humhub/views/mail/layouts/text.php
@@ -4,8 +4,8 @@
 ---
 
 <?php if (isset(Yii::$app->view->params['showUnsubscribe']) && Yii::$app->view->params['showUnsubscribe'] === true) : ?>
-    <?php $url = (isset(Yii::$app->view->params['unsubscribeUrl'])) ? Yii::$app->view->params['unsubscribeUrl'] : \yii\helpers\Url::to(['/notification/user'], true) ?>
-    <?= Yii::t('base', 'Unsubscribe') ?>: <?= $url ?>
+<?php $url = (isset(Yii::$app->view->params['unsubscribeUrl'])) ? Yii::$app->view->params['unsubscribeUrl'] : \yii\helpers\Url::to(['/notification/user'], true) ?>
+<?= Yii::t('base', 'Unsubscribe') ?>: <?= $url ?>
 <?php endif; ?>
 
 Powered by HumHub (http://www.humhub.org)
