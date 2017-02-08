1808381e1e8ee4305eca26380e9e9be74b02cfe0
buddh4 <mail@jharrer.de>
2017-02-08 16:32:36 +0100

    Fixed confirm modal + guest user js issues.

--- protected/humhub/modules/live/resources/js/humhub.live.js
+++ protected/humhub/modules/live/resources/js/humhub.live.js
@@ -1,23 +1,28 @@
-humhub.module('live', function(module, require, $) {    
+humhub.module('live', function (module, require, $) {
     var object = require('util').object;
+    var user = require('user');
     var liveClient;
 
-    var init = function() {
+    var init = function () {
+        if (user.isGuest()) {
+            return;
+        }
+        
         try {
             var clientType = require(module.config.client.type);
-            if(clientType) {
+            if (clientType) {
                 liveClient = new clientType(module.config.client.options);
             } else {
                 module.log.warn("Invalid live client configuration detected, live client could not be initialized.", module.config);
             }
-        } catch(e) {
+        } catch (e) {
             module.log.warn("Invalid live client configuration detected, live client could not be initialized.", module.config);
             module.log.error(e);
         }
     };
-    
-    var setDelay = function(value) {
-        if(object.isFunction(liveClient.setDelay)) {
+
+    var setDelay = function (value) {
+        if (object.isFunction(liveClient.setDelay)) {
             liveClient.setDelay(value);
         }
     };
