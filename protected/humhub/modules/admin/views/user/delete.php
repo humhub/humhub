d5736ed0baaf90729c33e8ed34841d1e5bcd1bdd
Lucas Bartholemy <lucas@bartholemy.com>
2017-03-24 21:25:07 +0100

    Enh: Improved administration user deletion view

--- protected/humhub/modules/admin/views/user/delete.php
+++ protected/humhub/modules/admin/views/user/delete.php
@@ -4,26 +4,34 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
 <div class="panel-body">
-    <h4><?php echo Yii::t('AdminModule.views_user_delete', 'Delete user'); ?></h4>
-    <p>
-        <?php echo Yii::t('AdminModule.views_user_delete', 'Are you sure you want to delete this user? If this user is owner of some spaces, <b>you</b> will become owner of these spaces.'); ?>
-    </p>
+    <h4><?= Yii::t('AdminModule.views_user_delete', 'Confirm user deletion'); ?></h4>
+    <br />
+    <p><?= Yii::t('AdminModule.views_user_delete', 'Are you sure you want to delete this user?'); ?></p>
 
-    <?php
-    echo \yii\widgets\DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'username',
-            'profile.firstname',
-            'profile.lastname',
-            'email:email',
-            'created_at:datetime',
-        ],
-    ]);
-    ?>
+    <ul>
+        <li><?= Yii::t('AdminModule.views_user_delete', 'All created contents of this user will be <b>deleted</b>.'); ?></li>
+        <li><?= Yii::t('AdminModule.views_user_delete', 'If this user is owner of some spaces, <b>you</b> will automatically become owner of these spaces.'); ?></li>
+    </ul>
 
-    <br/>
-    <?php echo Html::a(Yii::t('AdminModule.views_user_delete', 'Delete user'), Url::toRoute(['/admin/user/delete', 'id' => $model->id, 'doit' => 2]), array('class' => 'btn btn-danger', 'data-method' => 'POST')); ?>
-    &nbsp;
-    <?php echo Html::a(Yii::t('AdminModule.views_user_delete', 'Back'), Url::toRoute(['/admin/user/edit', 'id' => $model->id]), array('class' => 'btn btn-primary')); ?>
+    <br />
+</p>
+
+<?=
+\yii\widgets\DetailView::widget([
+    'model' => $model,
+    'attributes' => [
+        'username',
+        'profile.firstname',
+        'profile.lastname',
+        'email:email',
+        'created_at:datetime',
+    ],
+]);
+?>
+
+<br />
+<br />
+
+<?= Html::a(Yii::t('AdminModule.views_user_delete', 'Delete user'), Url::to(['/admin/user/delete', 'id' => $model->id, 'doit' => 2]), ['class' => 'btn btn-danger', 'data-method' => 'POST']); ?>
+<?= Html::a(Yii::t('AdminModule.views_user_delete', 'Cancel'), Url::to(['/admin/user/edit', 'id' => $model->id]), ['class' => 'btn btn-primary pull-right']); ?>
 </div>
