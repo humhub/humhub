83dd63f49812cd09a8d2cdd22b980c25ea8a5360
Lucas Bartholemy <lucas@bartholemy.com>
2017-03-26 15:34:04 +0200

    Plain text activity mail fixes

--- protected/humhub/modules/activity/views/layouts/mail_plaintext.php
+++ protected/humhub/modules/activity/views/layouts/mail_plaintext.php
@@ -9,13 +9,8 @@
 use yii\helpers\Html;
 ?>
 
+
 ---
 
-<?= $content; ?>
-<?php if (isset($record->content->space) && $record->content->space !== null): ?>
-    (<?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'via')); ?> <?= Html::encode($record->content->space->name); ?>)
-<?php endif; ?>
-
-<?php if ($url != "") : ?>
-    <?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'See online:')); ?> <?= urldecode($url); ?>
-<?php endif; ?>
+<?= $content; ?> <?php if (isset($record->content->space) && $record->content->space !== null): ?>(<?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'via')); ?> <?= Html::encode($record->content->space->name); ?>)
+<?php endif; ?><?php if ($url != "") : ?><?= strip_tags(Yii::t('ActivityModule.views_activityLayoutMail', 'See online:')); ?> <?= urldecode($url); ?><?php endif; ?>
