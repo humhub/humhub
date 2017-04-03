c1ed3644a79e4f015563af2d83894ac5a3771d7d
Carlos Ferreira <c.ferreira@acsiiweb.com>
2017-04-04 00:47:50 +0200

    Update 401_guests.php

--- protected/humhub/views/error/401_guests.php
+++ protected/humhub/views/error/401_guests.php
@@ -18,7 +18,7 @@ use yii\helpers\Url;
             <button class="btn btn-info" data-action-click="ui.modal.load" data-action-url="<?= Url::to(Yii::$app->user->loginUrl) ?>" data-ui-loader>
                 <?= Yii::t('base', 'Login') ?>
             </button>
-            <button data-action-click="client.back" class="btn btn-primary  pull-right" data-ui-loader>
+            <button data-action-click="client.back" class="btn btn-primary pull-right" data-ui-loader>
                 <?= Yii::t('base', 'Back'); ?>
             </button>
         </div>
