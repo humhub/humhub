a7f8a277390fc785d403d1c13fc1bdea623f6551
Evgeniy Tkachenko <et.coder@gmail.com>
2017-04-05 13:55:53 +0300

    Update colorPickerField.php

--- protected/humhub/widgets/views/colorPickerField.php
+++ protected/humhub/widgets/views/colorPickerField.php
@@ -5,10 +5,10 @@ use yii\helpers\Html;
 humhub\assets\HumHubColorPickerAsset::register($this);
 ?>
 
-<?= Html::activeTextInput($model, $field, ['class' => 'form-control', 'id' => $inputId, 'value' => $model->color, 'style' => 'display:none']); ?>
+<?= Html::activeTextInput($model, $field, ['class' => 'form-control', 'id' => $inputId, 'value' => $model->$field, 'style' => 'display:none']); ?>
 
 <script type="text/javascript">
     $(function() {
-        humhub.modules.ui.colorpicker.apply('#<?= $container ?>', '#<?= $inputId ?>', '<?= $model->color ?>')
+        humhub.modules.ui.colorpicker.apply('#<?= $container ?>', '#<?= $inputId ?>', '<?= $model->$field ?>')
     });
 </script>
