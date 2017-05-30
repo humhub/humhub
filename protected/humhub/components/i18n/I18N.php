b9008e9cb2c8a3e59ec5d7106abefe49587eff6a
githubjeka <et.coder@gmail.com>
2017-05-30 15:37:49 +0300

    Fixed syntax php error

--- protected/humhub/components/i18n/I18N.php
+++ protected/humhub/components/i18n/I18N.php
@@ -45,7 +45,7 @@ class I18N extends \yii\i18n\I18N
     public function setUserLocale($user)
     {
         if ($user === null) {
-            throw \yii\base\InvalidParamException("User cannot be null!");
+            throw new \yii\base\InvalidParamException('User cannot be null!');
         }
 
         if (!empty($user->language)) {
