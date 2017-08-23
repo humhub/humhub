ef9ef83f891573985f96bc7aa340c8b482873068
Sergey Shirokovskikh <shirokovskih_s@ekb.sima-land.ru>
2017-08-23 14:20:14 +0500

    fixed russian translate

--- protected/humhub/modules/user/messages/ru/views_auth_recoverPassword.php
+++ protected/humhub/modules/user/messages/ru/views_auth_recoverPassword.php
@@ -2,7 +2,7 @@
 return array (
   '<strong>Password</strong> recovery' => '<strong>Восстановление</strong> пароля',
   'Back' => 'Назад',
-  'Just enter your e-mail address. We´ll send you recovery instructions!' => 'Dведите ваш e-mail, чтобы получить инструкции по восстановлению!',
+  'Just enter your e-mail address. We´ll send you recovery instructions!' => 'Введите ваш e-mail, чтобы получить инструкции по восстановлению!',
   'Password recovery' => 'Восстановление пароля',
   'Reset password' => 'Сброс пароля',
   'enter security code above' => 'введите код',
