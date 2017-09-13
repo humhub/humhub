3677862b9942c14a207d930f4331f14c56119980
HumHub Translations <info@humhub.com>
2017-09-13 11:39:47 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/notification/messages/vi/models_forms_NotificationSettings.php
+++ protected/humhub/modules/notification/messages/vi/models_forms_NotificationSettings.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Allow desktop notifications by default.' => 'Cho phép desktop notification theo mặc định',
+  'Allow desktop notifications by default.' => 'Cho phép thông báo trên desktop theo mặc định',
   'Receive \'New Content\' Notifications for the following spaces' => 'Nhận thông báo \'nội dung mới\' cho những phòng bạn theo dõi',
-  'Receive desktop notifications when you are online.' => 'Nhận desktop notification khi bạn online',
+  'Receive desktop notifications when you are online.' => 'Nhận thông báo trên desktop khi bạn online',
 );
