cdd6069cec2d6e7a23dccbaba968dd998429786f
HumHub Translations <info@humhub.com>
2017-09-17 21:28:38 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/admin/messages/nb_no/views_approval_approveUserDecline.php
+++ protected/humhub/modules/admin/messages/nb_no/views_approval_approveUserDecline.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'Cancel' => 'Avbryt',
-  'Decline & delete user: <strong>{displayName}</strong>' => '',
+  'Decline & delete user: <strong>{displayName}</strong>' => 'Avvis og slett bruker: <strong>{displayName}</strong>',
 );
--- protected/humhub/modules/admin/messages/nb_no/views_module_list.php
+++ protected/humhub/modules/admin/messages/nb_no/views_module_list.php
@@ -1,13 +1,13 @@
 <?php
 return array (
-  '<strong>Modules</strong> directory' => '',
-  'Are you sure? *ALL* module data will be lost!' => '',
-  'Are you sure? *ALL* module related data and files will be lost!' => '',
+  '<strong>Modules</strong> directory' => '<strong>Modul</strong> katalog',
+  'Are you sure? *ALL* module data will be lost!' => 'Er du sikker? *ALLE* modul data vil bli slette!',
+  'Are you sure? *ALL* module related data and files will be lost!' => 'Er du sikker? *ALLE* moduler relaterte data og filer vil bli slettet!',
   'Configure' => 'Konfigurer',
   'Disable' => 'Deaktivér',
   'Enable' => 'Aktivér',
-  'Enable module...' => '',
-  'More info' => '',
-  'Set as default' => '',
-  'Uninstall' => '',
+  'Enable module...' => 'Aktiverer modul..',
+  'More info' => 'Mer info',
+  'Set as default' => 'Sett som default',
+  'Uninstall' => 'Avinstaller',
 );
