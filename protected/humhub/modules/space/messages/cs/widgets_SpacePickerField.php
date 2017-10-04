a0d60db7d756e424df4f4db986ead9a956dc1cc0
buddh4 <mail@jharrer.de>
2017-10-04 12:41:56 +0200

    Fixed #2755 Translation errors in Czech language

--- protected/humhub/modules/space/messages/cs/widgets_SpacePickerField.php
+++ protected/humhub/modules/space/messages/cs/widgets_SpacePickerField.php
@@ -2,7 +2,7 @@
 return array (
   'Add Space' => 'Přidat prostor',
   'No spaces found for the given query' => 'Pro daný dotaz nebyly nalezeny žádné mezery',
-  'Select {n,plural,=1{space} other{spaces}}' => 'Zvolit {n,plural,=1{space} jiné{spaces}}',
-  'This field only allows a maximum of {n,plural,=1{# space} other{# spaces}}' => 'Toto pole povoluje maximálně {n,plural,=1{# space} jiné{# spaces}}
+  'Select {n,plural,=1{space} other{spaces}}' => 'Zvolit {n,plural,=1{space} other{spaces}}',
+  'This field only allows a maximum of {n,plural,=1{# space} other{# spaces}}' => 'Toto pole povoluje maximálně {n,plural,=1{# space} other{# spaces}}
 ',
 );
--- protected/humhub/modules/space/messages/cs/widgets_views_spaceChooserItem.php
+++ protected/humhub/modules/space/messages/cs/widgets_views_spaceChooserItem.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  '{n,plural,=1{# new entry} other{# new entries}} since your last visit' => '{n,plural,=1{# new entry} jiné{# new entries}} od vaší poslední návštěvy',
+  '{n,plural,=1{# new entry} other{# new entries}} since your last visit' => '{n,plural,=1{# new entry} other{# new entries}} od vaší poslední návštěvy',
 );
