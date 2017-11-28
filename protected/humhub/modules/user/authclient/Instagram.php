3a0dfd409a201459b5d06d7717aaf6238f94a3b8
Sarah Tsumayoi <Felli@users.noreply.github.com>
2017-11-28 14:33:20 -0800

    Update Instagram.php

--- protected/humhub/modules/user/authclient/Instagram.php
+++ protected/humhub/modules/user/authclient/Instagram.php
@@ -1,11 +1,15 @@
 <?php
+
 /**
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  */
+
 namespace humhub\modules\user\authclient;
+
 use yii\authclient\OAuth2;
+
 class Instagram extends OAuth2
 {
     /**
