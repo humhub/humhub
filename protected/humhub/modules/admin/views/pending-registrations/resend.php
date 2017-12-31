6a04ee53abc1e139671cadd7ad394f53a4b2e3cc
Daniel Kesselberg <mail@danielkesselberg.de>
2017-12-31 15:36:06 +0100

    Add view for resend

---
+++ protected/humhub/modules/admin/views/pending-registrations/resend.php
@@ -0,0 +1,24 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Url;
+?>
+<div class="panel-body">
+    <h4><?= Yii::t('AdminModule.views_approval_resend', 'Send invitation email again?'); ?></h4>
+    <br>
+
+    <?= \yii\widgets\DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'email:email',
+            'created_at:datetime',
+            'updated_at:datetime',
+        ],
+    ]); ?>
+
+    <br>
+    <br>
+
+    <?= Html::a(Yii::t('AdminModule.views_approval_resend', 'Send invitation email'), Url::to(['/admin/pending-registrations/resend', 'id' => $model->id]), ['class' => 'btn btn-danger', 'data-method' => 'POST']); ?>
+    <?= Html::a(Yii::t('AdminModule.views_approval_resend', 'Cancel'), Url::to(['/admin/pending-registrations']), ['class' => 'btn btn-primary pull-right']); ?>
+</div>
