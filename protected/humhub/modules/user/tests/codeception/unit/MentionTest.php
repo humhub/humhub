9e8f50be9e55a4ed2d8150101050d1ab53a77517
Daniel Kesselberg <mail@danielkesselberg.de>
2018-01-11 19:29:39 +0100

    Code Style, Imports, PhpDoc

--- protected/humhub/modules/user/tests/codeception/unit/MentionTest.php
+++ protected/humhub/modules/user/tests/codeception/unit/MentionTest.php
@@ -2,19 +2,19 @@
 
 namespace humhub\modules\user\tests\codeception\unit;
 
-use tests\codeception\_support\HumHubDbTestCase;
-use Codeception\Specify;
+use humhub\modules\comment\models\Comment;
+use humhub\modules\post\models\Post;
 use humhub\modules\space\models\Space;
 use humhub\modules\user\models\Mentioning;
 use humhub\modules\user\notifications\Mentioned;
-use humhub\modules\comment\models\Comment;
-use humhub\modules\post\models\Post;
+use tests\codeception\_support\HumHubDbTestCase;
 
 class MentionTest extends HumHubDbTestCase
 {
 
-    use Specify;
-
+    /**
+     * @throws \yii\base\Exception
+     */
     public function testPostMention()
     {
         $this->becomeUser('User2');
@@ -29,11 +29,11 @@ class MentionTest extends HumHubDbTestCase
         $this->assertHasNotification(Mentioned::class, $post);
         $this->assertMailSent(1, 'Mentioned Notification');
     }
-    
+
     public function testMentionAuthor()
     {
         $this->becomeUser('User2');
-        
+
         // Mention Admin in Space 1 (Admin is author of post)
         $comment = new Comment([
             'message' => 'Hi @-u01e50e0d-82cd-41fc-8b0c-552392f5839c',
@@ -42,17 +42,17 @@ class MentionTest extends HumHubDbTestCase
         ]);
 
         $comment->save();
-        
+
         $this->assertHasNotification(Mentioned::class, $comment);
-        
+
         // We expect only the Mentioned mail
         $this->assertMailSent(1, 'Comment Notification Mail sent');
     }
-    
+
     public function testMentionNonAuthor()
     {
         $this->becomeUser('User2');
-        
+
         // Mention User1 in post
         $comment = new Comment([
             'message' => 'Hi @-u01e50e0d-82cd-41fc-8b0c-552392f5839d',
@@ -61,9 +61,9 @@ class MentionTest extends HumHubDbTestCase
         ]);
 
         $comment->save();
-        
+
         $this->assertHasNotification(Mentioned::class, $comment);
-        
+
         // Commented mail for Admin and Mentioned mail for User1
         $this->assertMailSent(2, 'Comment Notification Mail sent');
     }
