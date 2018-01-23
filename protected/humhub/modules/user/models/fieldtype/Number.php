fd00a4b64981380601fe8674b7340cc89467f60c
Carlos Ferreira <c.ferreira@acsiiweb.com>
2018-01-23 16:11:42 +0100

    Add use statement

--- protected/humhub/modules/user/models/fieldtype/Number.php
+++ protected/humhub/modules/user/models/fieldtype/Number.php
@@ -8,6 +8,7 @@
 
 namespace humhub\modules\user\models\fieldtype;
 
+use humhub\modules\user\models\Profile;
 use Yii;
 
 /**
@@ -68,7 +69,8 @@ class Number extends BaseType
                                 'class' => 'form-control',
                             ],
                         ]
-                    ]]);
+                    ]
+        ]);
     }
 
     /**
@@ -77,8 +79,8 @@ class Number extends BaseType
     public function save()
     {
         $columnName = $this->profileField->internal_name;
-        if (!\humhub\modules\user\models\Profile::columnExists($columnName)) {
-            $query = Yii::$app->db->getQueryBuilder()->addColumn(\humhub\modules\user\models\Profile::tableName(), $columnName, 'INT');
+        if (!Profile::columnExists($columnName)) {
+            $query = Yii::$app->db->getQueryBuilder()->addColumn(Profile::tableName(), $columnName, 'INT');
             Yii::$app->db->createCommand($query)->execute();
         }
 
