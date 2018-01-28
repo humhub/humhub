49428f4cc7eb0a5648f04775cdf2db7ea2830c04
Daniel Kesselberg <mail@danielkesselberg.de>
2018-01-28 22:55:26 +0100

    Try to fix php7.0 build

--- protected/humhub/modules/user/tests/codeception/acceptance/InviteCest.php
+++ protected/humhub/modules/user/tests/codeception/acceptance/InviteCest.php
@@ -8,24 +8,25 @@ class InviteCest
 {
     public function testUserInvite(AcceptanceTester $I)
     {
-        $I->wantTo('ensure that password recovery form works.');
-        
+        $I->wantTo('Ensure that invite form works.');
+
         $I->amUser();
         DirectoryMemberPage::openBy($I);
-        $I->click('Send invite');
 
+        $I->click('Send invite');
         $I->waitForText('Invite new people', null,'#globalModal');
-        
+
         $I->amGoingTo('invite an already existing user email');
         $I->fillField('#emails', 'user1@example.com');
         $I->click('Send invite');
         $I->expectTo('see an error message');
         $I->waitForText('user1@example.com is already registered!');
-        
+
         $I->amGoingTo('invite an non existing user email');
         $I->fillField('#emails', 'user1234@example.com');
         $I->click('Send invite');
         $I->expectTo('see a confirm message');
-        $I->seeSuccess();
+        $I->waitForText('User has been invited.');
+//        $I->seeSuccess();
     }
 }
