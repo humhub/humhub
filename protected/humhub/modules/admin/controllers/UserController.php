7dc10b345bc7c5e5b7a41a079e14eee5aec709a7
Lucas Bartholemy <lucas@bartholemy.com>
2018-01-28 22:16:34 +0100

    Fix no pagination in export data provider

--- protected/humhub/modules/admin/controllers/UserController.php
+++ protected/humhub/modules/admin/controllers/UserController.php
@@ -263,6 +263,7 @@ class UserController extends Controller
     {
         $searchModel = new UserSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+        $dataProvider->pagination = false;
 
         $exporter = new SpreadsheetExport([
             'dataProvider' => $dataProvider,
