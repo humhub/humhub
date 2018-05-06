f3a6056977e3c5c2ec18b797c651ab001e8b3b6c
Lucas Bartholemy <lucas@bartholemy.com>
2018-05-06 21:40:45 +0200

    Minor code cleanups

--- protected/humhub/widgets/LayoutAddons.php
+++ protected/humhub/widgets/LayoutAddons.php
@@ -24,22 +24,25 @@ class LayoutAddons extends BaseStack
      */
     public function init()
     {
-        if(!Yii::$app->request->isPjax) {
+        if (!Yii::$app->request->isPjax) {
             $this->addWidget(GlobalModal::class);
             $this->addWidget(GlobalConfirmModal::class);
 
-            if(Yii::$app->params['installed']) {
+            if (Yii::$app->params['installed']) {
                 $this->addWidget(\humhub\modules\tour\widgets\Tour::class);
                 $this->addWidget(\humhub\modules\admin\widgets\TrackingWidget::class);
             }
 
             $this->addWidget(LoaderWidget::class, ['show' => false, 'id' => "humhub-ui-loader-default"]);
             $this->addWidget(StatusBar::class);
-            $this->addWidget(BlueimpGallery::class);
-            $this->addWidget(MarkdownFieldModals::class);
+            if (Yii::$app->params['installed']) {
+                $this->addWidget(BlueimpGallery::class);
+                $this->addWidget(MarkdownFieldModals::class);
 
-            if (Yii::$app->params['enablePjax']) {
-                $this->addWidget(Pjax::class);
+                if (Yii::$app->params['enablePjax']) {
+                    $this->addWidget(Pjax::class);
+                }
+
             }
         }
         parent::init();
