cd87189e39f37a6d7d485499939982875b79bf1c
HumHub Translations <info@humhub.com>
2018-05-23 15:25:37 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/admin/messages/it/views_setting_index.php
+++ protected/humhub/modules/admin/messages/it/views_setting_index.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  '<strong>Confirm</strong> image deletion' => '',
+  '<strong>Confirm</strong> image deletion' => '<strong>Conferma</strong> l\'eliminazione dell\'immagine',
   'Dashboard' => 'Bacheca',
   'Delete' => 'Cancella',
   'E.g. http://example.com/humhub' => 'Esempio: http://example.com/humhub',
   'Friendship' => 'Amicizia',
-  'New users will automatically be added to these space(s).' => '',
+  'New users will automatically be added to these space(s).' => 'I nuovi utenti saranno automaticamente aggiunti a questo spazio/spazi.',
   'Save' => 'Salva',
   'Wall entry layout' => '',
-  'You\'re using no logo at the moment. Upload your logo now.' => '',
+  'You\'re using no logo at the moment. Upload your logo now.' => 'Non stai usando nessun logo al momento. Carica il tuo logo ora.',
 );
