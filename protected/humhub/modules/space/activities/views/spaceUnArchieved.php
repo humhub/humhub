d27e1976168a8ff3f55b1667cd67560b592dbbc9
haykhakobyan94 <gaikakopian94@gmail.com>
2018-06-13 15:05:08 +0400

    fixing errors

--- protected/humhub/modules/space/activities/views/spaceUnArchieved.php
+++ protected/humhub/modules/space/activities/views/spaceUnArchieved.php
@@ -2,12 +2,11 @@
 
 use yii\helpers\Html;
 use humhub\libs\Helpers;
-use humhub\modules\content\components\ContentContainerController;
 
 echo Yii::t('ActivityModule.views_activities_ActivitySpaceMemberAdded', '%spaceName% has been unarchived', [
     '%spaceName%' => '<strong>' . Html::encode(Helpers::truncateText($source->name, 40)) . '</strong>'
 ]);
-?>
+
 
 
 
