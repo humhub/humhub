9dc691fc82d940355f1dbec13cfcd7667bd3838b
Lucas Bartholemy <lucas@bartholemy.com>
2018-06-27 11:48:56 +0200

    Chg: Renamed information cronjob section to background jobs and added queue status

---
+++ protected/humhub/modules/admin/views/information/background-jobs.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @var string $driverName
+ * @var int|null $lastRunHourly
+ * @var int|null $lastRunDaily
+ * @var int|null $waitingJobs
+ * @var int|null $delayedJobs
+ * @var int|null $doneJobs
+ * @var int|null $reservedJobs
+ */
+
+if (empty($lastRunHourly)) {
+    $lastRunHourly = "<span style='color:red'>" . Yii::t('AdminModule.information', 'Never') . "</span>";
+} else {
+    $lastRunHourly = Yii::$app->formatter->asRelativeTime($lastRunHourly);
+}
+if (empty($lastRunDaily)) {
+    $lastRunDaily = "<span style='color:red'>" . Yii::t('AdminModule.information', 'Never') . "</span>";
+} else {
+    $lastRunDaily = Yii::$app->formatter->asRelativeTime($lastRunDaily);
+}
+?>
+<div class="row">
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <?= Yii::t('AdminModule.information', '<strong>CronJob</strong> Status'); ?>
+            </div>
+            <div class="panel-body">
+                <strong><?= Yii::t('AdminModule.information', 'Last run (hourly):'); ?></strong><br> <?= $lastRunHourly; ?>
+                <br/><br/>
+                <strong><?= Yii::t('AdminModule.information', 'Last run (daily):'); ?></strong><br> <?= $lastRunDaily; ?>
+                <br/><br/>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6">
+        <div class="panel">
+            <div class="panel-heading">
+                <?= Yii::t('AdminModule.information', '<strong>Queue</strong> Status'); ?>
+            </div>
+            <div class="panel-body">
+                <strong><?= Yii::t('AdminModule.information', 'Driver'); ?></strong><br/>
+                <?= $driverName; ?><br/>
+                <br/>
+
+                <strong><?= Yii::t('AdminModule.information', 'Waiting'); ?></strong><br/>
+                <?= $waitingJobs ?><br/>
+                <br/>
+
+                <strong><?= Yii::t('AdminModule.information', 'Delayed'); ?></strong><br/>
+                <?= $delayedJobs ?><br/>
+                <br/>
+
+                <strong><?= Yii::t('AdminModule.information', 'Reserved'); ?></strong><br/>
+                <?= $reservedJobs ?><br/>
+                <br/>
+
+                <strong><?= Yii::t('AdminModule.information', 'Done'); ?></strong><br/>
+                <?= $doneJobs ?><br/>
+                <br/>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<p><?= Yii::t('AdminModule.information', 'Please refer to the documentation to setup the cronjobs and queue workers.'); ?></p>
+
--- protected/humhub/modules/queue/driver/Redis.php
+++ protected/humhub/modules/queue/driver/Redis.php
@@ -8,6 +8,7 @@
 
 namespace humhub\modules\queue\driver;
 
+use humhub\modules\queue\interfaces\QueueInfoInterface;
 use yii\queue\redis\Queue;
 
 /**
@@ -16,7 +17,38 @@ use yii\queue\redis\Queue;
  * @since 1.2
  * @author Luke
  */
-class Redis extends Queue
+class Redis extends Queue implements QueueInfoInterface
 {
-    
+    /**
+     * @return int|null the number of waiting jobs in the queue
+     */
+    public function getWaitingJobCount()
+    {
+        return (int)$this->redis->llen($this->channel . ".waiting");
+    }
+
+    /**
+     * @return int|null the number of delayed jobs in the queue
+     */
+    public function getDelayedJobCount()
+    {
+        return (int)$this->redis->zcount($this->channel . ".delayed", '-inf', '+inf');
+    }
+
+    /**
+     * @return int|null the number of reserved jobs in the queue
+     */
+    public function getReservedJobCount()
+    {
+        return (int)$this->redis->zcount($this->channel . ".reserved", '-inf', '+inf');
+    }
+
+    /**
+     * @return int|null the number of done jobs in the queue
+     */
+    public function getDoneJobCount()
+    {
+        $total = $this->redis->get($this->channel . ".message_id");
+        return $total - $this->getWaitingJobCount() - $this->getDelayedJobCount() - $this->getReservedJobCount();
+    }
 }
