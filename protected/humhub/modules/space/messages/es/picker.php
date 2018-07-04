12603d83f0e627f5d646900d06eb592d2cba00f8
buddh4 <mail@jharrer.de>
2018-07-04 12:48:46 +0200

    Fixed spanish translation

--- protected/humhub/modules/space/messages/es/picker.php
+++ protected/humhub/modules/space/messages/es/picker.php
@@ -1,4 +1,4 @@
 <?php
 return array (
-  'Add {n,plural,=1{space} other{spaces}}' => 'Añadir {n,plural,=1{space} otros{spaces}}',
+  'Add {n,plural,=1{space} other{spaces}}' => 'Añadir {n,plural,=1{space} other{spaces}}',
 );
