fc950e6036873259b1ac8efdefe0f7b2bef4eb45
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-07-05 08:08:18 +0900

    fix broken collapsing widget

--- protected/humhub/modules/activity/widgets/views/activityStream.php
+++ protected/humhub/modules/activity/widgets/views/activityStream.php
@@ -19,8 +19,10 @@ use yii\helpers\Html;
 <div class="panel panel-default panel-activities" id="panel-activities">
    <?= \humhub\widgets\PanelMenu::widget(['id' => 'panel-activities']); ?>
     <div class="panel-heading"><?= Yii::t('ActivityModule.widgets_views_activityStream', '<strong>Latest</strong> activities'); ?></div>
+   <div class="panel-body">
     <?= Html::beginTag('div', $options) ?>
         <ul id="activityContents" class="media-list activities" data-stream-content>
         </ul>
       </div>
     <?= Html::endTag('div')?>
+</div>
