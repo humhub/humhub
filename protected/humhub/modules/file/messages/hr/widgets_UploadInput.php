fc7341dfa1d2d4da9c339b2b1f03b3d799ff2ac1
buddh4 <mail@jharrer.de>
2018-07-25 15:07:12 +0200

    Fix invalid translation syntax used in croatian language

--- protected/humhub/modules/file/messages/hr/widgets_UploadInput.php
+++ protected/humhub/modules/file/messages/hr/widgets_UploadInput.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'Sorry, you can only upload up to {n,plural,=1{# file} other{# files}} at once.' => 'Nažalost, možete učitati do {n,plural,=1{# file} other{# files}} odjednom.',
-  'This upload field only allows a maximum of {n,plural,=1{# file} other{# files}}.' => 'Ovo polje za prijenos dopušta samo najviše {n,plural,=1{# file} drugi{# files}}.',
+  'This upload field only allows a maximum of {n,plural,=1{# file} other{# files}}.' => 'Ovo polje za prijenos dopušta samo najviše {n,plural,=1{# file} other{# files}}.',
 );
