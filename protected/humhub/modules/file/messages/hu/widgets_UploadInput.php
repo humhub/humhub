b78c7174adf1c41ac9fa7659c2471de467b7a483
buddh4 <mail@jharrer.de>
2018-07-25 15:25:11 +0200

    Fixed hungarian translation sytax error

--- protected/humhub/modules/file/messages/hu/widgets_UploadInput.php
+++ protected/humhub/modules/file/messages/hu/widgets_UploadInput.php
@@ -1,5 +1,5 @@
 <?php
 return array (
   'Sorry, you can only upload up to {n,plural,=1{# file} other{# files}} at once.' => '',
-  'This upload field only allows a maximum of {n,plural,=1{# file} other{# files}}.' => 'Ez a feltöltési mező csak {n,plural,=1{# file} engedélyez maximum vagy {# files}}',
+  'This upload field only allows a maximum of {n,plural,=1{# file} other{# files}}.' => 'Ez a feltöltési mező csak engedélyez maximum {n,plural,=1{# file} other{# files}}',
 );
