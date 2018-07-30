945b63c371e1a0d2530ebf9b9a4c4d65ca5dc593
Lucas Bartholemy <lucas@bartholemy.com>
2018-07-30 15:45:18 +0200

    Prepare Beta.3 Release

--- protected/humhub/config/common.php
+++ protected/humhub/config/common.php
@@ -13,7 +13,7 @@ Yii::setAlias('@themes', '@webroot/themes');
 
 $config = [
     'name' => 'HumHub',
-    'version' => '1.3.0-beta.2',
+    'version' => '1.3.0-beta.3',
     'basePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
     'bootstrap' => ['log', 'humhub\components\bootstrap\ModuleAutoLoader', 'queue', 'humhub\modules\ui\view\bootstrap\ThemeLoader'],
     'sourceLanguage' => 'en',
