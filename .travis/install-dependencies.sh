e2002ab4aacb6ac47dbba9b4c47fc7c83230100c
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-08 20:24:46 +0200

    :construction_worker: Cleanup travis.yml

---
+++ .travis/install-dependencies.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# -e = exit when one command returns != 0, -v print each command before executing
+set -ev
+
+# Install chomedriver
+curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.40/chromedriver_linux64.zip \
+    && unzip -o -d $HOME chromedriver_linux64.zip \
+	&& chmod +x $HOME/chromedriver
+
+# Install composer package
+if [ ${TRAVIS_BRANCH} = "v1.2" ]; then
+    composer global require fxp/composer-asset-plugin
+fi
+
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
