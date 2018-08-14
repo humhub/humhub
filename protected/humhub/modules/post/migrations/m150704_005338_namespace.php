642d24fa6267d53f7f89f65f57a04fd8a14c6f07
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-14 21:33:28 +0200

    Revert: Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/post/migrations/m150704_005338_namespace.php
+++ protected/humhub/modules/post/migrations/m150704_005338_namespace.php
@@ -8,7 +8,7 @@ class m150704_005338_namespace extends Migration
 {
     public function up()
     {
-        $this->renameClass('Post', Post::className());
+        $this->renameClass('Post', Post::class);
     }
 
     public function down()
