422e6a6d9e26e5b7baa86da09f1adc610accb44e
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-24 17:08:00 +0200

    Only trigger build for master / pr = false and set matrix to master

--- .travis/trigger-modules.sh
+++ .travis/trigger-modules.sh
@@ -6,10 +6,16 @@ set -ev
 # change to build directory
 cd ${TRAVIS_BUILD_DIR}
 
-curl -X POST \
-  https://api.travis-ci.org/repo/humhub%2Fhumhub-modules-calendar/requests \
-  -H 'Accept: application/json' \
-  -H 'Authorization: token '${AUTH_TOKEN} \
-  -H 'Content-Type: application/json' \
-  -H 'Travis-API-Version: 3' \
-  -d '{"request": {"branch": "master"}}'
+# build only when branch master and no pull request
+if [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST} = "false" ]; then
+
+    # trigger build for calendar
+    curl -X POST \
+        https://api.travis-ci.org/repo/humhub%2Fhumhub-modules-calendar/requests \
+        -H 'Accept: application/json' \
+        -H 'Authorization: token '${AUTH_TOKEN} \
+        -H 'Content-Type: application/json' \
+        -H 'Travis-API-Version: 3' \
+        -d '{"request":{"branch":"master","config":{"env":{"matrix":["HUMHUB_VERSION=master"]}}}}'
+
+fi
