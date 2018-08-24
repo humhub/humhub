619ebfd05e8bf921734afd08acc90ab84c69b759
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-24 18:34:13 +0200

    Chg: Trigger related modules only for php 7.2

--- .travis/trigger-modules.sh
+++ .travis/trigger-modules.sh
@@ -3,16 +3,16 @@
 # -e = exit when one command returns != 0, -v print each command before executing
 set -ev
 
-# build only when branch master and no pull request
-if [ ${TRAVIS_BRANCH} = "master" ] && [ ${TRAVIS_PULL_REQUEST} = "false" ]; then
-
-    # trigger build for calendar
-    curl -X POST \
-        https://api.travis-ci.org/repo/humhub%2Fhumhub-modules-calendar/requests \
-        -H 'Accept: application/json' \
-        -H 'Authorization: token '${AUTH_TOKEN} \
-        -H 'Content-Type: application/json' \
-        -H 'Travis-API-Version: 3' \
-        -d '{"request":{"branch":"master","config":{"env":{"global":["HUMHUB_PATH=/opt/humhub"],"matrix":["HUMHUB_VERSION=master"]}}}}'
-
+# only trigger related modules for php 7.2 || early exit when pull request || early exit when not master
+if [ "${TRAVIS_PHP_VERSION}" != "7.2" ] || [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
+    exit 0;
 fi
+
+# trigger build for calendar
+curl -X POST \
+    https://api.travis-ci.org/repo/humhub%2Fhumhub-modules-calendar/requests \
+    -H 'Accept: application/json' \
+    -H 'Authorization: token '${AUTH_TOKEN} \
+    -H 'Content-Type: application/json' \
+    -H 'Travis-API-Version: 3' \
+    -d '{"request":{"branch":"master","config":{"merge_mode":"deep_merge","env":{"matrix":["HUMHUB_VERSION=master"]}}}}'
