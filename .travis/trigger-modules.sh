f59410deb6441d6860f0eafc9eb040e3ff3c78a3
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-24 21:24:38 +0200

    Enh: Dont run install before deploy stage

--- .travis/trigger-modules.sh
+++ .travis/trigger-modules.sh
@@ -3,13 +3,13 @@
 # -e = exit when one command returns != 0, -v print each command before executing
 set -ev
 
-# early exit when pull request || early exit when not master
+# early exit when pull request || when not master
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] || [ "${TRAVIS_BRANCH}" != "master" ]; then
     exit 0;
 fi
 
 # trigger build for calendar
-curl -X POST \
+curl -s -X POST \
     https://api.travis-ci.org/repo/humhub%2Fhumhub-modules-calendar/requests \
     -H 'Accept: application/json' \
     -H 'Authorization: token '${AUTH_TOKEN} \
