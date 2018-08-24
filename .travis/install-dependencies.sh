7eea47973dadf61e16b3aaa3535f93ec4e2d4ce4
Daniel Kesselberg <mail@danielkesselberg.de>
2018-08-24 15:56:04 +0200

    Install travis-cli

--- .travis/install-dependencies.sh
+++ .travis/install-dependencies.sh
@@ -3,12 +3,17 @@
 # -e = exit when one command returns != 0, -v print each command before executing
 set -ev
 
+# Install travis cli
+gem install travis -v 1.8.9 --no-rdoc --no-ri
+
 # Install chomedriver
 curl -s -L -o chromedriver_linux64.zip https://chromedriver.storage.googleapis.com/2.41/chromedriver_linux64.zip \
     && unzip -o -d $HOME chromedriver_linux64.zip \
 	&& chmod +x $HOME/chromedriver
 
+# Install node
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+# Update npm
 npm install -g npm@latest
