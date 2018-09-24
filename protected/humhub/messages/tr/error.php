5c35c1edbca1ea73f0dd908bce63af18c3da9940
HumHub Translations <info@humhub.com>
2018-09-24 13:13:05 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/messages/tr/error.php
+++ protected/humhub/messages/tr/error.php
@@ -6,7 +6,7 @@ return array (
   'Login required for this section.' => 'Bu bölüm için giriş gerekli.',
   'You are not allowed to perform this action.' => 'Bu eylemi gerçekleştirmek için izin gereklidir.',
   'You are not permitted to access this section.' => 'Bu bölüme erişme izniniz yok.',
-  'You need admin permissions to access this section.' => '',
-  'Your user account has not been approved yet, please try again later or contact a network administrator.' => '',
-  'Your user account is inactive, please login with an active account or contact a network administrator.' => '',
+  'You need admin permissions to access this section.' => 'Bu bölüme erişmek için yönetici izinlerine ihtiyacınız var.',
+  'Your user account has not been approved yet, please try again later or contact a network administrator.' => 'Kullanıcı hesabınız henüz onaylanmadı. Lütfen daha sonra tekrar deneyin veya bir ağ yöneticisine başvurun.',
+  'Your user account is inactive, please login with an active account or contact a network administrator.' => 'Kullanıcı hesabınız etkin değil, lütfen aktif bir hesapla giriş yapın veya bir ağ yöneticisine başvurun.',
 );
