6827feaa5e1869c5f554a8cdb85f032e812382f3
buddh4 <mail@jharrer.de>
2018-10-07 20:23:48 +0200

    Added comment unit tests

--- protected/humhub/modules/comment/tests/codeception/unit/CommentTest.php
+++ protected/humhub/modules/comment/tests/codeception/unit/CommentTest.php
@@ -2,6 +2,7 @@
 
 namespace tests\codeception\unit\modules\comment\components;
 
+use humhub\modules\user\models\User;
 use tests\codeception\_support\HumHubDbTestCase;
 use Codeception\Specify;
 use humhub\modules\post\models\Post;
@@ -31,6 +32,102 @@ class CommentTest extends HumHubDbTestCase
         
         $this->assertNotNull(\humhub\modules\activity\models\Activity::findOne(['object_model' => Comment::class, 'object_id' => $comment->id]));
         $this->assertNotNull(\humhub\modules\notification\models\Notification::findOne(['source_class' => Comment::class, 'source_pk' => $comment->id]));
+    }
+
+    public function testDeleteUser()
+    {
+        $this->becomeUser('User2');
+
+        $comment = new Comment([
+            'message' => 'User2 comment!',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]);
+
+        $comment->save();
+
+        $user2 = User::findOne(['id' => 3]);
+        $user2->delete();
+
+        $this->assertNull(Comment::findOne(['id' => $comment->id]));
+    }
+
+    public function testDeleteCommentedContent()
+    {
+        $this->becomeUser('User2');
+
+        $comment = new Comment([
+            'message' => 'User2 comment!',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]);
+
+        $comment->save();
+
+        $post = Post::findOne(['id' => 11]);
+        $post->delete();
+
+        $this->assertNull(Comment::findOne(['id' => $comment->id]));
+    }
+
+    public function testGetCommentLimited()
+    {
+        $this->becomeUser('User2');
+
+        (new Comment([
+            'message' => 'Test comment1',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        (new Comment([
+            'message' => 'Test comment2',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        (new Comment([
+            'message' => 'Test comment3',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        $comments = Comment::GetCommentsLimited(Post::class, 11, 2);
+        $this->assertCount(2, $comments);
+        $this->assertEquals('Test comment2', $comments[0]->message);
+        $this->assertEquals('Test comment3', $comments[1]->message);
+
+    }
+
+    public function testGetCommentCount()
+    {
+        $this->becomeUser('User2');
+
+        $count = Comment::GetCommentCount(Post::class, 11);
+        $this->assertEquals(0, $count);
+
+        Comment::flushCommentCache(Post::class, 11);
+
+        (new Comment([
+            'message' => 'Test comment1',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        (new Comment([
+            'message' => 'Test comment2',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        (new Comment([
+            'message' => 'Test comment3',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]))->save();
+
+        $count = Comment::GetCommentCount(Post::class, 11);
+        $this->assertEquals(3, $count);
     }
 
 }
