cb503c20ae6a3fbbf1261c338774e41978ede5b8
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-11-07 08:13:13 +0900

    Update views_setting_statistic.php

--- protected/humhub/modules/admin/messages/hr/views_setting_statistic.php
+++ protected/humhub/modules/admin/messages/hr/views_setting_statistic.php
@@ -1,5 +1,6 @@
 <?php
-return array (
+
+return [
   'Save' => 'Spremi',
-  'You can add an statistics HTML code snippet - which will added to all rendered pages.' => 'Možete dodati statistiku isječka HTML koda - koji će se dodati na sve prikazane stranice.',
-);
+  'You can add statistic HTML code snippets - that are added to all rendered pages.' => '',
+];
