e8d4b8dc087fadb23ded34d8b858bfd724237334
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-11-07 08:26:06 +0900

    Update views_setting_statistic.php

--- protected/humhub/modules/admin/messages/pt_br/views_setting_statistic.php
+++ protected/humhub/modules/admin/messages/pt_br/views_setting_statistic.php
@@ -1,5 +1,6 @@
 <?php
-return array (
+
+return [
   'Save' => 'Salvar',
-  'You can add an statistics HTML code snippet - which will added to all rendered pages.' => 'Você pode incluir um código HTML de Rastreamento que será adiconado à todas as páginas.',
-);
+  'You can add statistic HTML code snippets - that are added to all rendered pages.' => '',
+];
