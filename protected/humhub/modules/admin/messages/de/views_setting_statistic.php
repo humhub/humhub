470c6bd8479cdc2a7f7f62a65509e175d34c3e5b
Sarah Tsumayoi <Felli@users.noreply.github.com>
2018-11-22 15:08:31 +0900

    Update views_setting_statistic.php

--- protected/humhub/modules/admin/messages/de/views_setting_statistic.php
+++ protected/humhub/modules/admin/messages/de/views_setting_statistic.php
@@ -2,5 +2,5 @@
 
 return [
   'Save' => 'Speichern',
-  'You can add statistic HTML code snippets - that are added to all rendered pages.' => '',
+  'You can add statistic HTML code snippets - that are added to all rendered pages.' => 'Sie können statistische HTML-Codeausschnitte hinzufügen, die allen gerenderten Seiten hinzugefügt werden.',
 ];
