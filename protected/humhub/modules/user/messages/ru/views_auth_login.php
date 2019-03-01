ef81d2bc8b570805f11a18c1bfcfc81f5093009f
hagalaz <newme.lego@gmail.com>
2019-03-01 23:24:22 +0300

    Update views_auth_login.php

--- protected/humhub/modules/user/messages/ru/views_auth_login.php
+++ protected/humhub/modules/user/messages/ru/views_auth_login.php
@@ -22,7 +22,7 @@ return [
     '<strong>Please</strong> sign in' => '<strong>Войти</strong>',
     '<strong>Sign</strong> up' => '<strong>Зарегистрироваться</strong>',
     'Create a new one.' => 'Создать новый',
-    'Don\'t have an account? Join the network by entering your e-mail address.' => 'Нет аккаунта? Присоеденитесь к сети с помощью e-mail',
+    'Don\'t have an account? Join the network by entering your e-mail address.' => 'Нет аккаунта? Присоединитесь к сети с помощью e-mail',
     'Forgot your password?' => 'Забыли пароль?',
     'If you\'re already a member, please login with your username/email and password.' => 'Если вы уже являетесь зарегистрированным пользователем, пожалуйста введите свое имя пользователя и пароль.',
     'Login' => 'Авторизация',
