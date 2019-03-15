697799d2894673d5c4dbe18828d97f4556203a45
Lucas Bartholemy <lucas@bartholemy.com>
2019-03-15 12:25:05 +0100

    Added compatibility layer class

---
+++ protected/humhub/modules/user/authclient/ZendLdapClient.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2019 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\user\authclient;
+
+
+use humhub\modules\ldap\authclient\LdapAuth;
+
+/**
+ * Class ZendLdapClient
+ *
+ * @deprecated since 1.4
+ * @see LdapAuth
+ * @package humhub\modules\user\authclient
+ */
+class ZendLdapClient extends LdapAuth
+{
+
+}
