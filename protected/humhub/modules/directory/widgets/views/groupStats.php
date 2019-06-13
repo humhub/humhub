81616a6ad847e1f9cb1a75fee4917bb1154ce5c5
Sarah Tsumayoi <Felli@users.noreply.github.com>
2019-06-13 06:16:03 -0400

    Fix: Progress Color changing

--- protected/humhub/modules/directory/widgets/views/groupStats.php
+++ protected/humhub/modules/directory/widgets/views/groupStats.php
@@ -5,22 +5,21 @@ use yii\helpers\Html;
 humhub\modules\directory\assets\DirectoryAsset::register($this);
 ?>
 
-
 <div class="panel panel-default" id="groups-statistics-panel">
 
     <!-- Display panel menu widget -->
-    <?= humhub\widgets\PanelMenu::widget(['id' => 'groups-statistics-panel']); ?>
+<?= humhub\widgets\PanelMenu::widget(['id' => 'groups-statistics-panel']); ?>
 
     <div class="panel-heading">
-        <?= Yii::t('DirectoryModule.base', '<strong>Group</strong> stats'); ?>
+<?= Yii::t('DirectoryModule.base', '<strong>Group</strong> stats'); ?>
     </div>
     <div class="panel-body">
         <div class="knob-container" style="text-align: center; opacity: 0;">
             <strong><?= Yii::t('DirectoryModule.base', 'Total groups'); ?></strong><br><br>
 
-            <input id="groups-total" class="knob" data-width="120" data-height="140" data-displayPrevious="true"
-                   data-readOnly="true"
-                   data-fgcolor="<?= $this->theme->variable('primary'); ?>" data-skin="tron"
+            <input id="groups-total" class="knob" data-width="120" data-height="140" data-displayPrevious="true" data-readOnly="true"
+                   data-fgcolor="<?= $this->theme->variable('primary'); ?>"
+                   data-skin="tron"
                    data-thickness=".2" value="<?= $statsTotalGroups; ?>"
                    data-max="<?= $statsTotalGroups; ?>"
                    style="font-size: 25px !important; margin-top: 44px !important;">
@@ -29,10 +28,9 @@ humhub\modules\directory\assets\DirectoryAsset::register($this);
         <hr>
 
         <div class="knob-container" style="text-align: center; opacity: 0;">
-            <strong><?php echo Yii::t('DirectoryModule.base', 'Average members'); ?></strong><br><br>
+            <strong><?= Yii::t('DirectoryModule.base', 'Average members'); ?></strong><br><br>
 
-            <input id="group-average" class="knob" data-width="120" data-height="140" data-displayPrevious="true"
-                   data-readOnly="true"
+            <input id="group-average" class="knob" data-width="120" data-height="140" data-displayPrevious="true" data-readOnly="true"
                    data-fgcolor="<?= $this->theme->variable('primary'); ?>"
                    data-skin="tron"
                    data-thickness=".2" value="<?= $statsAvgMembers; ?>"
@@ -42,8 +40,7 @@ humhub\modules\directory\assets\DirectoryAsset::register($this);
         <hr>
 
         <div style="text-align: center;">
-            <strong><?= Yii::t('DirectoryModule.base', 'Top Group'); ?>
-                :</strong> <?= Html::encode($statsTopGroup->name); ?>
+            <strong><?= Yii::t('DirectoryModule.base', 'Top Group'); ?>:</strong> <?= Html::encode($statsTopGroup->name); ?>
         </div>
     </div>
 </div>
