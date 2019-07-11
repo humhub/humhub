387b78a43fbad16bbac9b45812caa46631e2ec67
craxrev <mortadha.ghanmi56@gmail.com>
2019-07-11 17:02:28 +0100

    first attempt redesigning the top-bar

---
+++ themes/Coinsence/views/admin/setting/design.php
@@ -0,0 +1,69 @@
+<?php
+
+use yii\widgets\ActiveForm;
+use yii\helpers\Url;
+use humhub\compat\CHtml;
+
+\humhub\modules\admin\assets\AdminAsset::register($this);
+
+$this->registerJsConfig('admin', [
+    'text' => [
+        'confirm.deleteLogo.header' => Yii::t('AdminModule.views_setting_index', '<strong>Confirm</strong> image deletion'),
+        'confirm.deleteLogo.body' => Yii::t('UserModule.views_setting_index', 'Do you really want to delete your logo image?'),
+        'confirm.deleteLogo.confirm' => Yii::t('AdminModule.views_setting_index', 'Delete')
+    ]
+]);
+?>
+
+<div class="panel-body">
+    <h4><?= Yii::t('AdminModule.setting', 'Appearance Settings'); ?></h4>
+    <div class="help-block">
+        <?= Yii::t('AdminModule.setting', 'These settings refer to the appearance of your social network.'); ?>
+    </div>
+
+    <br>
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+
+    <?= $form->field($model, 'theme')->dropDownList($model->getThemes()); ?>
+
+    <?= $form->field($model, 'paginationSize'); ?>
+
+    <?= $form->field($model, 'displayName')->dropDownList(['{username}' => Yii::t('AdminModule.views_setting_design', 'Username (e.g. john)'), '{profile.firstname} {profile.lastname}' => Yii::t('AdminModule.views_setting_design', 'Firstname Lastname (e.g. John Doe)')]); ?>
+
+    <?= $form->field($model, 'spaceOrder')->dropDownList(['0' => Yii::t('AdminModule.views_setting_design', 'Alphabetical'), '1' => Yii::t('AdminModule.views_setting_design', 'Last visit')]); ?>
+
+    <?= $form->field($model, 'dateInputDisplayFormat')->dropDownList([
+        '' => Yii::t('AdminModule.views_setting_design', 'Auto format based on user language - Example: {example}', ['{example}' => Yii::$app->formatter->asDate(time(), 'short')]),
+        'php:d/m/Y' => Yii::t('AdminModule.views_setting_design', 'Fixed format (dd/mm/yyyy) - Example: {example}', ['{example}' => Yii::$app->formatter->asDate(time(), 'php:d/m/Y')]),
+    ]);
+    ?>
+    <strong><?= Yii::t('AdminModule.views_setting_index', 'Wall entry layout'); ?></strong>
+    <br>
+    <br>
+    <?= $form->field($model, 'horImageScrollOnMobile')->checkbox(); ?>
+
+    <?= $form->field($model, 'logo')->fileInput(['id' => 'admin-logo-file-upload', 'data-action-change' => 'admin.changeLogo', 'style' => 'display: none', 'name' => 'logo[]']); ?>
+
+    <div class="well">
+        <div class="image-upload-container" id="logo-upload">
+            <img class="img-rounded-logo" id="logo-image" src="<?= ($logo->hasImage()) ? $logo->getUrl() : '' ?>" data-src="holder.js/140x140"
+                 alt="<?= Yii::t('AdminModule.views_setting_index', "You're using no logo at the moment. Upload your logo now."); ?>"
+                 style="max-height: 50px;">
+
+            <div class="image-upload-buttons" id="logo-upload-buttons" style="display: block;">
+                <a id="admin-logo-upload-button" href="#"  class="btn btn-info btn-sm"><i class="fa fa-cloud-upload"></i></a>
+
+                <a id="admin-delete-logo-image" href="#" style="<?= ($logo->hasImage()) ? '' : 'display:none' ?>" class="btn btn-danger btn-sm"
+                    data-action-click="admin.deletePageLogo"
+                    data-action-url="<?= Url::to(['/admin/setting/delete-logo-image']) ?>" ><i class="fa fa-times"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+    <?= CHtml::submitButton(Yii::t('AdminModule.views_setting_design', 'Save'), ['class' => 'btn btn-primary', 'data-ui-loader' => ""]); ?>
+
+    <?= \humhub\widgets\DataSaved::widget(); ?>
+    <?php ActiveForm::end(); ?>
+</div>
---
+++ themes/Coinsence/views/layouts/main.php
@@ -0,0 +1,72 @@
+<?php
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+\humhub\assets\AppAsset::register($this);
+
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
+use yii\helpers\Url; ?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+    <head>
+        <title><?= strip_tags($this->pageTitle); ?></title>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <?php $this->head() ?>
+        <?= $this->render('head'); ?>
+    </head>
+    <body>
+        <?php $this->beginBody() ?>
+
+        <!-- start: first top navigation bar -->
+        <div id="topbar-first" class="topbar">
+            <div class="container-o">
+                <div class="topbar-brand hidden-xs">
+                    <?= \humhub\widgets\SiteLogo::widget(); ?>
+                </div>
+
+                <div class="topbar-actions pull-right">
+                    <?= \humhub\modules\user\widgets\AccountTopMenu::widget(); ?>
+                </div>
+
+                <div class="notifications pull-right">
+                    <?= \humhub\widgets\NotificationArea::widget(); ?>
+                </div>
+
+                <div class="search">
+                    <?php $form = ActiveForm::begin(['action' => Url::to(['/search']), 'method' => 'GET']); ?>
+                    <div class="form-group form-group-search">
+                        <?= Html::textInput('SearchForm[keyword]', null, ['placeholder' => Yii::t('SearchModule.views_search_index', 'Search for user, spaces and content'),
+                            'title' => Yii::t('SearchModule.views_search_index', 'Search for user, spaces and content'), 'class' => 'form-control', 'id' => 'search-input-field']) ?>
+                        <?php echo Html::submitButton('<i class="fa fa-search"></i>', ['class' => 'btn btn-default btn-sm form-button-search', 'data-ui-loader' => '']); ?>
+                    </div>
+                    <?php ActiveForm::end(); ?>
+                </div>
+
+
+            </div>
+        </div>
+        <!-- end: first top navigation bar -->
+
+        <!-- start: second top navigation bar -->
+        <div id="topbar-second" class="topbar">
+            <div class="container-o">
+                <ul class="nav" id="top-menu-nav">
+                    <!-- load space chooser widget -->
+                    <?= \humhub\modules\space\widgets\Chooser::widget(); ?>
+
+                    <!-- load navigation from widget -->
+                    <?= \humhub\widgets\TopMenu::widget(); ?>
+                </ul>
+            </div>
+        </div>
+        <!-- end: second top navigation bar -->
+
+        <?= $content; ?>
+
+        <?php $this->endBody() ?>
+    </body>
+</html>
+<?php $this->endPage() ?>
