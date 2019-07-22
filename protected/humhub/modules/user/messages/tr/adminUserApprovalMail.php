ed490d31c67b767863041f11602896dffb268d77
HumHub Translations <info@humhub.com>
2019-07-22 16:18:04 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/user/messages/tr/adminUserApprovalMail.php
+++ protected/humhub/modules/user/messages/tr/adminUserApprovalMail.php
@@ -2,6 +2,6 @@
 return array (
   'Hello {displayName},' => 'Merhaba {displayName},',
   'New user needs approval' => 'Yeni kullanıcı onay bekliyor',
-  'Please click on the link below to view request:' => 'Istek görüntülemek için aşağıdaki linke tıklayınız:',
+  'Please click on the link below to view request:' => 'İstek görüntülemek için aşağıdaki linke tıklayınız:',
   'a new user {displayName} needs approval.' => 'yeni bir kullanıcı {displayName} onayı gerekiyor.',
 );
