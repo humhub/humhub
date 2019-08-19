d2c6897ddea44c2450ad6fa13bad069061fc1907
Lucas Bartholemy <lucas@bartholemy.com>
2019-08-19 15:16:34 +0200

    fix sw.js url

--- protected/humhub/modules/web/config.php
+++ protected/humhub/modules/web/config.php
@@ -6,7 +6,7 @@ return [
     'class' => \humhub\modules\web\Module::class,
     'isCoreModule' => true,
     'urlManagerRules' => [
-        'sw.js' => 'web/service-worker/index',
+        'sw.js' => 'web/pwa-service-worker/index',
         'manifest.json' => 'web/pwa-manifest/index',
         'offline.pwa.html' => 'web/pwa-offline/index'
     ],
