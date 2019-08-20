5f5c692e24f2550d8b1f55336d2bbad92d728d0f
HumHub Translations <info@humhub.com>
2019-08-20 18:46:07 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/installer/messages/fi/forms_SecurityForm.php
+++ protected/humhub/modules/installer/messages/fi/forms_SecurityForm.php
@@ -1,8 +1,8 @@
 <?php
 return array (
-  'Allow access for non-registered users to public content (guest access)' => 'Salli rekisteröimättömien käyttäjien pääsy julkiseen sisältöön (vieraiden pääsy)',
+  'Allow access for non-registered users to public content (guest access)' => 'Salli rekisteröimättömien käyttäjien pääsy julkiseen sisältöön (ulkopuolisten vierailu sivustolla)',
   'Allow friendships between members' => 'Salli jäsenten ystävyyssuhteet',
-  'External users can register (show registration form on login)' => 'Ulkoiset käyttäjät voivat rekisteröityä (näytä rekisteröitymislomake kirjautumissivulla)',
+  'External users can register (show registration form on login)' => 'Käyttäjät voivat rekisteröityä itse (näytä rekisteröitymislomake kirjautumissivulla)',
   'Newly registered users have to be activated by an admin first' => 'Ylläpitäjän on hyväksyttävä rekisteröityneet käyttäjät',
   'Registered members can invite new users via email' => 'Rekisteröityneet käyttäjät voivat kutsua uusia käyttäjiä sähköpostitse',
 );
