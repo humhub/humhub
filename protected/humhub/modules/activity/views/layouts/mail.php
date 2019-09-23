e96eda570289b8d65266daef4f72acc01bff1277
Lucas Bartholemy <lucas@bartholemy.com>
2019-09-23 21:44:25 +0200

    fix old variable usage

--- protected/humhub/modules/activity/views/layouts/mail.php
+++ protected/humhub/modules/activity/views/layouts/mail.php
@@ -5,8 +5,18 @@
  * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  */
- 
+
+use humhub\modules\content\components\ContentContainerActiveRecord;
 use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $space Space */
+/* @var $url string */
+/* @var $contentContainer ContentContainerActiveRecord */
+/* @var $html string */
+/* @var $text string */
+/* @var $originator User */
+
 ?>
 
 <!-- START NOTIFICATION/ACTIVITY -->
@@ -66,16 +76,17 @@ use yii\helpers\Html;
                                                             <tr>
                                                                 <td style="font-size: 13px; line-height: 22px; font-family: <?= Yii::$app->view->theme->variable('mail-font-family', 'Open Sans, Arial, Tahoma, Helvetica, sans-serif') ?>; color:#555555; font-weight:300; text-align:left; ">
                                                                     <!-- prevent content overflow -->
-                                                                    <div style="width:480px;overflow:hidden;text-overflow:ellipsis;font-size: 13px; line-height: 22px; font-family: <?= Yii::$app->view->theme->variable('mail-font-family', 'Open Sans, Arial, Tahoma, Helvetica, sans-serif') ?>; color:#555555; font-weight:300; text-align:left;">
+                                                                    <div
+                                                                        style="width:480px;overflow:hidden;text-overflow:ellipsis;font-size: 13px; line-height: 22px; font-family: <?= Yii::$app->view->theme->variable('mail-font-family', 'Open Sans, Arial, Tahoma, Helvetica, sans-serif') ?>; color:#555555; font-weight:300; text-align:left;">
                                                                         <!-- content output-->
                                                                         <?= $content; ?>
 
                                                                         <!-- check if activity object has a space -->
-                                                                        <?php if ($record->content->space !== null): ?>
-                                                                            (<?= Yii::t('ActivityModule.base', 'via'); ?>
-                                                                            <a href="<?= $record->content->space->createUrl('/space/space', [], true); ?>"
-                                                                               style="text-decoration: none; color: #555555;">
-                                                                                   <?= Html::encode($record->content->space->name); ?></a>)
+                                                                        <?php if ($space !== null): ?>
+                                                                            <?= Html::a($space->displayName,
+                                                                                $space->createUrl(null, [], true), [
+                                                                                    'style' => 'text-decoration: none; color: #555555;'
+                                                                                ]); ?>
                                                                         <?php endif; ?>
 
                                                                         <?php if ($url != "") : ?>
@@ -87,7 +98,7 @@ use yii\helpers\Html;
                                                                             <!-- END: CONTENT LINK -->
                                                                         <?php endif; ?>
                                                                     </div>
-                                                                           
+
                                                                 </td>
                                                             </tr>
 
