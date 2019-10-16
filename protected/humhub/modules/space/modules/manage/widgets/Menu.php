7904a156755184124ada81bb841d540c292ec861
Lucas Bartholemy <lucas@bartholemy.com>
2019-10-16 13:20:07 +0200

    Added deprecated Menu.php class as fallback

---
+++ protected/humhub/modules/space/modules/manage/widgets/Menu.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2016 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+namespace humhub\modules\space\modules\manage\widgets;
+
+/**
+ * Menu compatibility
+ *
+ * @see \humhub\modules\space\widgets\HeaderControlsMenu
+ * @deprecated since version 1.1
+ * @author Luke
+ */
+class Menu extends \humhub\modules\space\widgets\HeaderControlsMenu
+{
+    
+}
