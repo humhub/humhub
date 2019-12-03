c189656b8ae35d0bdd11172a5d50050aa69cba34
Ace2213 <Yamman92@gmail.com>
2019-12-03 11:39:26 -0300

    Privacy hint in profile settings
    It pained me to type "Security Settings" when referring to Privacy Settings, but that's the official name.

--- protected/humhub/modules/user/views/account/edit.php
+++ protected/humhub/modules/user/views/account/edit.php
@@ -4,7 +4,8 @@
 
 <?php $this->beginContent('@user/views/account/_userProfileLayout.php') ?>
     <div class="help-block">
-        <?= Yii::t('UserModule.views_account_edit', 'Here you can edit your general profile data, which is visible in the about page of your profile.'); ?>
+        <?= Yii::t('UserModule.views_account_edit', 'The info you add here will be visible on the About page of your profile.<br/>
+        The visibility of your About page can be controlled in your Security Settings.'); ?>
     </div>
     <?php $form = ActiveForm::begin(['enableClientValidation' => false, 'options' => ['data-ui-widget' => 'ui.form.TabbedForm', 'data-ui-init' => '', 'style' => 'display:none']]); ?>
         <?= $hForm->render($form); ?>
