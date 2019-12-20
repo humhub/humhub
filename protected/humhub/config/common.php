ed1b9643b2d58cde9ba6c11df212e9059d85fa0e
Lucas Bartholemy <lucas@bartholemy.com>
2019-12-20 16:08:48 +0100

    Prepare 1.3.19 release

--- protected/humhub/config/common.php
+++ protected/humhub/config/common.php
@@ -16,7 +16,7 @@ Yii::$classMap['Zend\Stdlib\ArrayObject'] = '@humhub/compat/ArrayObject.php';
 
 $config = [
     'name' => 'HumHub',
-    'version' => '1.3.18',
+    'version' => '1.3.19',
     'basePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
     'bootstrap' => ['log', 'humhub\components\bootstrap\ModuleAutoLoader', 'queue', 'humhub\modules\ui\view\bootstrap\ThemeLoader'],
     'sourceLanguage' => 'en',
