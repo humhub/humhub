6878225d9729438fd3f8a12fedeecb928526eeb7
Lucas Bartholemy <lucas@bartholemy.com>
2020-04-01 18:34:17 +0200

    Larger login logo

--- protected/humhub/widgets/views/logo.php
+++ protected/humhub/widgets/views/logo.php
@@ -1,22 +1,27 @@
 <?php
 
 use yii\helpers\Html;
+
 ?>
 <?php if ($place == "topMenu") : ?>
     <?php if ($logo->hasImage()) : ?>
-        <a class="navbar-brand hidden-xs"  href="<?= Yii::$app->homeUrl; ?>">
-            <img class="img-rounded" src="<?= $logo->getUrl(); ?>" alt="<?= Yii::t('base', 'Logo of {appName}', ['appName' => Html::encode(Yii::$app->name)])?>" id="img-logo"/>
+        <a class="navbar-brand hidden-xs" href="<?= Yii::$app->homeUrl; ?>">
+            <img class="img-rounded" src="<?= $logo->getUrl(); ?>"
+                 alt="<?= Yii::t('base', 'Logo of {appName}', ['appName' => Html::encode(Yii::$app->name)]) ?>"
+                 id="img-logo"/>
         </a>
     <?php endif; ?>
-    <a class="navbar-brand" style="<?php if ($logo->hasImage()) : ?>display:none;<?php endif; ?> "  href="<?= Yii::$app->homeUrl; ?>" id="text-logo">
-           <?= Html::encode(Yii::$app->name); ?>
+    <a class="navbar-brand" style="<?php if ($logo->hasImage()) : ?>display:none;<?php endif; ?> "
+       href="<?= Yii::$app->homeUrl; ?>" id="text-logo">
+        <?= Html::encode(Yii::$app->name); ?>
     </a>
 <?php endif; ?>
 
 <?php if ($place == "login") : ?>
     <?php if ($logo->hasImage()) : ?>
         <a href="<?= Yii::$app->homeUrl; ?>" data-pjax-prevent>
-            <img class="img-rounded" src="<?= $logo->getUrl(); ?>" id="img-logo" alt="<?= Yii::t('base', 'Logo of {appName}', ['appName' => Html::encode(Yii::$app->name)]) ?>"/>
+            <img class="img-rounded" src="<?= $logo->getUrl(500, 250); ?>" id="img-logo"
+                 alt="<?= Yii::t('base', 'Logo of {appName}', ['appName' => Html::encode(Yii::$app->name)]) ?>"/>
         </a>
         <br>
     <?php else: ?>
