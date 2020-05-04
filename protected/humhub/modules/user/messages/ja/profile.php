0d0c65b4a44d0cd01a55b7ea2bb4d1ea391c290e
HumHub Translations <info@humhub.com>
2020-05-04 18:12:09 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/user/messages/ja/profile.php
+++ protected/humhub/modules/user/messages/ja/profile.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  '%y Years' => '%y 年',
+  '%y Years' => '%y歳',
   '<b>This profile stream is still empty!</b>' => '<b>このプロフィール・ストリームには何も書かれていません。</b>',
   '<b>This profile stream is still empty</b><br>Be the first and post something...' => '',
-  '<b>Your profile stream is still empty</b><br>Get started and post something...' => '<b>あなたのストリームには何も書かれていません</b><br>何かを適当に書いてみましょう\'',
+  '<b>Your profile stream is still empty</b><br>Get started and post something...' => '<b>あなたのストリームには何も書かれていません</b><br />何かを適当に書いてみましょう\'',
   '<strong>About</strong> this user' => '<strong>ユーザー</strong> 詳細',
   '<strong>Profile</strong> menu' => '<strong>プロフィール</strong>',
   'About' => 'About',
