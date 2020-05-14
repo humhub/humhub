ae5a9da347e5bc21a5167c0270257547a6f377dd
buddh4 <mail@jharrer.de>
2020-05-14 17:20:48 +0200

    Fixes validation error handling in post and comments.

--- protected/humhub/modules/comment/models/Comment.php
+++ protected/humhub/modules/comment/models/Comment.php
@@ -61,6 +61,7 @@ class Comment extends ContentAddonActiveRecord implements ContentOwner
     {
         return [
             [['message'], 'safe'],
+            [['message'], 'required'],
         ];
     }
 
