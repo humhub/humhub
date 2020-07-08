92dd96467eea2565650f55ed7d70060d87c5845c
El-Badry <mahmoudelbadry1994@gmail.com>
2020-07-08 19:44:55 +0200

    Updating Arabic Translation
    Signed-off-by: El-Badry <mahmoudelbadry1994@gmail.com>

--- protected/humhub/modules/admin/messages/ar/console.php
+++ protected/humhub/modules/admin/messages/ar/console.php
@@ -19,18 +19,19 @@
 return [
     '
 Module successfully disabled!
-' => '',
+' => 'تم تعطيل الوحدة بنجاح!
+',
     '
 Module successfully enabled!
-' => '',
+' => 'تم تفعيل الوحدة بنجاح!',
     '--- Disable module: {moduleId} ---
 
-' => '',
+' => '--- تعطيل الوحدة: {moduleId} ---',
     '--- Enable module: {moduleId} ---
 
-' => '',
+' => '--- تمكين الوحدة: {moduleId} ---',
     'Module not found or activated!
-' => '',
+' => 'لم يتم العثور على الوحدة أو تفعيلها!',
     'Module not found!
-' => '',
+' => 'الوحدة غير موجودة!',
 ];
