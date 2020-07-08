60e4bda2f901cc75a424ebcfdc0763baedf0b097
El-Badry <mahmoudelbadry1994@gmail.com>
2020-07-08 22:47:12 +0200

    Updating Arabic Translation~
    Signed-off-by: El-Badry <mahmoudelbadry1994@gmail.com>

--- protected/humhub/modules/post/messages/ar/base.php
+++ protected/humhub/modules/post/messages/ar/base.php
@@ -1,12 +1,12 @@
 <?php
 return array (
-  'Allow others to create new posts on your profile page' => '',
-  'Allows the user to create posts' => '',
+  'Allow others to create new posts on your profile page' => 'اسمح للآخرين بإنشاء مشاركات جديدة على صفحة ملفك الشخصي',
+  'Allows the user to create posts' => 'يسمح للمستخدم بإنشاء المشاركات',
   'Collapse' => 'تصغير',
-  'Create post' => '',
+  'Create post' => 'إنشاء مشاركة',
   'Edit your post...' => 'تحرير موضوعك...',
   'Read full post...' => 'قراءة الموضوع كاملاً...',
-  'This space is archived.' => '',
+  'This space is archived.' => 'تمت أرشفة هذه المساحة.',
   'What\'s on your mind?' => 'ماذا يخطر ببالك؟',
   'post' => 'نشر',
 );
