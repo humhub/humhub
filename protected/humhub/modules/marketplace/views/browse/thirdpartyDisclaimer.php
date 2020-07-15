a30745656804ccbac9938bdd7f118b2a0bfae6ae
Lucas Bartholemy <luke-@users.noreply.github.com>
2020-07-15 18:39:59 +0200

    Update thirdpartyDisclaimer.php

--- protected/humhub/modules/marketplace/views/browse/thirdpartyDisclaimer.php
+++ protected/humhub/modules/marketplace/views/browse/thirdpartyDisclaimer.php
@@ -22,7 +22,7 @@
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-primary" data-dismiss="modal">
-                <?= Yii::t('MarketplaceModule . base', 'Ok'); ?>
+                <?= Yii::t('MarketplaceModule.base', 'Ok'); ?>
             </button>
         </div>
     </div>
