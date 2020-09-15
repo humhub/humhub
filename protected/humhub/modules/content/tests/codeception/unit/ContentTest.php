6122cb5fd138b936182a1004de80264c8c1d99a5
buddh4 <mail@jharrer.de>
2020-09-15 17:46:44 +0200

    Added ContentTest.php

---
+++ protected/humhub/modules/content/tests/codeception/unit/ContentTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ *
+ */
+
+namespace tests\codeception\unit\modules\content;
+
+use tests\codeception\_support\HumHubDbTestCase;
+use humhub\modules\post\models\Post;
+
+use humhub\modules\space\models\Space;
+use humhub\modules\content\models\Content;
+
+class ContentTest extends HumHubDbTestCase
+{
+
+    public function testNewPostIsNotEdited()
+    {
+        $this->becomeUser('User2');
+        $space = Space::findOne(['id' => 2]);
+
+        $post1 = new Post($space, Content::VISIBILITY_PUBLIC, ['message' => 'Test']);
+        $this->assertTrue($post1->save());
+        $this->assertFalse($post1->content->isEdited());
+
+        // Reload content
+        $post1 = Post::findOne(['id' => $post1->id]);
+        $this->assertFalse($post1->content->isEdited());
+    }
+
+    public function testEditedPostIsEdited()
+    {
+        $this->becomeUser('User2');
+        $space = Space::findOne(['id' => 2]);
+
+        $post1 = new Post($space, Content::VISIBILITY_PUBLIC, ['message' => 'Test']);
+        $this->assertTrue($post1->save());
+
+        // Reload content
+        $post1 = Post::findOne(['id' => $post1->id]);
+        $post1->message = 'Updated Message';
+        $this->assertTrue($post1->save());
+        $post1->refresh();
+        $this->assertTrue($post1->content->isEdited());
+
+        // Reload content
+        $post1 = Post::findOne(['id' => $post1->id]);
+        $this->assertTrue($post1->content->isEdited());
+    }
+
+}
