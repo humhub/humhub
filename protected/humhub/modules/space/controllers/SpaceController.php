7bbaccff3597f4910d70ad4941a89ba96645a0c2
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2020-09-15 05:56:01 -0400

    Update SpaceController.php

--- protected/humhub/modules/space/controllers/SpaceController.php
+++ protected/humhub/modules/space/controllers/SpaceController.php
@@ -45,7 +45,7 @@ class SpaceController extends ContentContainerController
         return [
             'acl' => [
                 'class' => AccessControl::class,
-                'guestAllowedActions' => ['index', 'home', 'stream'],
+                'guestAllowedActions' => ['index', 'home', 'about', 'stream'],
             ]
         ];
     }
