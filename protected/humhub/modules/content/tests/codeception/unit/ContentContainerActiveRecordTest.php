ade7234bff7fb9b7e5fcd66250423f3f48f690e2
Lucas Bartholemy <luke-@users.noreply.github.com>
2020-09-16 13:37:57 +0200

    Update ContentContainerActiveRecordTest.php

--- protected/humhub/modules/content/tests/codeception/unit/ContentContainerActiveRecordTest.php
+++ protected/humhub/modules/content/tests/codeception/unit/ContentContainerActiveRecordTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
+ * @copyright Copyright (c) 2020 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
  *
  */
