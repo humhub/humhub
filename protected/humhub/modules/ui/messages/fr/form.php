c065cb8b67ab221c6cd1388ac45555ab3df5c42a
HumHub Translations <info@humhub.com>
2020-10-15 17:15:42 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/ui/messages/fr/form.php
+++ protected/humhub/modules/ui/messages/fr/form.php
@@ -3,5 +3,5 @@ return array (
   'Invalid icon.' => 'Icône invalide.',
   'Select icon' => 'Sélectionner une icône',
   'Sort Order' => 'Ordre de tri',
-  'Values between 0 and 10000, the existing elements usually use steps of 100.' => '',
+  'Values between 0 and 10000, the existing elements usually use steps of 100.' => 'Valeur entre 0 et 10000, les éléments existant utilisent des espacement de 100',
 );
