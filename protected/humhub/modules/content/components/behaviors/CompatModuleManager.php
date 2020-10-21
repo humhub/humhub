501cd6181b29ea03b8832f7be6efec8f1c5b00fa
Master_Hans <suvorov.ag.75@gmail.com>
2020-10-21 20:58:31 +0300

    #4494 Created ContentContainerModule::getInstallableModules($container).Remove redundant methods isAlwaysActive and  isModuleAlwaysActive.

--- protected/humhub/modules/content/components/behaviors/CompatModuleManager.php
+++ protected/humhub/modules/content/components/behaviors/CompatModuleManager.php
@@ -66,9 +66,4 @@ class CompatModuleManager extends Behavior
     {
         return $this->moduleManager->disable($moduleId);
     }
-
-    public function isModuleAlwaysActive($id)
-    {
-        return $this->moduleManager->isAlwaysActive($id);
-    }
 }
