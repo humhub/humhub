f91b24ce4905ab83f8f97ef64a4f102acc60b06e
buddh4 <mail@jharrer.de>
2020-10-26 16:48:36 +0100

    Fix #4581: Users see content of archived spaces on dashboard

--- CHANGELOG_DEV.md
+++ CHANGELOG_DEV.md
@@ -16,7 +16,8 @@ HumHub Changelog
 - Enh #4531: Remove wall entry left padding on mobile 
 - Fix #4554: Code blocks in comments uses same background color
 - Fix #4574: Different style between clickable and non clickable space/user counter
-- Fix #4575: Increased text size of "Read more" link on short-text post 
+- Fix #4575: Increased text size of "Read more" link on short-text post
+- Fix #4581: Users see content of archived spaces on dashboard
 
 
 1.7.0-beta.1 (October 16, 2020)
--- protected/humhub/modules/dashboard/components/actions/DashboardStreamAction.php
+++ protected/humhub/modules/dashboard/components/actions/DashboardStreamAction.php
@@ -66,7 +66,9 @@ class DashboardStreamAction extends ActivityStreamAction
             ->select(["contentcontainer.id"])
             ->from('space')
             ->leftJoin('contentcontainer', 'space.id=contentcontainer.pk AND contentcontainer.class=:spaceClass')
-            ->where('space.visibility=' . Space::VISIBILITY_ALL);
+            ->where('space.visibility=' . Space::VISIBILITY_ALL)
+            ->andWhere('space.status='. Space::STATUS_ENABLED);
+
         $union = Yii::$app->db->getQueryBuilder()->build($publicSpacesSql)[0];
 
         $publicProfilesSql = (new Query())
