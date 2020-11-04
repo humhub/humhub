164a55794724483757634ce1d38d6d29c677eb50
HumHub Translations <info@humhub.com>
2020-11-04 14:20:32 +0100

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/like/messages/bg/notifications.php
+++ protected/humhub/modules/like/messages/bg/notifications.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-  'Likes' => '',
-  'Receive Notifications when someone likes your content.' => '',
-  'View Online' => '',
-  '{displayNames} likes your {contentTitle}.' => '',
-  '{displayNames} likes {contentTitle}.' => '',
-  '{displayName} likes your {contentTitle}.' => '',
-  '{displayName} likes {contentTitle}.' => '',
+  'Likes' => 'Харесвания',
+  'Receive Notifications when someone likes your content.' => 'Получавайте известия, когато някой харесва вашето съдържание.',
+  'View Online' => 'Преглед онлайн',
+  '{displayNames} likes your {contentTitle}.' => '{displayNames} харесва вашето {contentTitle}.',
+  '{displayNames} likes {contentTitle}.' => '{displayNames} харесва {contentTitle}.',
+  '{displayName} likes your {contentTitle}.' => '{displayName} харесва вашето {contentTitle}.',
+  '{displayName} likes {contentTitle}.' => '{displayName} харесва {contentTitle}.',
 );
