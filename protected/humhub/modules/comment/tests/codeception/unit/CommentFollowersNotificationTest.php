3feef63f865520f0d326879e04cb56789cf9d25c
Master_Hans <suvorov.ag.75@gmail.com>
2020-12-03 22:23:01 +0300

    #4316 Created CommentFollowersNotificationTest test with to methods testCommentNotification and testSubCommentReplyNotification.

--- protected/humhub/modules/comment/tests/codeception/unit/CommentFollowersNotificationTest.php
+++ protected/humhub/modules/comment/tests/codeception/unit/CommentFollowersNotificationTest.php
@@ -1,16 +1,16 @@
 <?php namespace comment;
 
 use Codeception\Specify;
+use humhub\modules\comment\notifications\NewComment;
 use humhub\modules\comment\models\Comment;
 use humhub\modules\post\models\Post;
-use humhub\modules\user\models\User;
 use tests\codeception\_support\HumHubDbTestCase;
 
 class CommentFollowersNotificationTest extends HumHubDbTestCase
 {
     use Specify;
 
-    public function testCommentFollowersNotification()
+    public function testCommentNotification()
     {
         //test comment on base content
         $this->becomeUser('User2');
@@ -24,15 +24,50 @@ class CommentFollowersNotificationTest extends HumHubDbTestCase
         $comment->save();
 
         //check up followers
-        $this->assertNotEmpty($comment->content->getPolymorphicRelation()->getFollowers(null, true, true), 'Followers for this comment not found');
+        $this->assertNotEmpty($comment->getCommentedRecord()->getFollowers(null, true, true), 'Followers for this comment not found');
 
-        //check up the current comment author
-        $this->assertNotEmpty($comment->user, 'Current comment user is empty!');
+        $this->assertHasNotification(NewComment::class, $comment);
+
+        //check up for sent emails at least one
+        $this->assertMailSent(1, 'Comment Notification Mail sent');
+
+    }
+
+    public function testSubCommentReplyNotification()
+    {
+        //test comment on base content
+        $this->becomeUser('User2');
+
+        $comment = new Comment([
+            'message' => 'User2 comment!',
+            'object_model' => Post::class,
+            'object_id' => 11
+        ]);
+
+        $comment->save();
+        $commentId = $comment->id;
+
+        $this->assertHasNotification(NewComment::class, $comment);
+
+        //test comment on base content
+        $this->becomeUser('User2');
+
+        $comment = new Comment([
+            'message' => 'User3 comment!',
+            'object_model' => Comment::class,
+            'object_id' => $commentId
+        ]);
+
+        $comment->save();
+
+        //check up followers
+        $this->assertNotEmpty($comment->getCommentedRecord()->getFollowers(null, true, true), 'Followers for this comment not found');
 
         //check up parent object of current comment
-        $parent = $comment->object_model::findOne(['id', $comment->object_id]);
-        $parentUser = User::findOne(['id' => $parent->created_by]);
-        $this->assertNotEmpty($parentUser, 'Parent object of current comment not found!');
+        /** @var Comment $parent*/
+        $parent = $comment->getPolymorphicRelation();
+
+        $this->assertNotEmpty($parent->created_by, 'Parent object created by user id is empty!');
 
         //check up the user author of commented record
         $this->assertNotEmpty($comment->getCommentedRecord()->owner, 'Author of the commented record of this comment not found!');
@@ -40,16 +75,5 @@ class CommentFollowersNotificationTest extends HumHubDbTestCase
         //check up for sent emails at least one
         $this->assertMailSent(1, 'Comment Notification Mail sent');
 
-        /**
-         * Send notifications to only these followers:
-         * author of the parent comment
-         * author of the commented record
-         */
-        $followersShouldReceiveNotification = [
-            $parentUser, // author of the parent comment
-            $comment->getCommentedRecord()->owner, // author of the commented record
-        ];
-
-        $this->assertNotEmpty($followersShouldReceiveNotification, 'Followers who should receive notification array is empty!');
     }
 }
