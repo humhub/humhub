6b21a48fbf45960b365c014b5b21dba2e1cd0e04
yurabakhtin <yurybakh@gmail.com>
2020-12-30 15:00:12 +0300

    Don’t create Default Group during a fresh installation

--- protected/humhub/migrations/m201228_064513_default_group.php
+++ protected/humhub/migrations/m201228_064513_default_group.php
@@ -1,5 +1,6 @@
 <?php
 
+use humhub\models\Setting;
 use humhub\modules\user\models\Group;
 use humhub\modules\user\models\User;
 use yii\db\Migration;
@@ -23,29 +24,32 @@ class m201228_064513_default_group extends Migration
             Yii::$app->getModule('user')->settings->set('auth.showRegistrationUserGroup', '1');
         }
 
-        // Move value from setting:auth.defaultUserGroup into new column group:is_default_group
-        if (empty($defaultUserGroupId) ||
-            !($group = Group::findOne(['id' => $defaultUserGroupId])) ||
-            $group->is_admin_group) {
-            // Create one default Group if setting:auth.defaultUserGroup was not selected to any group:
-            $group = new Group();
-            $group->name = 'Default Group';
-            $group->description = 'Group for users who are not assigned to any other group';
-        }
+        // Try to create "Default Group" only for upgrade case because on new installation the group "Users" is used as default group:
+        if (Setting::isInstalled()) {
+            // Move value from setting:auth.defaultUserGroup into new column group:is_default_group
+            if (empty($defaultUserGroupId) ||
+                !($group = Group::findOne(['id' => $defaultUserGroupId])) ||
+                $group->is_admin_group) {
+                // Create one default Group if setting:auth.defaultUserGroup was not selected to any group:
+                $group = new Group();
+                $group->name = 'Default Group';
+                $group->description = 'Group for users who are not assigned to any other group';
+            }
 
-        // Make default either old group that is used for new users or new created group above:
-        $group->is_default_group = 1;
-        if ($group->save()) {
-            // Assign users to the Default Group who were not assigned to any other group before:
-            $this->execute('INSERT INTO group_user (user_id, group_id, created_at, updated_at)
-                SELECT user.id, :defaultGroupId, NOW(), NOW()
-                  FROM user
-                  LEFT JOIN group_user ON group_user.user_id = user.id
-                 WHERE group_user.id IS NULL
-                   AND user.status != :userStatusSoftDeleted', [
-                ':defaultGroupId' => $group->id,
-                ':userStatusSoftDeleted' => User::STATUS_SOFT_DELETED,
-            ]);
+            // Make default either old group that is used for new users or new created group above:
+            $group->is_default_group = 1;
+            if ($group->save()) {
+                // Assign users to the Default Group who were not assigned to any other group before:
+                $this->execute('INSERT INTO group_user (user_id, group_id, created_at, updated_at)
+                    SELECT user.id, :defaultGroupId, NOW(), NOW()
+                      FROM user
+                      LEFT JOIN group_user ON group_user.user_id = user.id
+                     WHERE group_user.id IS NULL
+                       AND user.status != :userStatusSoftDeleted', [
+                    ':defaultGroupId' => $group->id,
+                    ':userStatusSoftDeleted' => User::STATUS_SOFT_DELETED,
+                ]);
+            }
         }
 
         // Remove old setting:
