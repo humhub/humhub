7e1b191bdb1af832c47ebb1710017255db0252ec
yurabakhtin <yurybakh@gmail.com>
2021-01-29 18:44:49 +0300

    Revert unnecessary primary key length restriction

--- protected/humhub/modules/content/migrations/m201130_073907_default_permissions.php
+++ protected/humhub/modules/content/migrations/m201130_073907_default_permissions.php
@@ -21,8 +21,7 @@ class m201130_073907_default_permissions extends Migration
             'class' => Schema::TYPE_STRING,
             'state' => Schema::TYPE_BOOLEAN,
         ]);
-        // Limit PK length to 767 bytes (50 + 50 + 50 + 41) * 4 = 191 * 4 = 764
-        $this->addPrimaryKey('contentcontainer_default_permission_pk', 'contentcontainer_default_permission', ['permission_id(50)', 'group_id', 'module_id', 'contentcontainer_class(41)']);
+        $this->addPrimaryKey('contentcontainer_default_permission_pk', 'contentcontainer_default_permission', ['permission_id', 'group_id', 'module_id', 'contentcontainer_class']);
     }
 
     /**
