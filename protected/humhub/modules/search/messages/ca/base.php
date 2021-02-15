98107cbbd46a89b00ef27df4dbd7029710e77901
HumHub Translations <info@humhub.com>
2021-02-15 11:34:20 +0100

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/search/messages/ca/base.php
+++ protected/humhub/modules/search/messages/ca/base.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  '<strong>Search </strong> results' => '',
-  'Advanced search settings' => '',
-  'All' => '',
+  '<strong>Search </strong> results' => '<strong>Resultats</strong> de la cerca',
+  'Advanced search settings' => 'Cerca avançada',
+  'All' => 'Totes',
   'Content' => 'Contingut',
   'Search' => 'Cerca',
-  'Search for user, spaces and content' => '',
-  'Search only in certain spaces:' => '',
+  'Search for user, spaces and content' => 'Cerca usuaris, espais i contingut',
+  'Search only in certain spaces:' => 'Busca només en certs espais:',
   'Spaces' => 'Espais',
-  'Specify space' => '',
+  'Specify space' => 'Especifica un espai',
   'Users' => 'Usuaris',
-  'Your search returned no matches.' => '',
+  'Your search returned no matches.' => 'La teva cerca no ha donat cap resultat.',
 );
--- protected/humhub/modules/user/messages/ca/chooser.php
+++ protected/humhub/modules/user/messages/ca/chooser.php
@@ -1,9 +1,9 @@
 <?php
 return array (
-  'Add more...' => '',
-  'An unexpected error occurred while loading the result.' => '',
+  'Add more...' => 'Afegeix més...',
+  'An unexpected error occurred while loading the result.' => 'S\'ha produït un error inesperat en carregar el resultat.',
   'Load more' => 'Carregar més',
-  'No users found for the given query.' => '',
+  'No users found for the given query.' => 'No s\'ha trobat cap usuari per aquesta consulta.',
   'Please enter at least {n} character' => '',
   'Select {n,plural,=1{item} other{items}}' => '',
   'Select {n,plural,=1{user} other{users}}' => '',
