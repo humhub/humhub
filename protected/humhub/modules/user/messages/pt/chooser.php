618aa7deca512d7f977b53a68e0560136ac492a7
HumHub Translations <info@humhub.com>
2021-02-23 17:20:30 +0100

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/user/messages/pt/chooser.php
+++ protected/humhub/modules/user/messages/pt/chooser.php
@@ -1,14 +1,14 @@
 <?php
 return array (
-  'Add more...' => 'Adiciona mais...',
-  'An unexpected error occurred while loading the result.' => 'Um erro inesperado ocorreu durante a geração do resultado',
+  'Add more...' => 'Adiciona...',
+  'An unexpected error occurred while loading the result.' => 'Ocorreu um erro inesperado durante a operação',
   'Load more' => 'Carregar mais',
-  'No users found for the given query.' => 'Não foram encontradas pessoas para a questão.',
+  'No users found for the given query.' => 'Nenhum usuário encontrado nesta consulta.',
   'Please enter at least {n} character' => 'Por favor inclui pelo menos {n} caracteres',
   'Select {n,plural,=1{item} other{items}}' => 'Selecciona {n,plural,=1{item} other{items}}',
   'Select {n,plural,=1{user} other{users}}' => 'Selecciona {n,plural,=1{user} other{users}}',
   'This field only allows a maximum of {n,plural,=1{# item} other{# items}}.' => 'Este campo permite apenas um máximo de {n,plural,=1{# item} other{# items}}.',
   'This field only allows a maximum of {n,plural,=1{# user} other{# users}}.' => 'Este campo permite apenas um máximo de {n,plural,=1{# user} other{# users}}.',
-  'You reached the maximum number of allowed characters ({n}).' => 'Alcançaste o número máximo de caracteres permitidos ({n}).',
-  'Your search returned no matches.' => 'A tua pesquisa não deu frutos condizentes',
+  'You reached the maximum number of allowed characters ({n}).' => 'Alcançaste o número máximo de caracteres permitido ({n}).',
+  'Your search returned no matches.' => 'A tua pesquisa não tem resultados correspondentes',
 );
