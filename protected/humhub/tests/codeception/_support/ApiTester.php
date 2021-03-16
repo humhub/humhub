3dbd8e2dbb4f732b3e2d6f7e4a4585adf74e630a
yurabakhtin <yurybakh@gmail.com>
2021-03-16 14:59:37 +0300

    Improve API tests for pagination response

--- protected/humhub/tests/codeception/_support/ApiTester.php
+++ protected/humhub/tests/codeception/_support/ApiTester.php
@@ -1,6 +1,8 @@
 <?php
 
 use Codeception\Util\HttpCode;
+use yii\data\Pagination;
+use yii\web\Link;
 
 /**
  * Inherited Methods
@@ -65,5 +67,116 @@ class ApiTester extends \Codeception\Actor
         $this->seeResponseCodeIs(HttpCode::BAD_REQUEST);
         $this->seeResponseIsJson();
         $this->seeResponseContainsJson($json);
+    }
+
+    /**
+     * Send GET API Request and check a response contains the results
+     *
+     * @param string $url
+     * @param array $jsonResults
+     * @param array $paginationParams
+     */
+    public function seePaginationGetResponse($url, $jsonResults = [], $paginationParams = [])
+    {
+        $this->sendGet($url);
+        $this->seePaginationResponseContainsJson($url, $jsonResults, $paginationParams);
+    }
+
+    /**
+     * Send POST API Request and check a response contains the results
+     *
+     * @param string $url
+     * @param array $postData
+     * @param array $jsonResults
+     * @param array $paginationParams
+     */
+    public function seePaginationPostResponse($url, $postData, $jsonResults = [], $paginationParams = [])
+    {
+        $this->sendPost($url, $postData);
+        $this->seePaginationResponseContainsJson($url, $jsonResults, $paginationParams);
+    }
+
+    /**
+     * Send PUT API Request and check a response contains the results
+     *
+     * @param string $url
+     * @param array $putData
+     * @param array $jsonResults
+     * @param array $paginationParams
+     */
+    public function seePaginationPutResponse($url, $putData, $jsonResults = [], $paginationParams = [])
+    {
+        $this->sendPut($url, $putData);
+        $this->seePaginationResponseContainsJson($url, $jsonResults, $paginationParams);
+    }
+
+    /**
+     * Send POST API Request and check a response contains the results
+     *
+     * @param string $url
+     * @param array $deleteData
+     * @param array $jsonResults
+     * @param array $paginationParams
+     */
+    public function seePaginationDeleteResponse($url, $deleteData, $jsonResults = [], $paginationParams = [])
+    {
+        $this->sendPost($url, $deleteData);
+        $this->seePaginationResponseContainsJson($url, $jsonResults, $paginationParams);
+    }
+
+    /**
+     * Send GET API Request and check a response contains the results
+     *
+     * @param string $url
+     * @param array $jsonResults
+     * @param array $paginationParams Possible keys: 'total', 'page', 'pages'
+     */
+    public function seePaginationResponseContainsJson($url, $jsonResults = [], $paginationParams = [])
+    {
+        $json = array_merge([
+            'total' => count($jsonResults),
+            'page' => 1,
+            'pages' => 1,
+        ], $paginationParams);
+
+        $json['links'] = $this->getPaginationUrls($url, $json);
+        $json['results'] = $jsonResults;
+
+        $this->seeSuccessResponseContainsJson($json);
+    }
+
+    /**
+     * Get pagination URLs, Used to check JSON response
+     *
+     * @param string $url
+     * @param array $params Possible keys: 'page', 'pages'
+     * @return string[]
+     */
+    protected function getPaginationUrls($url, $params)
+    {
+        $links = [Link::REL_SELF => $this->getPaginationUrl($url, $params['page'])];
+
+        if ($params['pages'] > 0) {
+            $links[Pagination::LINK_FIRST] = $this->getPaginationUrl($url, 0);
+            $links[Pagination::LINK_LAST] = $this->getPaginationUrl($url, $params['pages'] - 1);
+            if ($params['page'] > 1) {
+                $links[Pagination::LINK_PREV] = $this->getPaginationUrl($url, $params['page'] - 1);
+            }
+            if ($params['page'] < $params['pages'] - 1) {
+                $links[Pagination::LINK_NEXT] = $this->getPaginationUrl($url, $params['page'] + 1);
+            }
+        }
+
+        return $links;
+    }
+
+    /**
+     * @param string $url
+     * @param int $page
+     * @return string
+     */
+    protected function getPaginationUrl($url, $page = 1)
+    {
+        return '/api/v1/' . trim($url, '/') . '?page=' . (empty($page) ? 1 : $page) . '&per-page=100';
     }
 }
