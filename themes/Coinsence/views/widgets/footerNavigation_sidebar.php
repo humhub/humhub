176fe2e751a057e770043d8cac2e807e3f4cab8f
craxrev <mortadha.ghanmi56@gmail.com>
2021-03-25 14:18:45 +0100

    fix minor issues with legal links

---
+++ themes/Coinsence/views/widgets/footerNavigation_sidebar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+use yii\helpers\Html;
+use humhub\widgets\PoweredBy;
+
+$i = 0;
+?>
+
+<?php if ($numItems > 0): ?>
+    <div class="footer-nav">
+        <small>
+
+            <?php foreach ($items as $item): ?>
+                <?= Html::a($item['label'], $item['url']); ?>
+
+                <?php if (++$i !== $numItems): ?>
+                    &nbsp;&middot;&nbsp;
+                <?php endif; ?>
+
+            <?php endforeach; ?>
+        </small>
+    </div>
+    <br />
+<?php endif; ?>
