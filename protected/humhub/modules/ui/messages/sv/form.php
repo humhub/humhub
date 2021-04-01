ea5d20f5554c44c7bdf8cce03514563288f4d90f
HumHub Translations <info@humhub.com>
2021-04-01 20:18:49 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/ui/messages/sv/form.php
+++ protected/humhub/modules/ui/messages/sv/form.php
@@ -3,5 +3,5 @@ return array (
   'Invalid icon.' => 'Felaktig ikon.',
   'Select icon' => 'Välj ikon',
   'Sort Order' => 'Sorteringsordning',
-  'Values between 0 and 10000, the existing elements usually use steps of 100.' => '',
+  'Values between 0 and 10000, the existing elements usually use steps of 100.' => 'Värden mellan 0 och 10000. De existerande elementen använder oftast steg på 100.',
 );
