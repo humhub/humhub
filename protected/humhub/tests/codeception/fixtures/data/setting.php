59530bd2ad21b79ef6be059dc777fb8f4ff9c2e9
Tri Phan <tri.phan@fdssoft.com>
2021-05-26 17:20:52 +0700

    Change topbar color

--- protected/humhub/tests/codeception/fixtures/data/setting.php
+++ protected/humhub/tests/codeception/fixtures/data/setting.php
@@ -21,7 +21,7 @@ return [
     ['name' => 'theme', 'value' => 'HumHub', 'module_id' => 'base'],
     ['name' => 'tour', 'value' => '1', 'module_id' => 'base'],
     ['name' => 'colorDefault', 'value' => '#ededed', 'module_id' => 'base'],
-    ['name' => 'colorPrimary', 'value' => '#708fa0', 'module_id' => 'base'],
+    ['name' => 'colorPrimary', 'value' => '#eb4034', 'module_id' => 'base'],
     ['name' => 'colorInfo', 'value' => '#6fdbe8', 'module_id' => 'base'],
     ['name' => 'colorSuccess', 'value' => '#97d271', 'module_id' => 'base'],
     ['name' => 'colorDanger', 'value' => '#ff8989', 'module_id' => 'base'],
