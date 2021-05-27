b796f032963a800c4bdb0bfe4294c63280dd0833
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2021-05-27 20:01:48 +0300

    Hide post input field on single entry stream (#5084)
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- protected/humhub/modules/space/views/space/home.php
+++ protected/humhub/modules/space/views/space/home.php
@@ -6,13 +6,16 @@
 
 use humhub\modules\activity\widgets\ActivityStreamViewer;
 use humhub\modules\post\widgets\Form;
+use humhub\modules\space\models\Space;
 use humhub\modules\space\modules\manage\widgets\PendingApprovals;
 use humhub\modules\space\widgets\Members;
 use humhub\modules\space\widgets\Sidebar;
 use humhub\modules\stream\widgets\StreamViewer;
 
-
-echo Form::widget(['contentContainer' => $space]);
+/* @var $space Space */
+/* @var $canCreatePosts bool */
+/* @var $isMember bool */
+/* @var $isSingleContentRequest bool */
 
 $emptyMessage = '';
 if ($canCreatePosts) {
@@ -22,15 +25,18 @@ if ($canCreatePosts) {
 } else {
     $emptyMessage = Yii::t('SpaceModule.base', '<b>You are not member of this space and there is no public content, yet!</b>');
 }
+?>
 
-echo StreamViewer::widget([
+<div data-stream-create-content="stream.wall.WallStream"<?php if ($isSingleContentRequest) : ?> style="display:none"<?php endif; ?>>
+    <?= Form::widget(['contentContainer' => $space]); ?>
+</div>
+
+<?= StreamViewer::widget([
     'contentContainer' => $space,
     'streamAction' => '/space/space/stream',
     'messageStreamEmpty' => $emptyMessage,
     'messageStreamEmptyCss' => ($canCreatePosts) ? 'placeholder-empty-stream' : '',
-]);
-
-?>
+]); ?>
 
 <?php
 $this->beginBlock('sidebar');
