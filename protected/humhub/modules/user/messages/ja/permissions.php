6a1642fc4f34c8936e11eef3343c1d575f5bfad3
HumHub Translations <info@humhub.com>
2021-06-14 16:41:16 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/user/messages/ja/permissions.php
+++ protected/humhub/modules/user/messages/ja/permissions.php
@@ -1,6 +1,5 @@
 <?php
-
-return [
-    'Can Access \'People\'' => '',
-    'Can access \'People\' section.' => '',
-];
+return array (
+  'Can Access \'People\'' => '"人"にアクセスできます',
+  'Can access \'People\' section.' => '"人"セクションにアクセスできます。',
+);
