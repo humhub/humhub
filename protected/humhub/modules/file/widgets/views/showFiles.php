f7cb32b36ecb4905de03a5f63655270fc6b5f1f6
yurabakhtin <yurybakh@gmail.com>
2021-07-21 17:02:21 +0300

    Fix video thumbnail on Safari browser

--- protected/humhub/modules/file/widgets/views/showFiles.php
+++ protected/humhub/modules/file/widgets/views/showFiles.php
@@ -52,25 +52,25 @@ $galleryColumnClass =  ThemeHelper::isFluid() ? $fluidColumnClass : $nonFluidCol
                     <?php if (FileHelper::getExtension($video->file_name) === 'webm'): ?>
                         <div class="<?= $fullWidthColumnClass ?>">
                             <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>" href="<?= $video->getUrl(); ?>#.webm" title="<?= Html::encode($video->file_name) ?>">
-                                <video src="<?= $video->getUrl() ?>" type="video/webm" controls preload="metadata" height="130"></video>
+                                <video src="<?= $video->getUrl() ?>#t=0.001" type="video/webm" controls preload="metadata" height="130"></video>
                             </a>
                         </div>
                     <?php elseif (FileHelper::getExtension($video->file_name) === 'mp4'): ?>
                         <div class="<?= $fullWidthColumnClass ?>">
                             <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>" href="<?= $video->getUrl(); ?>#.mp4" title="<?= Html::encode($video->file_name) ?>">
-                                <video src="<?= $video->getUrl() ?>" type="video/mp4" controls preload="metadata" height="130"></video>
+                                <video src="<?= $video->getUrl() ?>#t=0.001" type="video/mp4" controls preload="metadata" height="130"></video>
                             </a>
                         </div>
                     <?php elseif (FileHelper::getExtension($video->file_name) === 'ogv'): ?>
                         <div class="<?= $fullWidthColumnClass ?>">
                             <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>" href="<?= $video->getUrl(); ?>#.ogv" title="<?= Html::encode($video->file_name) ?>">
-                                <video src="<?= $video->getUrl() ?>" type="video/ogg" controls preload="metadata" height="130"></video>
+                                <video src="<?= $video->getUrl() ?>#t=0.001" type="video/ogg" controls preload="metadata" height="130"></video>
                             </a>
                         </div>
                     <?php elseif (FileHelper::getExtension($video->file_name) === 'mov'): ?>
                         <div class="<?= $fullWidthColumnClass ?>">
                             <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>" href="<?= $video->getUrl(); ?>#.mov" title="<?= Html::encode($video->file_name) ?>">
-                                <video src="<?= $video->getUrl() ?>" type="video/quicktime" controls preload="metadata" height="130"></video>
+                                <video src="<?= $video->getUrl() ?>#t=0.001" type="video/quicktime" controls preload="metadata" height="130"></video>
                             </a>
                         </div>
                     <?php endif; ?>
