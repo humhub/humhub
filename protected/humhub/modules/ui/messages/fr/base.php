4760050c592ce8092a44f6b780f02a778761ccc7
HumHub Translations <info@humhub.com>
2021-08-03 11:35:41 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/ui/messages/fr/base.php
+++ protected/humhub/modules/ui/messages/fr/base.php
@@ -1,17 +1,7 @@
 <?php
-<<<<<<< HEAD
 return array (
   'Collapse' => 'Afficher moins',
   'Read more' => 'Afficher plus...',
   'Reset filters' => 'Réinitialiser les filtres',
   'User Interface' => 'Interface utilisateur',
 );
-=======
-
-return [
-    'Collapse' => 'Afficher moins',
-    'Read more' => 'Afficher plus...',
-    'User Interface' => 'Interface utilisateur',
-    'Reset filters' => 'Réinitialiser les filtres',
-];
->>>>>>> 56033c592c48e11c8553aeca8a99f047701fbed1
