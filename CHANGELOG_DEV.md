0a6728772b7dc9eafe6c4c61fc9df89e2bbbf73b
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2021-10-15 12:15:21 +0300

    Filter archived spaces on directory (#5349)
    * Filter archived spaces on directory
    
    * Update CHANGELOG.md
    
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- CHANGELOG_DEV.md
+++ CHANGELOG_DEV.md
@@ -36,4 +36,5 @@
 - Fix #5331: Fix js error on pjax open "Directory" page
 - Enh #5333: Module's events handlers registration: check if method exists
 - Enh #5300: Blocking users for space
+- Enh #5349: Filter archived spaces on directory
 - Enh #5350: Show the latest file version
---
+++ protected/humhub/modules/space/widgets/views/spaceDirectoryStatus.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2021 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+/* @var $text string */
+/* @var $class string */
+?>
+
+<span class="card-status <?= $class ?>"><?= $text ?></span>
