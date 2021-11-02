16476fad66c36fbbda7d1f17eed5ccbcf0da03ec
yoshida-github-ai <83218900+yoshida-github-ai@users.noreply.github.com>
2021-11-02 16:55:42 +0900

    Update activities.php

--- protected/humhub/modules/content/messages/ja/activities.php
+++ protected/humhub/modules/content/messages/ja/activities.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Contents' => 'コンテンツ',
-  'Whenever a new content (e.g. post) has been created.' => '',
+ 'Contents' => 'コンテンツ',
+  'Whenever a new content (e.g. post) has been created.' => '新しいコンテンツ（投稿など）が作成されたときはいつでも',
   '{displayName} created a new {contentTitle}.' => '{displayName} 新規の {contentTitle}.',
 );
