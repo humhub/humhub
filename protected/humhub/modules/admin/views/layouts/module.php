65c12b06784be64e2ab5f712814feb3e8f498c78
yurabakhtin <yurybakh@gmail.com>
2021-12-17 08:54:21 +0300

    Rework modules section

---
+++ protected/humhub/modules/admin/views/layouts/module.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2021 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ */
+
+use humhub\modules\ui\view\helpers\ThemeHelper;
+
+/* @var $content string */
+?>
+<div class="<?php if (ThemeHelper::isFluid()): ?>container-fluid<?php else: ?>container<?php endif; ?> container-directory container-modules">
+    <div class="row">
+        <div class="col-lg-12">
+            <?= $content; ?>
+        </div>
+    </div>
+</div>
