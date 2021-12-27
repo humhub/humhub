f4ef2eea0091866346abe4ee1e9dbd97523b1a5a
yurabakhtin <yurybakh@gmail.com>
2021-12-27 15:35:27 +0300

    Fix user modules layout on fluid themes

--- protected/humhub/modules/user/views/account/editModules.php
+++ protected/humhub/modules/user/views/account/editModules.php
@@ -11,7 +11,7 @@ use humhub\modules\ui\view\helpers\ThemeHelper;
 
 ModuleAsset::register($this);
 ?>
-<div class="<?php if (ThemeHelper::isFluid()) : ?>container-fluid<?php else: ?>container<?php endif; ?> container-directory container-modules container-content-modules">
+<div class="container container-directory container-modules container-content-modules">
     <h4><?= Yii::t('UserModule.manage', '<strong>User</strong> modules'); ?></h4>
 
     <div class="row cards">
