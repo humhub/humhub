074f18a1a4da50b3f30147a53e6bbd7f34bd280f
Lucas Bartholemy <lucas@bartholemy.com>
2022-01-07 16:46:04 +0100

    Removed old message files

--- protected/humhub/modules/admin/messages/hu/controllers_ApprovalController.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'Invalid user state: {state}' => 'Érvénytelen felhasználói állapot: {state}',
-  'User not found!' => 'Felhasználó nem található!',
-);
--- protected/humhub/modules/admin/messages/tr/controllers_ApprovalController.php
+++
@@ -1,5 +0,0 @@
-<?php
-return array (
-  'Invalid user state: {state}' => 'Geçersiz kullanıcı durumu: {state}',
-  'User not found!' => 'Kullanıcı bulunamadı!',
-);
