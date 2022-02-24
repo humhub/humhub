cdd71eb7a76537ab7cb4df9ef818e302143f9c6c
Soundwave2142 <39148595+Soundwave2142@users.noreply.github.com>
2022-02-24 17:49:08 +0200

    #4823 Removed CHTML and CActiveForm classes and  usages (plus refactoring) (#5405)
    * #4823 Removed CHTML and CActiveForm Usages (plus refactoring)
    
    * #4823 missing id returned (Removed CHTML and CActiveForm Usages)
    
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- CHANGELOG_DEV.md
+++ CHANGELOG_DEV.md
@@ -33,3 +33,4 @@
 - Enh #4558: Deprecate CompatModuleManager
 - Enh #5323: Remove deprecated "Setting" classes
 - Enh #5381: Optimize module states query
+- Enh #4823: Removed CHTML and CActiveForm classes as well as usages (plus refactoring)
--- protected/humhub/modules/admin/models/forms/ModuleSetAsDefaultForm.php
+++ protected/humhub/modules/admin/models/forms/ModuleSetAsDefaultForm.php
@@ -2,16 +2,28 @@
 
 namespace humhub\modules\admin\models\forms;
 
+use humhub\modules\content\components\ContentContainerModuleManager;
+use humhub\modules\content\models\ContentContainerModuleState;
+use humhub\modules\space\models\Space;
+use humhub\modules\user\models\User;
+use Yii;
+use yii\base\Model;
+
 /**
  * GroupForm is used to modify group settings
  *
  * @package humhub.modules_core.admin.forms
  * @since 0.5
  */
-class ModuleSetAsDefaultForm extends \yii\base\Model
+class ModuleSetAsDefaultForm extends Model
 {
+    /** @var string */
+    protected $moduleId;
 
+    /** @var int | string */
     public $spaceDefaultState;
+
+    /** @var int | string */
     public $userDefaultState;
 
     /**
@@ -22,8 +34,58 @@ class ModuleSetAsDefaultForm extends \yii\base\Model
     public function rules()
     {
         return [
-            [['userDefaultState', 'spaceDefaultState'], 'integer'],
+            [['userDefaultState', 'spaceDefaultState'], 'in', 'range' => ContentContainerModuleState::getStates()],
         ];
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'spaceDefaultState' => Yii::t('AdminModule.modules', 'Space default state'),
+            'userDefaultState' => Yii::t('AdminModule.modules', 'User default state')
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getStatesList()
+    {
+        return ContentContainerModuleState::getStates(true);
+    }
+
+    /**
+     * @param $id
+     * @return $this
+     */
+    public function setModule($id): self
+    {
+        if ($this->moduleId == $id) {
+            return $this;
+        }
+
+        $this->moduleId = $id;
+        $this->spaceDefaultState = ContentContainerModuleManager::getDefaultState(Space::class, $id) ?? ContentContainerModuleState::STATE_DISABLED;
+        $this->userDefaultState = ContentContainerModuleManager::getDefaultState(User::class, $id) ?? ContentContainerModuleState::STATE_DISABLED;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function save($validate = true)
+    {
+        if ($validate && !$this->validate()) {
+            return false;
+        }
+
+        ContentContainerModuleManager::setDefaultState(User::class, $this->moduleId, $this->userDefaultState);
+        ContentContainerModuleManager::setDefaultState(Space::class, $this->moduleId, $this->spaceDefaultState);
+
+        return true;
+    }
 }
