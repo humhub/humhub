e12c30373b7f08dc1ba50826518d56c1cb8950d4
Yura Bakhtin <yurybakh@gmail.com>
2022-03-15 21:44:31 +0100

    Update file content id for comment records on migration

--- protected/humhub/modules/file/migrations/m220302_135158_add_content_id.php
+++ protected/humhub/modules/file/migrations/m220302_135158_add_content_id.php
@@ -1,6 +1,7 @@
 <?php
 
 use humhub\components\Migration;
+use humhub\modules\comment\models\Comment;
 
 /**
  * Class m220302_135158_add_content_id
@@ -19,6 +20,33 @@ class m220302_135158_add_content_id extends Migration
                ON file.object_model = content.object_model
               AND file.object_id = content.object_id
               SET file.content_id = content.id');
+
+        // Root comments
+        $this->execute('UPDATE file
+            INNER JOIN comment
+               ON file.object_model = :commentModel
+              AND file.object_id = comment.id
+            INNER JOIN content
+               ON comment.object_model = content.object_model
+              AND comment.object_id = content.object_id
+              SET file.content_id = content.id', [
+            'commentModel' => Comment::class
+        ]);
+
+        // Sub comments
+        $this->execute('UPDATE file
+            INNER JOIN comment AS c1
+               ON file.object_model = :commentModel
+              AND file.object_id = c1.id
+              AND c1.object_model = :commentModel
+            INNER JOIN comment AS c2
+              ON c1.object_id = c2.id
+            INNER JOIN content
+               ON c2.object_model = content.object_model
+              AND c2.object_id = content.object_id
+              SET file.content_id = content.id', [
+            'commentModel' => Comment::class
+        ]);
     }
 
     /**
