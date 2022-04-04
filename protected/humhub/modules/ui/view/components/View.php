285dfb16e5fa8d867e1a4019267dd61d972eaf14
Yura Bakhtin <yurybakh@gmail.com>
2022-04-04 15:36:07 +0200

    Improve code of rendering ajax content

--- protected/humhub/modules/ui/view/components/View.php
+++ protected/humhub/modules/ui/view/components/View.php
@@ -159,9 +159,49 @@ class View extends \yii\web\View
         $this->beginBody();
         echo $content;
         $this->endBody();
-        $this->endAjaxPage();
+        $this->endPage(true);
 
         return ob_get_clean();
+    }
+
+    /**
+     * Renders a string as Ajax including assets without end page so it can be called several times.
+     *
+     * @param string $content
+     * @return string Rendered content
+     */
+    public function renderAjaxPartial(string $content): string
+    {
+        ob_start();
+        ob_implicit_flush(false);
+
+        $this->beginPage();
+        $this->head();
+        $this->beginBody();
+        echo $content;
+        $this->endBody();
+        $this->renderEndPage();
+
+        return ob_get_clean();
+    }
+
+    /**
+     * Render of ending page with all attached assets.
+     * This method doesn't mark a page is ended in order to allow to call it several times.
+     */
+    private function renderEndPage()
+    {
+        $this->trigger(self::EVENT_END_PAGE);
+
+        $content = ob_get_clean();
+
+        echo strtr($content, [
+            self::PH_HEAD => $this->renderHeadHtml(),
+            self::PH_BODY_BEGIN => $this->renderBodyBeginHtml(),
+            self::PH_BODY_END => $this->renderBodyEndHtml(true),
+        ]);
+
+        $this->clear();
     }
 
     /**
@@ -179,7 +219,7 @@ class View extends \yii\web\View
         $this->beginBody();
         echo $this->renderFile($viewFile, $params, $context);
         $this->endBody();
-        $this->endAjaxPage();
+        $this->endPage(true);
 
         return ob_get_clean();
     }
@@ -436,24 +476,6 @@ class View extends \yii\web\View
         $this->flushJsConfig();
 
         return parent::endBody();
-    }
-
-    /**
-     * Ending of an AJAX page.
-     */
-    public function endAjaxPage()
-    {
-        $this->trigger(self::EVENT_END_PAGE);
-
-        $content = ob_get_clean();
-
-        echo strtr($content, [
-            self::PH_HEAD => $this->renderHeadHtml(),
-            self::PH_BODY_BEGIN => $this->renderBodyBeginHtml(),
-            self::PH_BODY_END => $this->renderBodyEndHtml(true),
-        ]);
-
-        $this->clear();
     }
 
     /**
