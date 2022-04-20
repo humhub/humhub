3f07bddcf09c0a6357c64bc8092d0f47182459f6
HumHub Translations <info@humhub.com>
2022-04-20 15:19:05 +0200

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/content/messages/es/manage.php
+++ protected/humhub/modules/content/messages/es/manage.php
@@ -1,6 +1,5 @@
 <?php
-
-return [
-    'Are you really sure? *ALL* module data for your profile will be deleted!' => '',
-    'Are you sure? *ALL* module data for this space will be deleted!' => '',
-];
+return array (
+  'Are you really sure? *ALL* module data for your profile will be deleted!' => '¿Estás seguro? ¡Se borrarán *TODOS* los datos del módulo en tu perfil!',
+  'Are you sure? *ALL* module data for this space will be deleted!' => '¿Estás seguro? ¡Se borrarán *TODOS* los datos del módulo en este espacio!',
+);
