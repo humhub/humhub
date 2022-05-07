cd30ee0f4977e952340ca8444c08edd22e81d95c
Marc Farré <23310825+funkycram@users.noreply.github.com>
2022-05-07 10:35:48 +0200

    Update index.php
    Added logo to registration page

--- protected/humhub/modules/user/views/registration/index.php
+++ protected/humhub/modules/user/views/registration/index.php
@@ -1,20 +1,29 @@
 <?php
 
+use humhub\widgets\SiteLogo;
+use yii\bootstrap\ActiveForm;
 use yii\helpers\Html;
 
 $this->pageTitle = Yii::t('UserModule.auth', 'Create Account');
 ?>
 
 <div class="container" style="text-align: center;">
-    <h1 id="app-title" class="animated fadeIn"><?php echo Html::encode(Yii::$app->name); ?></h1>
+    <?= SiteLogo::widget(['place' => 'login']) ?>
+    <br>
+    <h1 id="app-title" class="animated fadeIn">
+        <?= Html::encode(Yii::$app->name) ?>
+    </h1>
     <br/>
     <div class="row">
-        <div id="create-account-form" class="panel panel-default animated bounceIn" style="max-width: 500px; margin: 0 auto 20px; text-align: left;">
-            <div class="panel-heading"><?php echo Yii::t('UserModule.auth', '<strong>Account</strong> registration'); ?></div>
+        <div id="create-account-form" class="panel panel-default animated bounceIn"
+             style="max-width: 500px; margin: 0 auto 20px; text-align: left;">
+            <div class="panel-heading">
+                <?= Yii::t('UserModule.auth', '<strong>Account</strong> registration') ?>
+            </div>
             <div class="panel-body">
-                <?php $form = \yii\bootstrap\ActiveForm::begin(['id' => 'registration-form', 'enableClientValidation' => false]); ?>
+                <?php $form = ActiveForm::begin(['id' => 'registration-form', 'enableClientValidation' => false]); ?>
                 <?= $hForm->render($form); ?>
-                <?php \yii\bootstrap\ActiveForm::end(); ?>
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
     </div>
@@ -30,12 +39,12 @@ $this->pageTitle = Yii::t('UserModule.auth', 'Create Account');
     })
 
     // Shake panel after wrong validation
-<?php foreach ($hForm->models as $model) : ?>
+    <?php foreach ($hForm->models as $model) : ?>
     <?php if ($model->hasErrors()) : ?>
-            $('#create-account-form').removeClass('bounceIn');
-            $('#create-account-form').addClass('shake');
-            $('#app-title').removeClass('fadeIn');
+    $('#create-account-form').removeClass('bounceIn');
+    $('#create-account-form').addClass('shake');
+    $('#app-title').removeClass('fadeIn');
     <?php endif; ?>
-<?php endforeach; ?>
+    <?php endforeach; ?>
 
 </script>
