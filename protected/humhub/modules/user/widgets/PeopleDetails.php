d97330a30a108e02f59b52c863c6780ddd79c670
funkycram <contact@marc.fun>
2022-05-12 14:11:45 +0200

    Enh #5695: Truncate long profile texts in People cards

--- protected/humhub/modules/user/widgets/PeopleDetails.php
+++ protected/humhub/modules/user/widgets/PeopleDetails.php
@@ -8,12 +8,13 @@
 namespace humhub\modules\user\widgets;
 
 use humhub\components\Widget;
+use humhub\libs\StringHelper;
 use humhub\modules\user\models\ProfileField;
 use humhub\modules\user\models\User;
 
 /**
  * PeopleDetails shows details for back side of the people card
- * 
+ *
  * @since 1.9
  * @author Luke
  */
@@ -84,7 +85,7 @@ class PeopleDetails extends Widget
             return false;
         }
 
-        return $profileFields[$internalName]->getUserValue($this->user, false);
+        return StringHelper::truncate(profileFields[$internalName]->getUserValue($this->user, false), 200, '...', true);
     }
 
 }
