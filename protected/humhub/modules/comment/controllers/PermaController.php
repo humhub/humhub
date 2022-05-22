f5406b807c71d40015b58fae0942a9c9056e1a5a
Marc Farré <23310825+funkycram@users.noreply.github.com>
2022-05-22 16:06:47 +0200

    Update PermaController.php
    We should be able to be redirected to the content if it's a global content.
    I don't know if there was a reason to send a not found exception for global contents, but removing this check, the redirection works well with global contents, and email notification buttons now redirects to the global content.

--- protected/humhub/modules/comment/controllers/PermaController.php
+++ protected/humhub/modules/comment/controllers/PermaController.php
@@ -31,7 +31,7 @@ class PermaController extends Controller
     {
         $comment = Comment::findOne(['id' => $id]);
 
-        if (!$comment || !$comment->content || !$comment->canRead() || !$comment->content->container) {
+        if (!$comment || !$comment->content || !$comment->canRead()) {
             throw new NotFoundHttpException();
         }
 
