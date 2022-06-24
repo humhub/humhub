8ccdff0fb511a507a5164a8fd50a43a0d1eb0134
HumHub Translations <hello@humhub.com>
2022-06-24 16:24:06 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/stream/messages/de/base.php
+++ protected/humhub/modules/stream/messages/de/base.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Show {i} more.' => 'Zeige {i} weitere.',
-    'The content could not be found.' => 'Inhalt konnte nicht gefunden werden',
-    'You are not allowed to view this content.' => 'Du hast keine Berechtigung diesen Inhalt zu sehen',
-    'Date "From" should be before "To"!' => '',
-];
+return array (
+  'Date "From" should be before "To"!' => 'Datum "von" muss vor "bis" liegen!',
+  'Show {i} more.' => 'Zeige {i} weitere.',
+  'The content could not be found.' => 'Inhalt konnte nicht gefunden werden',
+  'You are not allowed to view this content.' => 'Du hast keine Berechtigung diesen Inhalt zu sehen',
+);
