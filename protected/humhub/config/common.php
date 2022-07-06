7f17546fbc1703982dfc7c0fdba7e772d7d1203a
Lucas Bartholemy <luke-@users.noreply.github.com>
2022-07-06 16:09:03 +0200

    Update common.php

--- protected/humhub/config/common.php
+++ protected/humhub/config/common.php
@@ -24,7 +24,7 @@ if (!defined('PKCS7_DETACHED')) {
 
 $config = [
     'name' => 'HumHub',
-    'version' => '1.11.3',
+    'version' => '1.11.4',
     'minRecommendedPhpVersion' => '7.4',
     'minSupportedPhpVersion' => '7.4',
     'basePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
