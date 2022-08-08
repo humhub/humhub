ae27cd358d733ac2991ba1fd962db96c87e8c411
HumHub Translations <hello@humhub.com>
2022-08-08 17:24:08 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/stream/messages/nl/base.php
+++ protected/humhub/modules/stream/messages/nl/base.php
@@ -1,8 +1,7 @@
 <?php
-
-return [
-    'Show {i} more.' => 'Toon {i} meer.',
-    'The content could not be found.' => 'De inhoud is niet gevonden.',
-    'You are not allowed to view this content.' => 'U mag deze inhoud niet bekijken.',
-    'Date "From" should be before "To"!' => '',
-];
+return array (
+  'Date "From" should be before "To"!' => 'Datum "Vanaf" moet eerder zijn dan "Tot"!',
+  'Show {i} more.' => 'Toon {i} meer.',
+  'The content could not be found.' => 'De inhoud is niet gevonden.',
+  'You are not allowed to view this content.' => 'U mag deze inhoud niet bekijken.',
+);
--- protected/humhub/modules/stream/messages/nl/filter.php
+++ protected/humhub/modules/stream/messages/nl/filter.php
@@ -1,13 +1,12 @@
 <?php
-
-return [
-    'Author' => 'Auteur',
-    'Content' => 'inhoud',
-    'Content Type' => 'Inhoudstype',
-    'Scope' => 'Bereik',
-    'Sorting' => 'Sorteer',
-    'Topic' => 'Rubriek',
-    'Visibility' => 'Zichtbaarheid',
-    'Date from' => '',
-    'Date to' => '',
-];
+return array (
+  'Author' => 'Auteur',
+  'Content' => 'inhoud',
+  'Content Type' => 'Inhoudstype',
+  'Date from' => 'Datum vanaf',
+  'Date to' => 'Datum tot',
+  'Scope' => 'Bereik',
+  'Sorting' => 'Sorteer',
+  'Topic' => 'Rubriek',
+  'Visibility' => 'Zichtbaarheid',
+);
