52a8beb471a2b8b3ae6174bb7112b49638de95f9
HumHub Translations <hello@humhub.com>
2022-11-11 10:23:55 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/post/messages/de/base.php
+++ protected/humhub/modules/post/messages/de/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    'Allow others to create new posts on your profile page' => 'Anderen Benutzern erlauben, auf deiner Profilseite neue Beiträge zu erstellen',
-    'Allows the user to create posts' => 'Erlaubt dem Benutzer das Erstellen von Beiträgen',
-    'Create post' => 'Beitrag erstellen',
-    'Edit your post...' => 'Bearbeite deinen Beitrag ...',
-    'Post could not be saved!' => 'Beitrag konnte nicht gespeichert werden!',
-    'The post must not be empty!' => 'Der Beitrag darf nicht leer sein!',
-    'This space is archived.' => 'Dieser Space ist archiviert.',
-    'What\'s on your mind?' => 'Was möchtest du mitteilen?',
-    'post' => 'Beitrag',
-    'Post' => '',
-];
+return array (
+  'Allow others to create new posts on your profile page' => 'Anderen Benutzern erlauben, auf deiner Profilseite neue Beiträge zu erstellen',
+  'Allows the user to create posts' => 'Erlaubt dem Benutzer das Erstellen von Beiträgen',
+  'Create post' => 'Beitrag erstellen',
+  'Edit your post...' => 'Bearbeite deinen Beitrag ...',
+  'Post' => 'Beitrag',
+  'Post could not be saved!' => 'Beitrag konnte nicht gespeichert werden!',
+  'The post must not be empty!' => 'Der Beitrag darf nicht leer sein!',
+  'This space is archived.' => 'Dieser Space ist archiviert.',
+  'What\'s on your mind?' => 'Was möchtest du mitteilen?',
+  'post' => 'Beitrag',
+);
