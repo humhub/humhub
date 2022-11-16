3285552828abdb3342d00d98f79aa9f542039f60
HumHub Translations <hello@humhub.com>
2022-11-16 11:06:52 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/post/messages/fr/base.php
+++ protected/humhub/modules/post/messages/fr/base.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    'Allow others to create new posts on your profile page' => 'Autoriser les utilisateurs à publier sur votre profil',
-    'Allows the user to create posts' => 'Autoriser les utilisateurs à publier',
-    'Create post' => 'Créer une publication',
-    'Edit your post...' => 'Éditer la publication...',
-    'Post could not be saved!' => 'La publication n’a pas pu être enregistrée !',
-    'The post must not be empty!' => 'La publication ne peut pas être vide !',
-    'This space is archived.' => 'Cet espace est archivé',
-    'What\'s on your mind?' => 'Publier quelque chose...',
-    'post' => 'Publication',
-    'Post' => '',
-];
+return array (
+  'Allow others to create new posts on your profile page' => 'Autoriser les autres à publier sur votre profil',
+  'Allows the user to create posts' => 'Autoriser les utilisateurs à publier',
+  'Create post' => 'Créer une publication',
+  'Edit your post...' => 'Éditer votre publication…',
+  'Post' => 'Publication',
+  'Post could not be saved!' => 'La publication n’a pas pu être enregistrée !',
+  'The post must not be empty!' => 'La publication ne peut pas être vide !',
+  'This space is archived.' => 'Cet espace est archivé.',
+  'What\'s on your mind?' => 'Publier quelque chose...',
+  'post' => 'publication',
+);
