c49309322f7a4a7cab699384bbba9064e58c79f2
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2023-01-12 22:20:02 +0400

    Fix private user visibility by guest (#6039)
    * Fix private user visibility by guest
    
    * Test user visibility on guest mode
    
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- protected/humhub/modules/user/tests/codeception/acceptance/InvisibleUserCest.php
+++ protected/humhub/modules/user/tests/codeception/acceptance/InvisibleUserCest.php
@@ -2,12 +2,13 @@
 
 namespace user\acceptance;
 
+use humhub\modules\user\models\User;
 use user\AcceptanceTester;
 
 class InvisibleUserCest
 {
 
-    public function testUserImpersonation(AcceptanceTester $I)
+    public function testUserInvisible(AcceptanceTester $I)
     {
         $userName = 'Sara Tester';
         $I->wantTo('ensure that user invisibility works');
@@ -60,6 +61,59 @@ class InvisibleUserCest
         $I->click('Members', '.statistics');
         $I->waitForText('Members', null, '#globalModal');
         $I->dontSee($userName, '#globalModal');
+    }
+
+    public function testUserVisibilityOnGuestMode(AcceptanceTester $I)
+    {
+        $I->wantTo('test profile visibilities on guest mode');
+        $I->amOnUser1Profile();
+        $I->waitForText('Please sign in');
+        $I->see('If you\'re already a member, please login with your username/email and password.');
+
+        $I->amGoingTo('enable guest mode');
+        $I->amAdmin();
+        $I->allowGuestAccess();
+
+        $I->amGoingTo('make user public');
+        $I->amUser1(true);
+        $I->amOnPage('/user/account/edit-settings');
+        $I->waitForText('Profile visibility');
+        $I->selectOption('#accountsettings-visibility', User::VISIBILITY_ALL);
+        $I->click('Save');
+        $I->seeSuccess();
+
+        $I->amGoingTo('view public user by guest');
+        $I->logout();
+        $I->amOnUser1Profile();
+        $I->waitForText('Peter Tester');
+
+        $I->amGoingTo('make user visible only for registered users');
+        $I->amUser1();
+        $I->amOnPage('/user/account/edit-settings');
+        $I->waitForText('Profile visibility');
+        $I->selectOption('#accountsettings-visibility', User::VISIBILITY_REGISTERED_ONLY);
+        $I->click('Save');
+        $I->seeSuccess();
+
+        $I->amGoingTo('view user available only for registered users by guest');
+        $I->logout();
+        $I->amOnUser1Profile();
+        $I->waitForText('Login required');
+        $I->see('You need to login to view this user profile!');
+
+        $I->amGoingTo('make user visible only for registered users');
+        $I->amAdmin();
+        $I->amOnRoute(['/admin/user/edit', 'id' => 2]);
+        $I->waitForText('Visibility');
+        $I->selectOption('#usereditform-visibility', User::VISIBILITY_HIDDEN);
+        $I->click('Save');
+        $I->seeSuccess();
+
+        $I->amGoingTo('view private/inivisile user by guest');
+        $I->logout();
+        $I->amOnUser1Profile();
+        $I->waitForText('Login required');
+        $I->see('You need to login to view this user profile!');
     }
 
 }
