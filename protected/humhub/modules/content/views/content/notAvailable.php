70f680e55afad09f809a46971cc61640511689b4
Yuriy Bakhtin <yurybakh@gmail.com>
2023-03-24 12:37:45 +0300

    Update style of message of not available content

--- protected/humhub/modules/content/views/content/notAvailable.php
+++ protected/humhub/modules/content/views/content/notAvailable.php
@@ -8,7 +8,7 @@
 <div class="panel panel-default">
     <div class="panel-body">
         <div class="alert alert-danger" style="margin:0">
-            <h3><?= Yii::t('ContentModule.base', 'Access denied') ?></h3>
+            <h4 style="margin-top:0;font-weight:500"><?= Yii::t('ContentModule.base', 'Access denied') ?></h4>
             <p class="text-muted"><?= Yii::t('ContentModule.base', 'You do not have permission to access this content, as it is reserved for members of this Space. Please become a member or apply for membership. The available options for membership will depend on the Space\'s settings.') ?></p>
         </div>
     </div>
