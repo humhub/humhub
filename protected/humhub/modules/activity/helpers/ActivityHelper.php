aa8929862839daac6bd3336c9b66eae987dd35d3
Yuriy Bakhtin <yurybakh@gmail.com>
2023-04-25 16:12:28 +0200

    Updated activities to same state as parent record

--- protected/humhub/modules/activity/helpers/ActivityHelper.php
+++ protected/humhub/modules/activity/helpers/ActivityHelper.php
@@ -5,33 +5,44 @@ namespace humhub\modules\activity\helpers;
 use humhub\components\ActiveRecord;
 use humhub\modules\activity\models\Activity;
 use Yii;
+use yii\db\ActiveQuery;
 
 class ActivityHelper
 {
 
-    public static function deleteActivitiesForRecord(?ActiveRecord $record)
+    public static function getActivitiesQuery(?ActiveRecord $record): ?ActiveQuery
     {
         if ($record === null) {
-            return;
+            return null;
         }
 
         $pk = $record->getPrimaryKey();
 
         // Check if primary key exists and is not array (multiple pk)
-        if ($pk !== null && !is_array($pk)) {
-
-            $modelsActivity = Activity::find()->where([
-                'object_id' => $pk,
-                'object_model' => get_class($record)
-            ])->each();
-
-            foreach ($modelsActivity as $activity) {
-                /* @var Activity $activity */
-                $activity->hardDelete();
-            }
-
-            Yii::debug('Deleted activities for ' . get_class($record) . " with PK " . $pk, 'activity');
+        if ($pk === null || is_array($pk)) {
+            return null;
         }
+
+        return Activity::find()->where([
+            'object_id' => $pk,
+            'object_model' => get_class($record)
+        ]);
+    }
+
+    public static function deleteActivitiesForRecord(?ActiveRecord $record): void
+    {
+        $activitiesQuery = self::getActivitiesQuery($record);
+
+        if ($activitiesQuery === null) {
+            return;
+        }
+
+        foreach ($activitiesQuery->each() as $activity) {
+            /* @var Activity $activity */
+            $activity->hardDelete();
+        }
+
+        Yii::debug('Deleted activities for ' . get_class($record) . ' with PK ' . $record->getPrimaryKey(), 'activity');
     }
 
 }
