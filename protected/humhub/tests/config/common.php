5b760d549bbc323b6743973f83a22bca42310d74
Gev <gevorgmansuryan@gmail.com>
2023-04-26 20:35:47 +0400

    Enh/right sidebar swipe

--- protected/humhub/tests/config/common.php
+++ protected/humhub/tests/config/common.php
@@ -3,7 +3,7 @@
 return [
     'components' => [
         'db' => [
-            'dsn' => 'mysql:host=mariadb;dbname=humhub_test',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=humhub_test',
             'username' => 'root',
             'password' => '123123',
             'charset' => 'utf8',
