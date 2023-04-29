c2978265763f42c7fb7dd87f11228f018318aadd
yuriimaz <120715308+yuriimaz@users.noreply.github.com>
2023-04-29 20:40:31 +0300

    Enh #29: AutoStart Tour for new Users (#6249)
    * Enh #29: AutoStart Tour for new Users
    
    * Added getIsFirstLogin and setFirstLoginDone methods in Tour module.
    Removed selfSetUpdatedBy method from User component.
    
    * Added since 1.15 for new param and methods and var method documented.
    Added AutoTourCest acceptance test for check auto tour.
    
    * Changed AutoTourCest acceptance test for check auto tour.
    Removed @author for new param.
    
    * Removed author tag for new methods.
    
    * Fixed acceptance test for Auto Tour
    
    * Update CHANGELOG-DEV.md
    
    ---------
    
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- CHANGELOG-DEV.md
+++ CHANGELOG-DEV.md
@@ -11,7 +11,8 @@ HumHub Changelog
 - Enh #6236: Logging: Show log entries from migrations with category migration
 - Fix #6216: Spaces icon in admin menu
 - Fix #6229: Bug on saving forms: Zend OPcache API is restricted by "restrict_api"
-- Enh #6240: Add ability to set showAtDashboard in SpaceMembership::addMember method 
+- Enh #6240: Add ability to set showAtDashboard in SpaceMembership::addMember method
+- Enh #29: AutoStart Tour for new Users
 - Fix #6243: Do not send notification when ApprovalRequest is not valid
 - Enh #6215: Added `LongRunningActiveJob` to avoid timeout for long running queue jobs
 - Enh #6253: Remove `DefaultSwipeOnMobile` checkbox
--- protected/humhub/modules/tour/Module.php
+++ protected/humhub/modules/tour/Module.php
@@ -4,7 +4,7 @@ namespace humhub\modules\tour;
 
 use Yii;
 use humhub\modules\tour\widgets\Dashboard;
-
+use humhub\modules\user\models\User;
 
 /**
  * This module shows an introduction tour for new users
@@ -25,17 +25,68 @@ class Module extends \humhub\components\Module
     public $isCoreModule = true;
 
     /**
+     * @var bool enable auto-start tour for new accounts
+     * @since 1.15
+     */
+    public $autoStartOnNewAccounts = false;
+
+    /**
+     * Check is first login
+     * @since 1.15
+     *
+     * @param User $user
+     * @return bool
+     */
+    private function getIsFirstLogin(User $user): bool
+    {
+        $settings = Yii::$app->getModule('tour')->settings;
+        $showWelcome = (
+            $user->id == 1 &&
+            Yii::$app->getModule('installer')->settings->get('sampleData') != 1 &&
+            $settings->user()->get('welcome') != 1
+        );
+
+        return (
+            !$showWelcome &&
+            $user->updated_by === null &&
+            $user->created_at === $user->updated_at
+        );
+    }
+
+    /**
+     * Set updated_by for current user
+     * @since 1.15
+     *
+     * @param User $user
+     * @return void
+     */
+    private function setFirstLoginDone(User $user)
+    {
+        $user->updateAttributes(['updated_by' => $user->id]);
+    }
+
+    /**
      * Event Callback
      */
     public static function onDashboardSidebarInit($event)
     {
-        if (Yii::$app->user->isGuest)
+        if (Yii::$app->user->isGuest) {
             return;
+        }
 
-        $settings = Yii::$app->getModule('tour')->settings;
+        /** @var Module $module */
+        $module = Yii::$app->getModule('tour');
+        $settings = $module->settings;
+
         if ($settings->get('enable') == 1 && $settings->user()->get("hideTourPanel") != 1) {
-            $event->sender->addWidget(Dashboard::class, [], ['sortOrder' => 100]);
+             $module->autoStartOnNewAccounts = $module->getIsFirstLogin(Yii::$app->user->identity);
+            if ($module->autoStartOnNewAccounts) {
+                $module->setFirstLoginDone(Yii::$app->user->identity);
+
+                Yii::$app->getResponse()->redirect(['/dashboard/dashboard', 'tour' => true]);
+            } else {
+                $event->sender->addWidget(Dashboard::class, [], ['sortOrder' => 100]);
+            }
         }
     }
-
 }
