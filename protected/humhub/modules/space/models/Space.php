884efedb45710527fd968da09ddc2bcfdbc77b1c
Marc Farré <contact@marc.fun>
2023-05-02 15:14:40 +0200

    Removed required sort_order attribute in Space model

--- protected/humhub/modules/space/models/Space.php
+++ protected/humhub/modules/space/models/Space.php
@@ -8,29 +8,29 @@
 
 namespace humhub\modules\space\models;
 
+use humhub\components\behaviors\GUID;
+use humhub\modules\content\components\ContentContainerActiveRecord;
 use humhub\modules\content\components\ContentContainerSettingsManager;
-use humhub\modules\search\interfaces\Searchable;
+use humhub\modules\content\models\Content;
 use humhub\modules\search\events\SearchAddEvent;
+use humhub\modules\search\interfaces\Searchable;
 use humhub\modules\search\jobs\DeleteDocument;
 use humhub\modules\search\jobs\UpdateDocument;
-use humhub\modules\space\behaviors\SpaceModelMembership;
+use humhub\modules\space\activities\Created;
 use humhub\modules\space\behaviors\SpaceController;
+use humhub\modules\space\behaviors\SpaceModelMembership;
 use humhub\modules\space\components\ActiveQuerySpace;
+use humhub\modules\space\components\UrlValidator;
 use humhub\modules\space\Module;
-use humhub\modules\user\behaviors\Followable;
-use humhub\components\behaviors\GUID;
 use humhub\modules\space\permissions\CreatePrivateSpace;
 use humhub\modules\space\permissions\CreatePublicSpace;
-use humhub\modules\space\components\UrlValidator;
-use humhub\modules\space\activities\Created;
-use humhub\modules\content\components\ContentContainerActiveRecord;
-use humhub\modules\content\models\Content;
-use humhub\modules\user\helpers\AuthHelper;
-use humhub\modules\user\models\GroupSpace;
-use humhub\modules\user\models\User;
-use humhub\modules\user\models\Follow;
-use humhub\modules\user\models\Invite;
 use humhub\modules\space\widgets\Wall;
+use humhub\modules\user\behaviors\Followable;
+use humhub\modules\user\helpers\AuthHelper;
+use humhub\modules\user\models\Follow;
+use humhub\modules\user\models\GroupSpace;
+use humhub\modules\user\models\Invite;
+use humhub\modules\user\models\User;
 use humhub\modules\user\models\User as UserModel;
 use Yii;
 
@@ -118,7 +118,7 @@ class Space extends ContentContainerActiveRecord implements Searchable
     {
         $rules = [
             [['join_policy', 'visibility', 'status', 'sort_order', 'auto_add_new_members', 'default_content_visibility'], 'integer'],
-            [['name', 'sort_order'], 'required'],
+            [['name'], 'required'],
             [['description', 'about', 'color'], 'string'],
             [['tagsField', 'blockedUsersField'], 'safe'],
             [['description'], 'string', 'max' => 100],
