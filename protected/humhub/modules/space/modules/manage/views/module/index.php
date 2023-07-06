8d9d3b286122d9e736365a4b08eeef4610bf2049
Yuriy Bakhtin <yurybakh@gmail.com>
2023-07-06 16:00:49 +0200

    Fix space and profile module list wrapper

--- protected/humhub/modules/space/modules/manage/views/module/index.php
+++ protected/humhub/modules/space/modules/manage/views/module/index.php
@@ -3,38 +3,35 @@
 use humhub\modules\content\components\ContentContainerModule;
 use humhub\modules\content\widgets\ContainerModule;
 use humhub\modules\space\models\Space;
-use humhub\modules\ui\view\helpers\ThemeHelper;
 use yii\web\View;
 
 /* @var View $this */
 /* @var Space $space */
 /* @var ContentContainerModule[] $availableModules */
 ?>
-<div class="<?php if (ThemeHelper::isFluid()) : ?>container-fluid<?php else: ?>container container-content-modules-col-3<?php endif; ?> container-cards container-modules container-content-modules">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4><?= Yii::t('SpaceModule.manage', '<strong>Space</strong> Modules'); ?></h4>
-            <div class="help-block"><?= Yii::t('SpaceModule.manage', 'Choose the modules you want to use for this Space. In order for the modules to be available to you here, they must have been previously installed by administrators of the network using the admin panel. If you cannot deactivate individual modules, it is because they have been set as the default for the entire network.') ?></div>
-        </div>
-        <div class="panel-body">
-            <?php if (empty($availableModules)) : ?>
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <?= Yii::t('SpaceModule.manage', 'Currently there are no modules available for this space!'); ?>
-                        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?= Yii::t('SpaceModule.manage', '<strong>Space</strong> Modules') ?>
+        <div class="help-block"><?= Yii::t('SpaceModule.manage', 'Choose the modules you want to use for this Space. In order for the modules to be available to you here, they must have been previously installed by administrators of the network using the admin panel. If you cannot deactivate individual modules, it is because they have been set as the default for the entire network.') ?></div>
+    </div>
+    <div class="panel-body">
+        <?php if (empty($availableModules)) : ?>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <?= Yii::t('SpaceModule.manage', 'Currently there are no modules available for this space!'); ?>
                     </div>
                 </div>
-            <?php endif; ?>
-
-            <div class="modules-group">
-                <?php foreach ($availableModules as $module) : ?>
-                    <?= ContainerModule::widget([
-                        'contentContainer' => $space,
-                        'module' => $module,
-                    ]); ?>
-                <?php endforeach; ?>
             </div>
+        <?php endif; ?>
+
+        <div class="modules-group">
+            <?php foreach ($availableModules as $module) : ?>
+                <?= ContainerModule::widget([
+                    'contentContainer' => $space,
+                    'module' => $module,
+                ]); ?>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
--- protected/humhub/modules/user/views/account/editModules.php
+++ protected/humhub/modules/user/views/account/editModules.php
@@ -7,31 +7,29 @@ use humhub\modules\user\models\User;
 /* @var User $user */
 /* @var ContentContainerModule[] $modules */
 ?>
-<div class="container container-cards container-modules container-content-modules container-content-modules-col-3">
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h4><?= Yii::t('UserModule.manage', '<strong>Profile</strong> modules'); ?></h4>
-            <div class="help-block"><?= Yii::t('UserModule.manage', 'Similar to Spaces, your personal profile also allows you to use modules. Please keep in mind that information you share on your profile is available to other users of the network.') ?></div>
-        </div>
-        <div class="panel-body">
-            <?php if (empty($modules)) : ?>
-                <div class="col-md-12">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <?= Yii::t('UserModule.manage', 'Currently there are no modules available for you!'); ?>
-                        </div>
+<div class="panel panel-default">
+    <div class="panel-heading">
+        <?= Yii::t('UserModule.manage', '<strong>Profile</strong> modules') ?>
+        <div class="help-block"><?= Yii::t('UserModule.manage', 'Similar to Spaces, your personal profile also allows you to use modules. Please keep in mind that information you share on your profile is available to other users of the network.') ?></div>
+    </div>
+    <div class="panel-body">
+        <?php if (empty($modules)) : ?>
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <?= Yii::t('UserModule.manage', 'Currently there are no modules available for you!'); ?>
                     </div>
                 </div>
-            <?php endif; ?>
-
-            <div class="modules-group">
-                <?php foreach ($modules as $module) : ?>
-                    <?= ContainerModule::widget([
-                        'contentContainer' => $user,
-                        'module' => $module,
-                    ]); ?>
-                <?php endforeach; ?>
             </div>
+        <?php endif; ?>
+
+        <div class="modules-group">
+            <?php foreach ($modules as $module) : ?>
+                <?= ContainerModule::widget([
+                    'contentContainer' => $user,
+                    'module' => $module,
+                ]); ?>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
