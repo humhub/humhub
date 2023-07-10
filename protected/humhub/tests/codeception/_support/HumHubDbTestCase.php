75fa497637994f40c5dce0e86210ad8a62c69b49
Martin Rüegg <martin.rueegg@metaworx.ch>
2023-07-10 21:07:48 +0200

    Enh UUID validator

--- protected/humhub/tests/codeception/_support/HumHubDbTestCase.php
+++ protected/humhub/tests/codeception/_support/HumHubDbTestCase.php
@@ -9,6 +9,7 @@ use Codeception\Configuration;
 use Codeception\Exception\ModuleException;
 use Codeception\Module;
 use Codeception\Module\Yii2;
+use humhub\libs\UUID;
 use humhub\models\UrlOembed;
 use humhub\modules\activity\tests\codeception\fixtures\ActivityFixture;
 use humhub\modules\content\tests\codeception\fixtures\ContentContainerFixture;
@@ -272,6 +273,30 @@ class HumHubDbTestCase extends Unit
         /** @noinspection PhpUnhandledExceptionInspection */
         $message = $this->getYiiModule()->grabLastSentEmail();
         $this->assertEquals($subject, str_replace(["\n", "\r"], '', $message->getSubject()));
+    }
+
+    public static function assertUUID($value, bool $allowNull = false, bool $strict = false, $message = '')
+    {
+        if ($allowNull && $value === null) {
+            return;
+        }
+
+        // validate UUID without changing the input (other than trimming)
+        $uuid = UUID::validate($value, null, null, null);
+
+        static::assertNotNull($uuid, $message);
+
+        if ($strict) {
+            static::assertEquals($uuid, $value, $message);
+        }
+    }
+
+    public static function assertNotUUID($value, $message = '')
+    {
+        // validate UUID without changing the input (other than trimming)
+        $uuid = UUID::validate($value, null, null, null);
+
+        static::assertNull($uuid, $message);
     }
 
     /**
