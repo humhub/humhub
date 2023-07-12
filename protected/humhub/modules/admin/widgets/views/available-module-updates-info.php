2489ba68ab8c438d8e898223b9b3b721c53a6e40
Yuriy Bakhtin <yurybakh@gmail.com>
2023-07-12 11:58:19 +0200

    Update wording

--- protected/humhub/modules/admin/widgets/views/available-module-updates-info.php
+++ protected/humhub/modules/admin/widgets/views/available-module-updates-info.php
@@ -10,7 +10,7 @@ use humhub\modules\marketplace\widgets\MarketplaceLink;
 /* @var int $count */
 ?>
 <div class="modules-updates-info">
-    <?= MarketplaceLink::warning(Yii::t('AdminModule.base', 'Install updates'))->right()->sm() ?>
-    <strong><?= Yii::t('AdminModule.base', 'Updates ready for {count} of your modules', ['count' => $count]) ?></strong><br>
+    <?= MarketplaceLink::warning(Yii::t('AdminModule.base', 'Install Updates'))->right()->sm() ?>
+    <strong><?= Yii::t('AdminModule.base', 'Updates available for {count,plural,=1{# of your module} other{# of your modules}}', ['count' => $count]) ?></strong><br>
     <?= Yii::t('AdminModule.base', 'Keep your system up-to-date and benefit from the latest improvements.') ?>
 </div>
