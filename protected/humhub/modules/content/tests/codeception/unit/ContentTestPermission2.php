3b838ca9ad1d705331a01849f921018926432782
Paul <45032974+hide-me@users.noreply.github.com>
2023-07-29 13:59:10 +0300

    Cleaned old comment

--- protected/humhub/modules/content/tests/codeception/unit/ContentTestPermission2.php
+++ protected/humhub/modules/content/tests/codeception/unit/ContentTestPermission2.php
@@ -3,25 +3,13 @@
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
  * @license https://www.humhub.com/licences
- *
  */
-
-/**
- * Created by PhpStorm.
- * User: buddha
- * Date: 27.07.2017
- * Time: 00:06
- */
-
 namespace humhub\modules\content\tests\codeception\unit;
-
 
 use humhub\libs\BasePermission;
 
 class ContentTestPermission2 extends BasePermission
 {
     public $moduleId = 'content';
-
     public $id = 'content-test-permission';
-
 }
