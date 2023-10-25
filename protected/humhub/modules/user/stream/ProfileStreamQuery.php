95058aab6d70c3909d6034b9a9ae8b505994f8b7
Yuriy Bakhtin <yurybakh@gmail.com>
2023-10-25 11:24:45 +0200

    Fix profile stream query filters

--- protected/humhub/modules/user/stream/ProfileStreamQuery.php
+++ protected/humhub/modules/user/stream/ProfileStreamQuery.php
@@ -24,6 +24,10 @@ class ProfileStreamQuery extends ContentContainerStreamQuery
      */
     public function beforeApplyFilters()
     {
+        parent::beforeApplyFilters();
+
+        $this->removeFilterHandler(ContentContainerStreamFilter::class);
+
         // The default scope may be overwritten by first request, the real default is handled in the stream filter navigation
         $this->addFilterHandler(new IncludeAllContributionsFilter([
             'container' => $this->container,
@@ -31,9 +35,5 @@ class ProfileStreamQuery extends ContentContainerStreamQuery
                 ? IncludeAllContributionsFilter::SCOPE_ALL
                 : IncludeAllContributionsFilter::SCOPE_PROFILE
         ]));
-
-        parent::beforeApplyFilters();
-
-        $this->removeFilterHandler(ContentContainerStreamFilter::class);
     }
 }
