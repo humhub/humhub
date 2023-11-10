72898a77c7c1f5a08680db8971c36d436e9a25c2
Yuriy Bakhtin <yurybakh@gmail.com>
2023-11-10 13:15:24 +0100

    Update tests

--- protected/humhub/modules/content/tests/codeception/unit/widgets/RichTextEmailHtmlConverterTest.php
+++ protected/humhub/modules/content/tests/codeception/unit/widgets/RichTextEmailHtmlConverterTest.php
@@ -46,25 +46,58 @@ class RichTextEmailHtmlConverterTest extends HumHubDbTestCase
             '<p>Test<img src="http://localhost/index-test.php?r=file%2Ffile%2Fdownload&amp;guid=' . $file->guid . '&amp;hash_sha1=&amp;token=' . $token . '" alt="test_image.jpg" style="max-width: 100%;">Test</p>', [
             RichTextToEmailHtmlConverter::OPTION_RECEIVER_USER => $admin,
         ]);
+
+        $this->assertConversionResult(
+            '![](http://local/image.jpg)',
+            '<p><img src="http://local/image.jpg" alt="" style="max-width: 100%;"></p>');
+
+        $this->assertConversionResult(
+            '![Alt text><](http://local/image.jpg "Description text" =200x100)',
+            '<p><img class="center-block" src="http://local/image.jpg" width="200" height="100" alt="Alt text" title="Description text" style="max-width: 100%; display: block; margin: auto;"></p>');
     }
 
-    public function testConvertAdditionalImageAttributesToHtml()
+    public function testConvertImageAlt()
     {
         $this->assertConversionResult(
-            '![Image alt text](http://local/image.jpg "Image description")',
-            '<p><img src="http://local/image.jpg" alt="Image alt text" title="Image description" style="max-width: 100%;"></p>');
+            '![Image <alt> "text"](http://local/image.jpg)',
+            '<p><img src="http://local/image.jpg" alt="Image &lt;alt&gt; &quot;text&quot;" style="max-width: 100%;"></p>');
+    }
+
+    public function testConvertImageDescription()
+    {
+        $this->assertConversionResult(
+        '![](http://local/image.jpg "Image <description> "text"")',
+            '<p><img src="http://local/image.jpg" alt="" title="Image &lt;description&gt; &quot;text&quot;" style="max-width: 100%;"></p>');
+    }
+
+    public function testConvertImageAlignment()
+    {
+        $this->assertConversionResult(
+            '![alt>](http://local/image.jpg "desc")',
+            '<p><img class="pull-right" src="http://local/image.jpg" alt="alt" title="desc" style="max-width: 100%; float: right;"></p>');
 
         $this->assertConversionResult(
-            '![Image alt text>](http://local/image.jpg "Image description" =100x)',
-            '<p><img class="pull-right" src="http://local/image.jpg" width="100" alt="Image alt text" title="Image description" style="max-width: 100%; float: right;"></p>');
+            '![alt<](http://local/image.jpg "desc")',
+            '<p><img class="pull-left" src="http://local/image.jpg" alt="alt" title="desc" style="max-width: 100%; float: left;"></p>');
 
         $this->assertConversionResult(
-            '![Image alt text<](http://local/image.jpg "Image description" =x200)',
-            '<p><img class="pull-left" src="http://local/image.jpg" height="200" alt="Image alt text" title="Image description" style="max-width: 100%; float: left;"></p>');
+            '![alt><](http://local/image.jpg "desc")',
+            '<p><img class="center-block" src="http://local/image.jpg" alt="alt" title="desc" style="max-width: 100%; display: block; margin: auto;"></p>');
+    }
+
+    public function testConvertImageSize()
+    {
+        $this->assertConversionResult(
+            '![alt](http://local/image.jpg =100x)',
+            '<p><img src="http://local/image.jpg" width="100" alt="alt" style="max-width: 100%;"></p>');
 
         $this->assertConversionResult(
-            '![Image alt text><](http://local/image.jpg "Image description" =50x120)',
-            '<p><img class="center-block" src="http://local/image.jpg" width="50" height="120" alt="Image alt text" title="Image description" style="max-width: 100%; display: block; margin: auto;"></p>');
+            '![alt](http://local/image.jpg =x200)',
+            '<p><img src="http://local/image.jpg" height="200" alt="alt" style="max-width: 100%;"></p>');
+
+        $this->assertConversionResult(
+            '![alt](http://local/image.jpg =50x120)',
+            '<p><img src="http://local/image.jpg" width="50" height="120" alt="alt" style="max-width: 100%;"></p>');
     }
 
     private function assertConversionResult($markdown, $expected = null, $options = [])
