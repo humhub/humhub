90bc8b34dda8088be78712a6de528e25bcf09f89
Yuriy Bakhtin <yurybakh@gmail.com>
2024-02-20 18:30:47 +0100

    Meta Search: Improve auto height

--- protected/humhub/widgets/views/searchMenu.php
+++ protected/humhub/widgets/views/searchMenu.php
@@ -26,21 +26,23 @@ SearchAsset::register($this);
         ->action('menu')
         ->options(['data-toggle' => 'dropdown'])
         ->cssClass('dropdown-toggle') ?>
-    <ul id="dropdown-search" class="dropdown-menu">
-        <li class="dropdown-header">
-            <div class="arrow"></div>
-            <?= Yii::t('base', 'Search') ?>
-        </li>
-        <li class="dropdown-search-form">
-            <?= Button::defaultType()
-                ->icon('search')
-                ->action('search')
-                ->cssClass('dropdown-search-button')
-                ->loader(false) ?>
-            <?= Html::input('text', 'keyword', '', ['class' => 'dropdown-search-keyword form-control']) ?>
-        </li>
-        <?php foreach ($searchProviders as $searchProvider) : ?>
-            <?= SearchProviderWidget::widget(['searchProvider' => $searchProvider]) ?>
-        <?php endforeach; ?>
-    </ul>
+    <div id="dropdown-search" class="dropdown-menu">
+        <ul class="dropdown-search-list">
+            <li class="dropdown-header">
+                <div class="arrow"></div>
+                <?= Yii::t('base', 'Search') ?>
+            </li>
+            <li class="dropdown-search-form">
+                <?= Button::defaultType()
+                    ->icon('search')
+                    ->action('search')
+                    ->cssClass('dropdown-search-button')
+                    ->loader(false) ?>
+                <?= Html::input('text', 'keyword', '', ['class' => 'dropdown-search-keyword form-control']) ?>
+            </li>
+            <?php foreach ($searchProviders as $searchProvider) : ?>
+                <?= SearchProviderWidget::widget(['searchProvider' => $searchProvider]) ?>
+            <?php endforeach; ?>
+        </ul>
+    </div>
 <?= Html::endTag('li') ?>
--- static/js/humhub/humhub.ui.search.js
+++ static/js/humhub/humhub.ui.search.js
@@ -8,50 +8,95 @@ humhub.module('ui.search', function(module, require, $) {
     Search.prototype.init = function() {
         const that = this;
 
-        $(document).on('click', '#dropdown-search', function (e) {
+        that.selectors = {
+            panel: '#dropdown-search',
+            list: '.dropdown-search-list',
+            input: 'input.dropdown-search-keyword',
+            provider: '.dropdown-search-provider',
+            providerContent: '.dropdown-search-provider-content',
+            providerCounter: '.dropdown-search-provider-title > span'
+        }
+
+        $(document).on('click', that.selectors.panel, function (e) {
             e.stopPropagation();
         });
 
-        this.$.find('.dropdown-search-keyword').on('keypress', function (e) {
+        that.getInput().on('keypress', function (e) {
             if (e.which === 13) {
                 that.search();
             }
         });
 
-        this.$.find('#dropdown-search').niceScroll({
-            cursorwidth: "7",
-            cursorborder: "",
-            cursorcolor: "#555",
-            cursoropacitymax: "0.2",
+        that.getList().niceScroll({
+            cursorwidth: '7',
+            cursorborder: '',
+            cursorcolor: '#555',
+            cursoropacitymax: '0.2',
             nativeparentscrolling: false,
             railpadding: {top: 0, right: 0, left: 0, bottom: 0}
         });
+
+        that.$.on('shown.bs.dropdown', function () {
+            that.refreshSize();
+        })
+
     }
 
-    Search.prototype.menu = function() {
-        const keywordInput = this.$.find('.dropdown-search-keyword');
-        if (keywordInput.length) {
-            keywordInput.focus();
+    Search.prototype.getPanel = function () {
+        return this.$.find(this.selectors.panel);
+    }
+
+    Search.prototype.getList = function () {
+        return this.$.find(this.selectors.list);
+    }
+
+    Search.prototype.getProviders = function () {
+        return this.$.find(this.selectors.provider);
+    }
+
+    Search.prototype.getInput = function () {
+        return this.$.find(this.selectors.input);
+    }
+
+    Search.prototype.hasInput = function () {
+        const input = this.getInput();
+        return input.length === 1 && input.is(':visible');
+    }
+
+    Search.prototype.menu = function () {
+        if (this.hasInput()) {
+            this.getInput().focus();
         }
     }
 
-    Search.prototype.search = function() {
+    Search.prototype.search = function () {
         const that = this;
 
-        this.$.find('.dropdown-search-provider').each(function () {
+        this.getProviders().each(function () {
             const provider = $(this);
             provider.addClass('provider-searching').show()
-                .find('.dropdown-search-provider-title > span').hide();
-            loader.set(provider.find('.dropdown-search-provider-content'), {size: '8px', css: {padding: '0px'}});
+                .find(that.selectors.providerCounter).hide();
+            loader.set(provider.find(that.selectors.providerContent), {size: '8px', css: {padding: '0px'}});
+
+            that.refreshSize();
 
             const data = {
                 provider: provider.data('provider'),
-                keyword: that.$.find('input.dropdown-search-keyword').val()
+                keyword: that.getInput().val()
             };
             client.post(module.config.url, {data}).then(function (response) {
                 provider.replaceWith(response.html);
+                that.refreshSize();
             });
         });
+    }
+
+    Search.prototype.refreshSize = function () {
+        this.getPanel().css('height', 'auto');
+        const maxHeight = $(window).height() - this.getPanel().offset().top;
+        if (this.getPanel().height() > maxHeight) {
+            this.getPanel().css('height', maxHeight);
+        }
     }
 
     module.export = Search;
