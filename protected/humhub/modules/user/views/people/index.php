b9f370f55c8cfcdf4947f9e2054d98b0b0f8a706
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2024-03-05 14:59:49 +0100

    Change people header with selected group (#6873)

--- protected/humhub/modules/user/views/people/index.php
+++ protected/humhub/modules/user/views/people/index.php
@@ -16,15 +16,7 @@ CardsAsset::register($this);
 <div class="panel panel-default">
 
     <div class="panel-heading">
-        <?php if ($people->isFilteredByGroup()) : ?>
-            <?= Yii::t('UserModule.base', '<strong>Group</strong> members - {group}', ['{group}' => Html::encode($people->filteredGroup->name)]); ?>
-            <?php if (!empty($people->filteredGroup->description)) : ?>
-                <div class="hint-block"><?= Html::encode($people->filteredGroup->description) ?></div>
-            <?php endif; ?>
-        <?php else: ?>
-            <?= Yii::t('UserModule.base', '<strong>People</strong>'); ?>
-        <?php endif; ?>
-
+        <?= Yii::t('UserModule.base', '<strong>People</strong>') ?>
         <?= PeopleHeadingButtons::widget() ?>
     </div>
 
