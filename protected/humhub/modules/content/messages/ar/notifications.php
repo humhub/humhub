a6e163ffd537fcf20dc6acdca527d86e425666e7
HumHub Translations <hello@humhub.com>
2024-05-06 15:32:02 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/content/messages/ar/notifications.php
+++ protected/humhub/modules/content/messages/ar/notifications.php
@@ -1,14 +1,13 @@
 <?php
-
-return [
-    'New Content' => 'محتوى جديد',
-    'Receive Notifications for new content you follow.' => 'تلقي الإخطارات للمحتوى الجديد الذي تتابعه.',
-    'View Online' => 'مشاهدة حية',
-    '{displayName} created {contentTitle}.' => 'قام {displayName} بإنشاء {contentTitle}.',
-    '{displayName} posted on your profile {contentTitle}.' => 'نشر {displayName} في ملفك الشخصي {contentTitle}.',
-    '{originator} just wrote {contentInfo}' => '{originator} كتب {contentInfo} للتو',
-    '{originator} just wrote {contentInfo} in space {space}' => '{originator} كتب للتو {contentInfo} في الفضاء {space}',
-    '{originator} notifies you about {contentInfo}' => 'يبلغك {Originator} عن {contentInfo}',
-    '{originator} notifies you about {contentInfo} in {space}' => 'يبلغك {originator} عن {contentInfo} في {space}',
-    'Your {contentTitle} has been deleted by {displayName} for \'{reason}\'' => '',
-];
+return array (
+  'New Content' => 'محتوى جديد',
+  'Receive Notifications for new content you follow.' => 'تلقي الإشعارات للمحتوى الجديد الذي تتابعه.',
+  'View Online' => 'مشاهدة حية',
+  'Your {contentTitle} has been deleted by {displayName} for \'{reason}\'' => 'تم حذف {contentTitle} الخاص بك بواسطة {displayName} بسبب \'{reason}\'',
+  '{displayName} created {contentTitle}.' => 'قام {displayName} بإنشاء {contentTitle}.',
+  '{displayName} posted on your profile {contentTitle}.' => 'نشر {displayName} في ملفك الشخصي {contentTitle}.',
+  '{originator} just wrote {contentInfo}' => '{originator} كتب {contentInfo} للتو',
+  '{originator} just wrote {contentInfo} in space {space}' => '{originator} كتب للتو {contentInfo} في الفضاء {space}',
+  '{originator} notifies you about {contentInfo}' => 'يبلغك {Originator} عن {contentInfo}',
+  '{originator} notifies you about {contentInfo} in {space}' => 'يبلغك {originator} عن {contentInfo} في {space}',
+);
