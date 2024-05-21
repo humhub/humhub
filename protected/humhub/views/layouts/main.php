f3c17955a0e6123907c65a38e20a4b3aa2e9e54e
Marc Farré <contact@marc.fun>
2024-05-21 10:44:29 +0200

    formatting

--- protected/humhub/views/layouts/main.php
+++ protected/humhub/views/layouts/main.php
@@ -17,55 +17,56 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>">
-<head>
-    <title><?= strip_tags($this->pageTitle) ?></title>
-    <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <?php $this->head() ?>
-    <?= $this->render('head') ?>
-</head>
-<body>
-<?php $this->beginBody() ?>
+    <head>
+        <title><?= strip_tags($this->pageTitle) ?></title>
+        <meta charset="<?= Yii::$app->charset ?>">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+        <?php $this->head() ?>
+        <?= $this->render('head') ?>
+    </head>
 
-<!-- start: first top navigation bar -->
-<div id="topbar-first" class="topbar">
-    <div class="container">
-        <div class="topbar-brand hidden-xs">
-            <?= SiteLogo::widget() ?>
+    <body>
+        <?php $this->beginBody() ?>
+
+        <!-- start: first top navigation bar -->
+        <div id="topbar-first" class="topbar">
+            <div class="container">
+                <div class="topbar-brand hidden-xs">
+                    <?= SiteLogo::widget() ?>
+                </div>
+
+                <div class="topbar-actions pull-right">
+                    <?= AccountTopMenu::widget() ?>
+                </div>
+
+                <div class="notifications pull-right">
+                    <?= NotificationArea::widget() ?>
+                </div>
+            </div>
         </div>
+        <!-- end: first top navigation bar -->
 
-        <div class="topbar-actions pull-right">
-            <?= AccountTopMenu::widget() ?>
+        <!-- start: second top navigation bar -->
+        <div id="topbar-second" class="topbar">
+            <div class="container">
+                <ul class="nav" id="top-menu-nav">
+                    <!-- load space chooser widget -->
+                    <?= Chooser::widget() ?>
+
+                    <!-- load navigation from widget -->
+                    <?= TopMenu::widget() ?>
+                </ul>
+
+                <ul class="nav pull-right" id="search-menu-nav">
+                    <?= TopMenuRightStack::widget() ?>
+                </ul>
+            </div>
         </div>
+        <!-- end: second top navigation bar -->
 
-        <div class="notifications pull-right">
-            <?= NotificationArea::widget() ?>
-        </div>
-    </div>
-</div>
-<!-- end: first top navigation bar -->
+        <?= $content ?>
 
-<!-- start: second top navigation bar -->
-<div id="topbar-second" class="topbar">
-    <div class="container">
-        <ul class="nav" id="top-menu-nav">
-            <!-- load space chooser widget -->
-            <?= Chooser::widget() ?>
-
-            <!-- load navigation from widget -->
-            <?= TopMenu::widget() ?>
-        </ul>
-
-        <ul class="nav pull-right" id="search-menu-nav">
-            <?= TopMenuRightStack::widget() ?>
-        </ul>
-    </div>
-</div>
-<!-- end: second top navigation bar -->
-
-<?= $content ?>
-
-<?php $this->endBody() ?>
-</body>
+        <?php $this->endBody() ?>
+    </body>
 </html>
 <?php $this->endPage() ?>
