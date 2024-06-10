0cec06d07ef6a3c4096be020b936d3fe363466d2
HumHub Translations <hello@humhub.com>
2024-06-10 14:15:51 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/user/messages/ar/manage.php
+++ protected/humhub/modules/user/messages/ar/manage.php
@@ -1,7 +1,6 @@
 <?php
-
-return [
-    '<strong>Profile</strong> modules' => '',
-    'Currently there are no modules available for you!' => '',
-    'Similar to Spaces, your personal profile also allows you to use modules. Please keep in mind that information you share on your profile is available to other users of the network.' => '',
-];
+return array (
+  '<strong>Profile</strong> modules' => 'موديولات<strong>الملف الشخصي</strong>',
+  'Currently there are no modules available for you!' => 'حالياً لا يوجد أي موديولات متاحة لك!',
+  'Similar to Spaces, your personal profile also allows you to use modules. Please keep in mind that information you share on your profile is available to other users of the network.' => 'كما هو الحال مع الباحات، يتيح لك ملفك الشخصي أيضًا استخدام الموديولات. يرجى أن تضع في اعتبارك أن المعلومات التي تشاركها في ملفك الشخصي متاحة لمستخدمي الشبكة الآخرين.',
+);
