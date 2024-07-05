9306a052e897aa7496805fbfda82b466c6a667b1
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2024-07-05 23:30:37 +0300

    Restyle top dropdown notifications (#7101)
    * Restyle top dropdown notifications
    
    * Fix tests for new style of notifications
    
    * Fix tests for new style of notifications
    
    * Restyle notifications
    
    * Restyle notifications
    
    * Restyle notifications
    
    * Restyle notifications
    
    * Wording

--- protected/humhub/modules/notification/views/layouts/web.php
+++ protected/humhub/modules/notification/views/layouts/web.php
@@ -2,52 +2,55 @@
 
 use humhub\modules\notification\models\Notification;
 use humhub\modules\space\models\Space;
+use humhub\modules\space\widgets\Image as SpaceImage;
+use humhub\modules\user\models\User;
+use humhub\modules\user\widgets\Image as UserImage;
 use humhub\widgets\TimeAgo;
 use yii\helpers\Html;
 
-/** @var \humhub\modules\user\models\User $originator */
+/** @var User $originator */
 /** @var Space $space */
 /** @var Notification $record */
 /** @var bool $isNew */
 /** @var string $content */
 /** @var string $url */
 /** @var string $relativeUrl */
-
 ?>
-<li class="<?php if ($isNew): ?>new<?php endif; ?>"
+<li <?php if ($isNew): ?>class="new"<?php endif; ?>
     data-notification-id="<?= $record->id ?>"
-    data-notification-group="<?= !empty($record->baseModel->getGroupkey()) ? Html::encode(get_class($record->baseModel)) . ':' . Html::encode($record->baseModel->getGroupKey()) : '' ?>">
-
-    <a href="<?= isset($relativeUrl) ? $relativeUrl : $url; ?>">
+    data-notification-group="<?= !empty($record->baseModel->getGroupkey())
+        ? Html::encode(get_class($record->baseModel) . ':' . $record->baseModel->getGroupKey())
+        : '' ?>">
+    <a href="<?= $relativeUrl ?? $url ?>">
         <div class="media">
-
-            <!-- show user image -->
-            <?php if ($originator): ?>
-                <img class="media-object img-rounded pull-left"
-                     data-src="holder.js/32x32" alt="32x32"
-                     style="width: 32px; height: 32px;"
-                     src="<?php echo $originator->getProfileImage()->getUrl(); ?>"/>
+            <div class="media-left">
+            <?php if ($originator) : ?>
+                <?= UserImage::widget([
+                    'user' => $originator,
+                    'width' => 32,
+                    'link' => false,
+                    'hideOnlineStatus' => true,
+                ]) ?>
             <?php endif; ?>
-
-            <!-- show space image -->
-            <?php if ($space): ?>
-                <img class="media-object img-rounded img-space pull-left"
-                     data-src="holder.js/20x20" alt="20x20"
-                     style="width: 20px; height: 20px;"
-                     src="<?= $space->getProfileImage()->getUrl(); ?>">
+            <?php if ($space instanceof Space) : ?>
+                <?= SpaceImage::widget([
+                    'space' => $space,
+                    'width' => 20,
+                    'link' => false,
+                    'htmlOptions' => ['class' => 'img-space'],
+                ]) ?>
             <?php endif; ?>
-
-            <!-- show content -->
-            <div class="media-body">
-
-                <?= $content; ?>
-
-                <br> <?= TimeAgo::widget(['timestamp' => $record->created_at]); ?>
-                <?php if ($isNew): ?>
-                    <span class="label label-danger"><?= Yii::t('NotificationModule.base', 'New'); ?></span>
-                <?php endif; ?>
             </div>
-
+            <div class="media-body">
+                <?= $content ?>
+                <br>
+                <?= TimeAgo::widget(['timestamp' => $record->created_at]) ?>
+            </div>
+            <div class="media-right align-center">
+            <?php if ($isNew) : ?>
+                <span class="badge-new"></span>
+            <?php endif; ?>
+            </div>
         </div>
     </a>
 </li>
