b2821f2289cab6e730904d98187c9efc22e67738
gevorgmansuryan <gevorgmansuryan@gmail.com>
2024-08-09 00:28:39 +0400

    Groups - Default permissions/Space creation

---
+++ protected/humhub/migrations/m240807_230603_add_permissions_to_existing_user_groups.php
@@ -0,0 +1,46 @@
+<?php
+
+use humhub\modules\space\permissions\CreatePrivateSpace;
+use humhub\modules\space\permissions\CreatePublicSpace;
+use humhub\modules\user\models\Group;
+use yii\db\Migration;
+
+/**
+ * Class m240807_230603_add_permissions_to_existing_user_groups
+ */
+class m240807_230603_add_permissions_to_existing_user_groups extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        if (Yii::$app->isInstalled()) {
+            $groups = Group::find();
+
+            /** @var Group $group */
+            foreach ($groups->each() as $group) {
+                Yii::$app->user->permissionManager->setGroupState(
+                    $group->id,
+                    CreatePrivateSpace::class,
+                    CreatePrivateSpace::STATE_ALLOW,
+                );
+                Yii::$app->user->permissionManager->setGroupState(
+                    $group->id,
+                    CreatePublicSpace::class,
+                    CreatePublicSpace::STATE_ALLOW,
+                );
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240807_230603_add_permissions_to_existing_user_groups cannot be reverted.\n";
+
+        return false;
+    }
+}
