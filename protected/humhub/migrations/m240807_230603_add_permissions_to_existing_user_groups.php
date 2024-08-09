c40812109d173c8402bfe40c98ecea1c5ebbf613
gevorgmansuryan <gevorgmansuryan@gmail.com>
2024-08-10 01:17:08 +0400

    Groups - Default permissions/Space creation

--- protected/humhub/migrations/m240807_230603_add_permissions_to_existing_user_groups.php
+++ protected/humhub/migrations/m240807_230603_add_permissions_to_existing_user_groups.php
@@ -1,9 +1,12 @@
 <?php
 
+use humhub\libs\BasePermission;
 use humhub\modules\space\permissions\CreatePrivateSpace;
 use humhub\modules\space\permissions\CreatePublicSpace;
 use humhub\modules\user\models\Group;
+use humhub\modules\user\models\GroupPermission;
 use yii\db\Migration;
+use yii\db\Query;
 
 /**
  * Class m240807_230603_add_permissions_to_existing_user_groups
@@ -16,30 +19,32 @@ class m240807_230603_add_permissions_to_existing_user_groups extends Migration
     public function safeUp()
     {
         if (Yii::$app->isInstalled()) {
-            $groups = Group::find();
+            foreach ((new Query())->from(Group::tableName())->batch() as $groups) {
+                foreach ($groups as $group) {
+                    foreach ([Yii::createObject(CreatePublicSpace::class), Yii::createObject(CreatePrivateSpace::class)] as $permission) {
+                        /** @var BasePermission $permission */
 
-            /** @var Group $group */
-            foreach ($groups->each() as $group) {
-                $alreadySet = Yii::$app->user->permissionManager->getGroupState(
-                    $group->id, Yii::createObject(CreatePrivateSpace::class) , false
-                );
-                if ($alreadySet == '') {
-                    Yii::$app->user->permissionManager->setGroupState(
-                        $group->id,
-                        CreatePrivateSpace::class,
-                        CreatePrivateSpace::STATE_ALLOW,
-                    );
-                }
+                        $exist = (new Query())->from(GroupPermission::tableName())
+                            ->where([
+                                'group_id' => $group['id'],
+                                'module_id' => $permission->getModuleId(),
+                                'class' => $permission::class,
+                            ])
+                            ->exists();
 
-                $alreadySet = Yii::$app->user->permissionManager->getGroupState(
-                    $group->id, Yii::createObject(CreatePublicSpace::class) , false
-                );
-                if ($alreadySet == '') {
-                    Yii::$app->user->permissionManager->setGroupState(
-                        $group->id,
-                        CreatePublicSpace::class,
-                        CreatePublicSpace::STATE_ALLOW,
-                    );
+                        if (!$exist) {
+                            Yii::$app->db->createCommand()->insert(
+                                GroupPermission::tableName(),
+                                [
+                                    'permission_id' => $permission->getId(),
+                                    'group_id' => $group['id'],
+                                    'module_id' => $permission->getModuleId(),
+                                    'class' => $permission::class,
+                                    'state' => $permission::STATE_ALLOW,
+                                ]
+                            )->execute();
+                        }
+                    }
                 }
             }
         }
