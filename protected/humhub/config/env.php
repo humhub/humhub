fafb72c5b212700aeef0a676c53eb3bfbedd71ee
gevorgmansuryan <gevorgmansuryan@users.noreply.github.com>
2024-08-19 22:02:27 +0000

    Autocommit PHP CS Fixer

--- protected/humhub/config/env.php
+++ protected/humhub/config/env.php
@@ -9,34 +9,39 @@ use yii\helpers\StringHelper;
 
 $env = [];
 
-$preparePath = function(string $key): array {
+$preparePath = function (string $key): array {
     return ArrayHelper::getColumn(
         explode('__', $key),
-        function($path) {
+        function ($path) {
             return Inflector::variablize(strtolower($path));
-        }
+        },
     );
 };
 
 foreach ($_ENV as $key => $value) {
     try {
         $value = Json::decode($value);
-    } catch (InvalidArgumentException) {}
+    } catch (InvalidArgumentException) {
+    }
 
     if (StringHelper::startsWith($key, 'HUMHUB_SETTINGS_')) {
         ArrayHelper::setValue(
-            $env,  [
+            $env,
+            [
                 'params', 'fixed-settings',
-                ...$preparePath(str_replace('HUMHUB_SETTINGS_', '', $key))
-            ], $value
+                ...$preparePath(str_replace('HUMHUB_SETTINGS_', '', $key)),
+            ],
+            $value,
         );
     }
     if (StringHelper::startsWith($key, 'HUMHUB_PARAMS_')) {
         ArrayHelper::setValue(
-            $env,  [
+            $env,
+            [
                 'params',
-                ...$preparePath(str_replace('HUMHUB_PARAMS_', '', $key))
-            ], $value
+                ...$preparePath(str_replace('HUMHUB_PARAMS_', '', $key)),
+            ],
+            $value,
         );
     }
 }
