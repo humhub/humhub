19deb93d26a0d8d5c04110f61843499e1d03b0ea
Paul <taoistkitsune@gmail.com>
2024-12-04 01:57:22 +0300

    Complete translation into Russian
    1. Full translation into Russian
    2. Improoved messy lang files (but saved all styles and formats)

--- protected/humhub/modules/space/messages/ru/widgets_views_changeImage.php
+++ protected/humhub/modules/space/messages/ru/widgets_views_changeImage.php
@@ -1,9 +1,5 @@
-<?php
-/**
- * Translation: Paul (https://paul.bid) paulbid@protonmail.com
- * 
- */
+<?php /* Translated by Paul (https://paul.bid) www.paul.bid@gmail.com */
 return array (
-  'Change image' => 'Сменить изображение',
-  'Current space image' => 'Изображение сообщества',
+  'Change image' => 'Изменить изображение',
+  'Current space image' => 'Текущее изображение сообщества',
 );
--- protected/humhub/modules/ui/messages/ru/base.php
+++ protected/humhub/modules/ui/messages/ru/base.php
@@ -1,7 +1,8 @@
-<?php
-return array (
+<?php /* Translated by Paul (https://paul.bid) www.paul.bid@gmail.com */
+return [
   'Collapse' => 'Свернуть',
   'Read more' => 'Развернуть',
   'Reset filters' => 'Сбросить фильтры',
+  'Select' => 'Выбрать',
   'User Interface' => 'Интерфейс Пользователя',
-);
+];
