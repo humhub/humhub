934d2e9e9018bafb217572218ad22f062b9f6a69
Paul <45032974+hide-me@users.noreply.github.com>
2024-12-13 18:32:35 +0300

    Update notifications.php

--- protected/humhub/modules/content/messages/ru/notifications.php
+++ protected/humhub/modules/content/messages/ru/notifications.php
@@ -6,8 +6,8 @@ return array (
   'Your {contentTitle} has been deleted by {displayName} for \'{reason}\'' => 'Ваш контент {contentTitle} была удалён пользователем {displayName} по причине: «{reason}»',
   '{displayName} created {contentTitle}.' => '{displayName} создал(а) {contentTitle}.',
   '{displayName} posted on your profile {contentTitle}.' => '{displayName} опубликовал(а) в вашем профиле {contentTitle}.',
-  '{originator} just wrote {contentInfo}' => '{originator} только что написал(а) {contentInfo}',
   '{originator} just wrote {contentInfo} in space {space}' => '{originator} только что написал(а) {contentInfo} в сообществе {space}',
-  '{originator} notifies you about {contentInfo}' => '{originator} уведомляет вас о {contentInfo}',
+  '{originator} just wrote {contentInfo}' => '{originator} только что написал(а) {contentInfo}',
   '{originator} notifies you about {contentInfo} in {space}' => '{originator} уведомляет вас о {contentInfo} в сообществе {space}',
+  '{originator} notifies you about {contentInfo}' => '{originator} уведомляет вас о {contentInfo}',
 );
