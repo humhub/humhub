108f022a766150e5a12ad61d3677ed083000a49a
Paul <45032974+hide-me@users.noreply.github.com>
2024-12-13 18:28:43 +0300

    Update permissions.php

--- protected/humhub/modules/comment/messages/ru/permissions.php
+++ protected/humhub/modules/comment/messages/ru/permissions.php
@@ -1,7 +1,7 @@
 <?php /* Translated by Paul (https://paul.bid) www.paul.bid@gmail.com */
 return array (
-    'Allows the user to add comments' => 'Может писать комментарии',
-    'Can manage (e.g. archive, stick, move or delete) arbitrary content' => 'Может управлять произвольным контентом. Например: архивировать, вставлять, перемещать или удалять его',
-    'Create comment' => 'Создание комментариев',
-    'Manage content' => 'Управление контентом',
+  'Allows the user to add comments' => 'Может писать комментарии',
+  'Can manage (e.g. archive, stick, move or delete) arbitrary content' => 'Может управлять произвольным контентом. Например: архивировать, вставлять, перемещать или удалять его',
+  'Create comment' => 'Создание комментариев',
+  'Manage content' => 'Управление контентом',
 );
