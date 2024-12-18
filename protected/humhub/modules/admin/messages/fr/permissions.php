36de99489c565f10f183d1df6bb475b8273b4256
HumHub Translations <hello@humhub.com>
2024-12-18 08:50:55 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/admin/messages/fr/permissions.php
+++ protected/humhub/modules/admin/messages/fr/permissions.php
@@ -1,15 +1,18 @@
 <?php
-return array (
-  'Access Admin Information' => 'Accéder aux informations d\'administration',
-  'Can access the \'Administration -> Information\' section.' => 'Autoriser les utilisateurs à accéder à la section \'Administration &gt; Informations\'',
-  'Can manage modules within the \'Administration ->  Modules\' section.' => 'Autoriser les utilisateurs à gérer les modules dans la section \'Administration &gt; Modules\'',
-  'Can manage Spaces within the \'Administration -> Spaces\' section.' => 'Autoriser les utilisateurs à gérer les espaces dans la section \'Administration &gt; Espaces\'',
-  'Can manage general settings.' => 'Autoriser les utilisateurs à gérer les utilisateurs, les espaces et les paramètres généraux',
-  'Can manage users and groups' => 'Autoriser les utilisateurs à gérer les utilisateurs et les groupes',
-  'Can manage users and user profiles.' => 'Autoriser les utilisateurs à gérer les autres utilisateurs',
-  'Manage Groups' => 'Gestion des groupes',
-  'Manage Modules' => 'Gestion des modules',
-  'Manage Settings' => 'Gestion des paramètres',
-  'Manage Spaces' => 'Gestion des espaces',
-  'Manage Users' => 'Gestion des utilisateurs',
-);
+
+return [
+    'Access Admin Information' => 'Accéder aux informations d\'administration',
+    'Can access the \'Administration -> Information\' section.' => 'Autoriser les utilisateurs à accéder à la section \'Administration &gt; Informations\'',
+    'Can manage Spaces within the \'Administration -> Spaces\' section.' => 'Autoriser les utilisateurs à gérer les espaces dans la section \'Administration &gt; Espaces\'',
+    'Can manage general settings.' => 'Autoriser les utilisateurs à gérer les utilisateurs, les espaces et les paramètres généraux',
+    'Can manage modules within the \'Administration ->  Modules\' section.' => 'Autoriser les utilisateurs à gérer les modules dans la section \'Administration &gt; Modules\'',
+    'Can manage users and groups' => 'Autoriser les utilisateurs à gérer les utilisateurs et les groupes',
+    'Can manage users and user profiles.' => 'Autoriser les utilisateurs à gérer les autres utilisateurs',
+    'Manage Groups' => 'Gestion des groupes',
+    'Manage Modules' => 'Gestion des modules',
+    'Manage Settings' => 'Gestion des paramètres',
+    'Manage Spaces' => 'Gestion des espaces',
+    'Manage Users' => 'Gestion des utilisateurs',
+    'Can manage (e.g. edit or delete) all content (even private)' => '',
+    'Manage All Content' => '',
+];
--- protected/humhub/modules/admin/messages/tr/permissions.php
+++ protected/humhub/modules/admin/messages/tr/permissions.php
@@ -1,15 +1,18 @@
 <?php
-return array (
-  'Access Admin Information' => 'Yönetici Bilgisine Erişim',
-  'Can access the \'Administration -> Information\' section.' => '\'Yönetim -&gt; Bilgi\' bölümüne erişebilir.',
-  'Can manage modules within the \'Administration ->  Modules\' section.' => '\'Yönetim -&gt; Modüller\' bölümündeki modülleri yönetebilir.',
-  'Can manage Spaces within the \'Administration -> Spaces\' section.' => '\'Yönetim -&gt; Mekanlar\' bölümündeki mekanları yönetebilir (oluştur/düzenle/sil).',
-  'Can manage general settings.' => 'Kullanıcı - mekan ve genel ayarları yönetebilir.',
-  'Can manage users and groups' => 'Kullanıcıları ve grupları yönetebilir',
-  'Can manage users and user profiles.' => 'Kullanıcıları ve kullanıcı profillerini yönetebilir.',
-  'Manage Groups' => 'Grupları Yönet',
-  'Manage Modules' => 'Modülleri Yönet',
-  'Manage Settings' => 'Ayarları Yönet',
-  'Manage Spaces' => 'Mekanları Yönet',
-  'Manage Users' => 'Kullanıcıları Yönet',
-);
+
+return [
+    'Access Admin Information' => 'Yönetici Bilgisine Erişim',
+    'Can access the \'Administration -> Information\' section.' => '\'Yönetim -&gt; Bilgi\' bölümüne erişebilir.',
+    'Can manage Spaces within the \'Administration -> Spaces\' section.' => '\'Yönetim -&gt; Mekanlar\' bölümündeki mekanları yönetebilir (oluştur/düzenle/sil).',
+    'Can manage general settings.' => 'Kullanıcı - mekan ve genel ayarları yönetebilir.',
+    'Can manage modules within the \'Administration ->  Modules\' section.' => '\'Yönetim -&gt; Modüller\' bölümündeki modülleri yönetebilir.',
+    'Can manage users and groups' => 'Kullanıcıları ve grupları yönetebilir',
+    'Can manage users and user profiles.' => 'Kullanıcıları ve kullanıcı profillerini yönetebilir.',
+    'Manage Groups' => 'Grupları Yönet',
+    'Manage Modules' => 'Modülleri Yönet',
+    'Manage Settings' => 'Ayarları Yönet',
+    'Manage Spaces' => 'Mekanları Yönet',
+    'Manage Users' => 'Kullanıcıları Yönet',
+    'Can manage (e.g. edit or delete) all content (even private)' => '',
+    'Manage All Content' => '',
+];
