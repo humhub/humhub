eafe7b06be099e755e28b4c8d44ffed7d5336543
Lucas Bartholemy <lucas@bartholemy.com>
2024-12-19 09:38:34 +0100

    Example

---
+++ protected/humhub/widgets/modal/FormModal.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace humhub\widgets\modal;
+
+use humhub\widgets\form\ActiveForm;
+
+class FormModal extends Modal
+{
+    public array $form;
+
+    public static function beginFormDialog($config = []): ActiveForm
+    {
+        $form = ActiveForm::begin($config['form'] ?? []);
+
+        parent::beginDialog($config);
+        return $form;
+    }
+
+    public static function endFormDialog()
+    {
+        ActiveForm::end();
+        parent::endDialog();
+    }
+}
