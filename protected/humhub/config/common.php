d0b0803a4ae56ce2993f6f663072b8ad9d054578
Marc Farré <23310825+marc-farre@users.noreply.github.com>
2024-12-30 09:01:12 +0000

    Fix DeviceDetectorHelper when no user agent (#7365)
    * Enh: Fix DeviceDetectorHelper::isMobile() and DeviceDetectorHelper::isTablet() when no user agent
    
    * Add PR ID to CHANGELOG

--- protected/humhub/config/common.php
+++ protected/humhub/config/common.php
@@ -42,7 +42,7 @@ $logTargetConfig = [
 
 $config = [
     'name' => 'HumHub',
-    'version' => '1.17.0-beta.4',
+    'version' => '1.17.0-beta.5',
     'minRecommendedPhpVersion' => '8.1',
     'minSupportedPhpVersion' => '8.1',
     'basePath' => dirname(__DIR__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
