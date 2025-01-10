301c3e61f849565b2d526d7a1083ad47849b9f21
HumHub Translations <hello@humhub.com>
2025-01-10 20:21:36 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/content/messages/sv/search.php
+++ protected/humhub/modules/content/messages/sv/search.php
@@ -1,19 +1,19 @@
 <?php
-return array (
+return [
   'Any' => 'Några',
   'Author' => 'Författare',
-  'Best' => '',
-  'Content type' => '',
-  'Date From' => '',
-  'Date To' => '',
-  'Find Content based on keywords' => '',
+  'Best' => 'Bäst',
+  'Content type' => 'Innehållstyp',
+  'Date From' => 'Datum från',
+  'Date To' => 'Datum till',
+  'Find Content based on keywords' => 'Hitta innehåll baserat på nyckelord',
   'Newest first' => 'Nyast först',
   'No results found!' => 'Inga resultat funna!',
-  'Results ({count})' => '',
+  'Results ({count})' => 'Resultat ({count})',
   'Search' => 'Sök',
   'Search...' => 'Sök...',
   'Sorting' => 'Sortering',
   'Space' => 'Forum',
   'Topic' => 'Ämne',
   'Try other keywords or remove filters.' => 'Prova andra sökord eller ta bort filter.',
-);
+];
