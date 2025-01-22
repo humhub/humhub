f28382c3b7b55b2a8fc3447167486813146ce8ec
HumHub Translations <hello@humhub.com>
2025-01-22 10:41:28 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/ui/messages/pt-BR/form.php
+++ protected/humhub/modules/ui/messages/pt-BR/form.php
@@ -1,5 +1,5 @@
 <?php
-return array (
+return [
   'Hide all stream entries in this Space by default' => 'Ocultar todas as entradas de fluxo neste espaço por padrão',
   'Hide all stream entries in your Profile by default' => 'Ocultar todas as entradas de fluxo em seu perfil por padrão',
   'Hide all stream entries of this module globally by default' => 'Ocultar todas as entradas de fluxo deste módulo globalmente por padrão',
@@ -7,7 +7,7 @@ return array (
   'Invalid icon.' => 'Ícone inválido.',
   'Note: Hidden entries can be made visible using the stream filtering options. Single stream entries can be marked as visible on an individual level.' => 'Nota: Entradas ocultas podem ser tornadas visíveis usando as opções de filtragem de fluxo. Entradas de fluxo único podem ser marcadas como visíveis em um nível individual.',
   'Note: The default settings can be adjusted individually for each Space and each single stream entry. Hidden entries can be made visible using the stream filtering options.' => 'Nota: As configurações padrão podem ser ajustadas individualmente para cada Espaço e cada entrada de fluxo único. Entradas ocultas podem ser tornadas visíveis usando as opções de filtragem de fluxo.',
-  'Select icon' => 'Selecione o ícone',
+  'Select icon' => 'Selecionar ícone',
   'Sort Order' => 'Ordem de Classificação',
   'Values between 0 and 10000, the existing elements usually use steps of 100.' => 'Valores entre 0 e 10000, os elementos existentes geralmente usam passos de 100.',
-);
+];
