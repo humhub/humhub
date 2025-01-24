ca685e0bcb5610a13b928c23d5559245f37c1201
Marc Farré <23310825+marc-farre@users.noreply.github.com>
2025-01-24 10:51:12 +0000

    Enh: Confusing message after creating an account (#7412)
    * Enh: Confusing message after creating an account
    
    * Add PR ID to CHANGELOG
    
    * Add Close button to modal
    
    * Fix tests

--- protected/humhub/modules/user/views/auth/register_success_modal.php
+++ protected/humhub/modules/user/views/auth/register_success_modal.php
@@ -1,24 +1,31 @@
 <?php
 
-use yii\helpers\Url;
+use humhub\modules\ui\view\components\View;
+use humhub\modules\user\models\Invite;
+use humhub\widgets\ModalButton;
+
+/* @var $this View */
+/* @var $model Invite */
 
 $this->pageTitle = Yii::t('UserModule.auth', 'Registration successful');
 ?>
+
 <div class="modal-dialog modal-dialog-small animated fadeIn">
     <div class="modal-content">
         <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-            <h4 class="modal-title"
-                id="myModalLabel"><?php echo Yii::t('UserModule.auth', '<strong>Registration</strong> successful!'); ?></h4>
+            <h4 class="modal-title" id="myModalLabel">
+                <strong><?= Yii::t('UserModule.auth', 'Almost there!') ?></strong>
+            </h4>
         </div>
         <div class="modal-body text-center">
-            <p><?php echo Yii::t('UserModule.auth', 'Please check your email and follow the instructions!'); ?></p>
-            <br>
-            <a href="<?= Url::to(["/"]) ?>" data-pjax-prevent data-ui-loader
-               class="btn btn-primary"><?php echo Yii::t('UserModule.auth', 'back to home') ?></a>
+            <p><?= Yii::t('UserModule.auth', 'An email has been sent to {emailAddress}. Please check your inbox to complete the registration.', [
+                'emailAddress' => $model->email,
+            ]) ?></p>
+            <p><?= Yii::t('UserModule.auth', 'If you don\'t see the email, please check your spam folder.') ?></p>
+        </div>
+        <div class="modal-footer">
+            <?= ModalButton::cancel(Yii::t('base', 'Close')) ?>
         </div>
     </div>
 </div>
-
-
-
