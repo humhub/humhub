c267e41be9bc881ff3fa63c5413688a3ef012f0e
Yuriy Bakhtin <yurybakh@gmail.com>
2025-03-31 12:20:50 +0200

    Don't require approval for user created from administration

--- protected/humhub/modules/admin/controllers/UserController.php
+++ protected/humhub/modules/admin/controllers/UserController.php
@@ -261,8 +261,7 @@ class UserController extends Controller
 
     public function actionAdd()
     {
-        $registration = new Registration(enableEmailField: true, enablePasswordForm: true, enableMustChangePassword: true);
-        $registration->enableUserApproval = true;
+        $registration = new Registration(enableEmailField: true, enableMustChangePassword: true);
 
         if ($registration->submitted('save') && $registration->validate() && $registration->register()) {
             return $this->redirect(['edit', 'id' => $registration->getUser()->id]);
