32043376f2a475b52a3686932cd1a7b46e066a5b
Marc Farré <contact@marc.fun>
2025-04-05 09:13:29 +0100

    Add themes and modules migrations

--- CHANGELOG-BS5.md
+++ CHANGELOG-BS5.md
@@ -3,10 +3,18 @@ HumHub Changelog
 
 Bootstrap 5 migration
 ------------
+
+Modules and themes must be migrated to Bootstrap 5.
+See MIGRATE_DEV-BS5.md
+
+Migration proccess:
+- The default HumHub theme will be activated
+- Themes in the `themes` folder will be renamed by adding `.bs3.old`. 
+- The Theme Builder module will be uninstalled.
+
 - Enh: Upgrade Bootstrap library (Bootstrap 3 v2.0.0 => Bootstrap 5 v2.0)
 - Enh: Upgrade Select2 Bootstrap Theme (Bootstrap 3 v0.1.0-beta.4 => Bootstrap v5 1.3.0)
 - Enh: Upgrade jQuery library (3.6.4 => 3.7.1)
 - Enh: Add [scssphp library](https://github.com/scssphp/scssphp) allowing to compile SCSS to CSS with PHP
 - Enh: Add CSS compiler after saving the Appearance settings form and after flushing cache
 - Enh: Add Appearance settings form fields: Primary and Secondary color, and a free SCSS textArea
-- Enh: For all changes related to BS5, see MIGRATE_DEV-BS5.md
---
+++ protected/humhub/migrations/m250405_072758_1_18_switch_to_humhub_theme_and_disable_themes.php
@@ -0,0 +1,69 @@
+<?php
+
+use humhub\helpers\ThemeHelper;
+use yii\db\Migration;
+use yii\helpers\FileHelper;
+
+class m250405_072758_1_18_switch_to_humhub_theme_and_disable_themes extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        // Copy Primary and Secondary colors to the Settings manager
+        $settingsManager = Yii::$app->settings;
+        $themeVariables = Yii::$app->view->theme->variables;
+        $settingsManager->set('themePrimaryColor', $themeVariables->get('primary'));
+        $settingsManager->set('themeSecondaryColor', $themeVariables->get('secondary'));
+
+        // Switch to HumHub theme
+        $hhTheme = ThemeHelper::getThemeByName('HumHub');
+        $hhTheme->activate();
+
+        // Uninstall the Theme Builder module
+        $moduleManager = Yii::$app->moduleManager;
+        $themeBuilderModuleId = 'theme-builder';
+        if ($moduleManager->getModule($themeBuilderModuleId)) {
+            $moduleManager->removeModule($themeBuilderModuleId);
+        }
+
+        // Loop over all /themes/* folder and rename all old theme folders to e.g. /themes/Example.bs3.old
+        $themesPath = Yii::getAlias('@themes');
+        foreach (ThemeHelper::getThemesByPath($themesPath) as $theme) {
+            if ($theme->name === 'HumHub') {
+                continue;
+            }
+            // Rename theme by adding .old
+            $oldThemePath = $themesPath . DIRECTORY_SEPARATOR . $theme->name . '.bs3.old';
+            if (!file_exists($oldThemePath)) {
+                rename($themesPath . DIRECTORY_SEPARATOR . $theme->name, $oldThemePath);
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m250405_072758_1_18_switch_to_humhub_theme_and_disable_themes cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250405_072758_1_18_switch_to_humhub_theme_and_disable_themes cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
