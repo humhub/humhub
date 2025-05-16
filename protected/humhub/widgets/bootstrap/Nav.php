a8607451a3914858d8b5533165b76c39b207d57b
Marc Farré <contact@marc.fun>
2025-05-16 15:08:49 +0100

    NavBar widget - https://github.com/humhub/humhub-internal/issues/651

---
+++ protected/humhub/widgets/bootstrap/Nav.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @link https://www.humhub.org/
+ * @copyright Copyright (c) 2018 HumHub GmbH & Co. KG
+ * @license https://www.humhub.com/licences
+ *
+ */
+
+namespace humhub\widgets\bootstrap;
+
+/**
+ * @since 1.8
+ */
+class Nav extends \yii\bootstrap5\Nav
+{
+}
