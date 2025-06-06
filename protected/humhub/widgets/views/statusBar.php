6c3530c6911d8affe2d3a6ed41ffcb2f257281f7
Yuriy Bakhtin <yurybakh@gmail.com>
2025-06-06 14:34:48 +0200

    Fix jQuery show/hide functions for status bar displaying

--- protected/humhub/widgets/views/statusBar.php
+++ protected/humhub/widgets/views/statusBar.php
@@ -1,4 +1,4 @@
-<div id="status-bar" class="clearfix" style="display:none;">
+<div id="status-bar" class="clearfix d-none">
     <div class="status-bar-body">
         <div class="status-bar-content"></div>
     </div>
