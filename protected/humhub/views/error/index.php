6de7a437cd59b28cd4d416dbb80a9498e756a7f4
gevorgmansuryan <gevorgmansuryan@gmail.com>
2025-06-17 18:25:45 +0400

    Fix Topic Permission issues

--- protected/humhub/views/error/index.php
+++ protected/humhub/views/error/index.php
@@ -9,7 +9,6 @@ $this->pageTitle = Yii::t('base', 'Error');
 <div class="container">
     <div class="panel panel-default">
         <div class="panel-heading">
-            <?php var_dump(Url::current()); ?>
             <?= Yii::t('base', 'Oooops...'); ?> <?= Yii::t('base', 'It looks like you may have taken the wrong turn.'); ?>
         </div>
         <div class="panel-body">
