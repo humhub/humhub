40d6b2b0b154b439001fe418f692479ff40c346a
gevorgmansuryan <gevorgmansuryan@gmail.com>
2025-06-19 03:14:58 +0400

    Cache issue with develop branch / db error

--- protected/humhub/modules/admin/views/setting/caching.php
+++ protected/humhub/modules/admin/views/setting/caching.php
@@ -3,9 +3,12 @@
 use humhub\modules\admin\models\forms\CacheSettingsForm;
 use humhub\modules\ui\form\widgets\ActiveForm;
 use humhub\widgets\Button;
+use yii\helpers\ArrayHelper;
 
 /* @var $cacheTypes [] */
 /* @var $model CacheSettingsForm */
+
+var_dump($model->isTypeFixed);
 
 ?>
 
@@ -13,8 +16,10 @@ use humhub\widgets\Button;
 
 <?php $form = ActiveForm::begin(['acknowledge' => true]); ?>
 
-<?php if (!Yii::$app->settings->isFixed('cacheClass')): ?>
-    <?= $form->field($model, 'type')->dropDownList($cacheTypes, ['readonly' => Yii::$app->settings->isFixed('cacheClass')]) ?>
+<?php if (!$model->isTypeFixed): ?>
+    <?= $form->field($model, 'type')->dropDownList($cacheTypes) ?>
+<?php else: ?>
+    <?= $form->field($model, 'type')->textInput(['value' => ArrayHelper::getValue($cacheTypes, $model->fixedTypeValue), 'readonly' => true]) ?>
 <?php endif; ?>
 
 <?= $form->field($model, 'expireTime')->textInput(['readonly' => Yii::$app->settings->isFixed('cacheExpireTime')]) ?>
