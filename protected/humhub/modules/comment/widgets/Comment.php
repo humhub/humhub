8403d77896b55f4960adb5f41211f307e0341ea8
Marc Farré <23310825+marc-farre@users.noreply.github.com>
2025-07-27 17:25:49 +0100

    BS5 - Stream Overflow - https://github.com/humhub/humhub-internal/issues/740 (#7600)

--- protected/humhub/modules/comment/widgets/Comment.php
+++ protected/humhub/modules/comment/widgets/Comment.php
@@ -36,7 +36,7 @@ class Comment extends Widget
     /**
      * @var string Default style class of div wrapper around Comment block
      */
-    public $defaultClass = 'single-comment d-flex overflow-auto';
+    public $defaultClass = 'single-comment d-flex';
 
     /**
      * @var string Additional style class of div wrapper around Comment block
