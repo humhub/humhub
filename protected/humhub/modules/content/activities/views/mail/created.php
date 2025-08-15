aba158497c025517818f0f6d31fd46901ae348e2
franky235 <francois.bourdon.perso@gmail.com>
2025-08-15 10:22:33 +0200

    add mailPreviewLength attribute controlled by config file

--- protected/humhub/modules/content/activities/views/mail/created.php
+++ protected/humhub/modules/content/activities/views/mail/created.php
@@ -4,6 +4,8 @@ use humhub\helpers\Html;
 use humhub\modules\content\interfaces\ContentOwner;
 use humhub\modules\content\widgets\richtext\converter\RichTextToShortTextConverter;
 use humhub\modules\content\widgets\richtext\RichText;
+use humhub\modules\content\activities\ContentCreated;
+use humhub\modules\activity\models\Activity;
 
 /* @var $originator \humhub\modules\user\models\User */
 /* @var $source ContentOwner */
@@ -12,8 +14,18 @@ echo Yii::t('ContentModule.activities', '{displayName} created a new {contentTit
     '{displayName}' => '<strong>' . Html::encode($originator->displayName) . '</strong>',
     '{contentTitle}' => '<strong>' . Html::encode($source->getContentName()) . '</strong>'
 ]);
+
+$maxLength = 0;
+
+// get corresponding mailPreviewLength if any
+$activity = Activity::findOne(['object_id' => $source->id]);
+if ($activity && ($activityBaseClass = $activity->getActivityBaseClass()) instanceof ContentCreated) {
+    $maxLength = $activityBaseClass->mailPreviewLength;
+}
+
 ?>
 <br>
-"<?= RichText::preview($source->getContentDescription(), 0, [
+
+"<?= RichText::preview($source->getContentDescription(), $maxLength, [
     RichTextToShortTextConverter::OPTION_CACHE_KEY => RichTextToShortTextConverter::buildCacheKeyForContent($source)
 ]) ?>"
