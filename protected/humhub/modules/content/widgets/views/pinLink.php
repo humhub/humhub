604ef2d91eea02cf1b48e3191e00e4e1e1ac0e1a
Marc Farré <23310825+marc-farre@users.noreply.github.com>
2025-09-03 07:47:30 +0100

    Fix icon spacing (#7672)

--- protected/humhub/modules/content/widgets/views/pinLink.php
+++ protected/humhub/modules/content/widgets/views/pinLink.php
@@ -5,24 +5,20 @@
 
 /* @var $pinUrl string */
 
-use humhub\modules\ui\icon\widgets\Icon;
+use humhub\widgets\bootstrap\Button;
 use yii\web\View;
 
 ?>
 <li>
     <?php if ($isPinned): ?>
-        <a href="#"
-           class="dropdown-item "
-           data-action-click="unpin"
-           data-action-url="<?= $unpinUrl ?>">
-            <?= Icon::get('map-pin') ?> <?php echo Yii::t('ContentModule.base', 'Unpin'); ?>
-        </a>
+        <?= Button::asLink(Yii::t('ContentModule.base', 'Unpin'))
+            ->action('unpin', $unpinUrl)
+            ->cssClass('dropdown-item')
+            ->icon('map-pin') ?>
     <?php else: ?>
-        <a href="#"
-           class="dropdown-item "
-           data-action-click="pin"
-           data-action-url="<?= $pinUrl ?>">
-            <?= Icon::get('map-pin') ?> <?php echo Yii::t('ContentModule.base', 'Pin to top'); ?>
-        </a>
+        <?= Button::asLink(Yii::t('ContentModule.base', 'Pin to top'))
+            ->action('pin', $pinUrl)
+            ->cssClass('dropdown-item')
+            ->icon('map-pin') ?>
     <?php endif; ?>
 </li>
