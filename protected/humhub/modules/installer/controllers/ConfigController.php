74a055869a88c691eaf45b79a319348088afd85c
Gevorg Mansuryan <gevorgmansuryan@gmail.com>
2025-09-21 15:20:57 +0400

    SaaS - Installer drop error on last step (#7721)

--- protected/humhub/modules/installer/controllers/ConfigController.php
+++ protected/humhub/modules/installer/controllers/ConfigController.php
@@ -33,7 +33,6 @@ use humhub\modules\user\models\Password;
 use humhub\modules\user\models\User;
 use Yii;
 use yii\base\InvalidConfigException;
-use yii\caching\DummyCache;
 use yii\web\HttpException;
 
 /**
@@ -559,7 +558,10 @@ class ConfigController extends Controller
 
     public function actionFinish()
     {
-        if (Yii::$app->settings->get('secret') == "") {
+        Yii::$app->settings->reload();
+        $secret = Yii::$app->settings->get('secret');
+
+        if (empty($secret)) {
             Yii::$app->settings->set('secret', UUID::v4());
         }
 
@@ -573,9 +575,11 @@ class ConfigController extends Controller
      */
     public function actionFinished()
     {
-        // Should not happen
-        if (Yii::$app->settings->get('secret') == "") {
-            throw new HttpException("Finished without secret setting!");
+        Yii::$app->settings->reload();
+        $secret = Yii::$app->settings->get('secret');
+
+        if (empty($secret)) { // Should not happen
+            throw new HttpException('Finished without secret setting!');
         }
 
         Yii::$app->settings->set('defaultTimeZone', Yii::$app->timeZone);
