4a31c02b651e85be98814e61d1e06bd170a89539
Yuriy Bakhtin <yurybakh@gmail.com>
2025-10-07 17:42:46 +0300

    Fix for Percona-XtraDB-Cluster

--- protected/humhub/modules/content/migrations/m251007_133345_fulltext_index_primary_key.php
+++ protected/humhub/modules/content/migrations/m251007_133345_fulltext_index_primary_key.php
@@ -11,7 +11,7 @@ class m251007_133345_fulltext_index_primary_key extends Migration
     {
         $this->safeDropForeignKey('fk_content_fulltext', 'content_fulltext');
         $this->safeDropIndex('fk_content_fulltext', 'content_fulltext');
-        $this->safeAddPrimaryKey(null, 'content_fulltext', 'content_id');
+        $this->safeAddPrimaryKey('', 'content_fulltext', 'content_id');
         $this->safeAddForeignKey('fk_content_fulltext', 'content_fulltext', 'content_id', 'content', 'id', 'CASCADE', 'CASCADE');
     }
 
