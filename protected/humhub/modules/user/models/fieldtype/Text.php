e6b9804e0dec11840eb22fe35834d039f7b9b06c
Marc Farré <contact@marc.fun>
2025-11-10 12:03:30 +0000

    Add comment

--- protected/humhub/modules/user/models/fieldtype/Text.php
+++ protected/humhub/modules/user/models/fieldtype/Text.php
@@ -179,6 +179,7 @@ class Text extends BaseType
         } elseif ($this->validator == self::VALIDATOR_URL) {
             $linkPrefix = $this->linkPrefix;
             $rules[] = [$this->profileField->internal_name, function($attribute) use ($linkPrefix) {
+                // Prepend the value with the Link Prefix (e.g. https://)
                 if ($linkPrefix && $this->$attribute && !str_starts_with($this->$attribute, $linkPrefix)) {
                     $this->$attribute = $linkPrefix . $this->$attribute;
                 }
