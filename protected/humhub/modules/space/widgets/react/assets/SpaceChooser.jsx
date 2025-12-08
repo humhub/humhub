46bea24ef7cca85ba3b85ac1a5b096ea9be89d5e
gevorgmansuryan <gevorgmansuryan@gmail.com>
2025-12-08 23:02:24 +0400

    templating / SpaceChooser as ReactJs component - WIP

--- protected/humhub/modules/space/widgets/react/assets/SpaceChooser.jsx
+++ protected/humhub/modules/space/widgets/react/assets/SpaceChooser.jsx
@@ -32,74 +32,76 @@ const SpaceChooser = ({
                     </div>
                 )}
             </a>
-            <ul
-                className="dropdown-menu"
-                id="space-menu-dropdown"
-                style={{ display: isOpen ? 'block' : 'none' }}
-            >
-                <li>
-                    <form
-                        action=""
-                        className="dropdown-header dropdown-controls"
-                        onSubmit={(e) => e.preventDefault()}
-                    >
-                        <div className={canAccessDirectory ? 'input-group' : ''}>
-                            <input
-                                type="text"
-                                id="space-menu-search"
-                                className="form-control"
-                                autoComplete="off"
-                                placeholder="Search for spaces"
-                                title="Search for spaces"
-                                value={searchQuery}
-                                onChange={(e) => setSearchQuery(e.target.value)}
-                            />
+            {isOpen && (
+                <ul
+                    className="dropdown-menu"
+                    id="space-menu-dropdown"
+                    style={{ display: 'block' }}
+                >
+                    <li>
+                        <form
+                            action=""
+                            className="dropdown-header dropdown-controls"
+                            onSubmit={(e) => e.preventDefault()}
+                        >
+                            <div className={canAccessDirectory ? 'input-group' : ''}>
+                                <input
+                                    type="text"
+                                    id="space-menu-search"
+                                    className="form-control"
+                                    autoComplete="off"
+                                    placeholder="Search for spaces"
+                                    title="Search for spaces"
+                                    value={searchQuery}
+                                    onChange={(e) => setSearchQuery(e.target.value)}
+                                />
 
-                            {canAccessDirectory && (
-                                <span id="space-directory-link" className="input-group-text">
+                                {canAccessDirectory && (
+                                    <span id="space-directory-link" className="input-group-text">
                   <a href={directoryUrl}>
                       <i className="fa fa-sitemap"></i>
                   </a>
                 </span>
-                            )}
+                                )}
 
-                            <div
-                                className="search-reset"
-                                id="space-search-reset"
-                                style={{ display: searchQuery ? 'block' : 'none' }}
-                                onClick={() => setSearchQuery('')}
-                            >
-                                <i className="fa fa-times-circle"></i>
+                                <div
+                                    className="search-reset"
+                                    id="space-search-reset"
+                                    style={{ display: searchQuery ? 'block' : 'none' }}
+                                    onClick={() => setSearchQuery('')}
+                                >
+                                    <i className="fa fa-times-circle"></i>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-                </li>
+                        </form>
+                    </li>
 
-                <li>
-                    <hr className="dropdown-divider" />
-                </li>
-                <li>
-                    <div id="space-menu-spaces" className="notLoaded hh-list">
-                        {spaces.map((space) => <SpaceChooserItem key={space.guid} space={space} />)}
-                    </div>
-                </li>
-                <li className="remoteSearch">
-                    <div id="space-menu-remote-search" className="dropdown-item notLoaded hh-list"></div>
-                </li>
-
-                {canCreateSpace && (
                     <li>
-                        <div className="dropdown-footer">
-                            <a
-                                href={createSpaceUrl}
-                                className="btn btn-accent col-lg-12"
-                            >
-                                Create Space
-                            </a>
+                        <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                        <div id="space-menu-spaces" className="notLoaded hh-list">
+                            {spaces.map((space) => <SpaceChooserItem key={space.guid} space={space} />)}
                         </div>
                     </li>
-                )}
-            </ul>
+                    <li className="remoteSearch">
+                        <div id="space-menu-remote-search" className="dropdown-item notLoaded hh-list"></div>
+                    </li>
+
+                    {canCreateSpace && (
+                        <li>
+                            <div className="dropdown-footer">
+                                <a
+                                    href={createSpaceUrl}
+                                    className="btn btn-accent col-lg-12"
+                                >
+                                    Create Space
+                                </a>
+                            </div>
+                        </li>
+                    )}
+                </ul>
+            )}
         </>
     );
 };
