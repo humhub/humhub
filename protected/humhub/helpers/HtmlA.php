ca7f58359a01c6560b13266475483d68042da081
Lucas Bartholemy <lucas@bartholemy.com>
2025-12-14 15:53:40 +0100

    Added HtmlA

---
+++ protected/humhub/helpers/HtmlA.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace humhub\helpers;
+
+use yii\helpers\Url;
+
+class HtmlA implements \Stringable
+{
+    use HtmlClickableElementTrait;
+
+    private string $text;
+    private array $options;
+    private bool $encodeLabel = false;
+
+    public function __construct(string $text, ?string $url, array $options)
+    {
+        if ($url !== null) {
+            $this->options['href'] = Url::to($url);
+        }
+        Html::addCssClass($options, ['class' => 'link']);
+
+        $this->text = $text;
+        $this->options = $options;
+    }
+
+    public function __toString()
+    {
+        $text = $this->icon . ($this->encodeLabel ? Html::encode($this->text) : $this->text);
+        return Html::tag('a', $text, $this->options);
+    }
+
+    public function post(): self
+    {
+        $this->options['data-method'] = 'POST';
+        return $this;
+    }
+
+    public function target($target): self
+    {
+        $this->options['target'] = $target;
+        return $this;
+    }
+
+    public function blank(): self
+    {
+        return $this->target('_blank');
+    }
+}
