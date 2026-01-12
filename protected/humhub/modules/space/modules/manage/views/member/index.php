c46af898b15a6529e6910f62046ead3048fcf695
Marc Farré <23310825+marc-farre@users.noreply.github.com>
2026-01-12 11:33:10 +0000

    Fix #7925: Space Members dropdown menu (#7926)
    https://github.com/humhub/humhub/issues/7925
    
    Co-authored-by: Lucas Bartholemy <luke-@users.noreply.github.com>

--- protected/humhub/modules/space/modules/manage/views/member/index.php
+++ protected/humhub/modules/space/modules/manage/views/member/index.php
@@ -24,24 +24,22 @@ use humhub\widgets\TimeAgo;
     <div class="panel-body">
 
         <?php $form = ActiveForm::begin(['method' => 'get']); ?>
-        <div class="container gx-0 overflow-x-hidden">
-            <div class="row gy-2">
-                <div class="col-lg-6">
-                    <div class="input-group">
-                        <?= Html::activeTextInput($searchModel, 'freeText', ['class' => 'form-control', 'placeholder' => Yii::t('AdminModule.user', 'Search by name, email or id.')]); ?>
-                        <button class="btn btn-light" type="submit"><i class="fa fa-search"></i></button>
-                    </div>
+        <div class="d-flex justify-content-between column-gap-4 row-gap-2 flex-column flex-sm-row">
+            <div class="flex-fill">
+                <div class="input-group">
+                    <?= Html::activeTextInput($searchModel, 'freeText', ['class' => 'form-control', 'placeholder' => Yii::t('AdminModule.user', 'Search by name, email or id.')]); ?>
+                    <button class="btn btn-light" type="submit"><i class="fa fa-search"></i></button>
                 </div>
-                <div class="col-lg-4">
-                    <?= Html::activeDropDownList($searchModel, 'group_id', MembershipSearch::getRoles($space), ['class' => 'form-control', 'data-action-change' => 'ui.form.submit']); ?>
-                </div>
-                <div class="col-lg-2">
-                    <div class="float-end">
-                        <?= MemberHeaderControlsMenu::widget([
-                            'space' => $space,
-                            'label' => Icon::get('cog'),
-                        ]) ?>
-                    </div>
+            </div>
+            <div class="flex-fill">
+                <?= Html::activeDropDownList($searchModel, 'group_id', MembershipSearch::getRoles($space), ['class' => 'form-control', 'data-action-change' => 'ui.form.submit']); ?>
+            </div>
+            <div class="flex-fill float-end">
+                <div class="float-end">
+                    <?= MemberHeaderControlsMenu::widget([
+                        'space' => $space,
+                        'label' => Icon::get('cog'),
+                    ]) ?>
                 </div>
             </div>
         </div>
