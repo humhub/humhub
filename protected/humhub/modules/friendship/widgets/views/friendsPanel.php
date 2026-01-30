ee00a7188f799bc9e5f7dffbcd67cfc5c58cc24e
Marc Farré <contact@marc.fun>
2026-01-30 18:03:35 +0000

    Friends widget, images stick once they wrap to the next line https://github.com/humhub/humhub-internal/issues/1043#issuecomment-3824729957

--- protected/humhub/modules/friendship/widgets/views/friendsPanel.php
+++ protected/humhub/modules/friendship/widgets/views/friendsPanel.php
@@ -13,19 +13,19 @@ use humhub\widgets\PanelMenu;
         </div>
 
         <div class="panel-body">
-            <?php foreach ($friends as $friend): ?>
-                <a href="<?= $friend->getUrl() ?>">
-                    <img src="<?= $friend->getProfileImage()->getUrl() ?>" class="rounded tt img_margin"
-                         height="24" width="24" alt="24x24" data-src="holder.js/24x24"
-                         style="width: 24px; height: 24px;"
-                         data-bs-toggle="tooltip" data-placement="top" title=""
-                         data-bs-title="<?php echo Html::encode($friend->displayName); ?>">
-                </a>
-            <?php endforeach; ?>
+            <div class="d-flex gap-2 flex-wrap">
+                <?php foreach ($friends as $friend): ?>
+                    <a href="<?= $friend->getUrl() ?>">
+                        <img src="<?= $friend->getProfileImage()->getUrl() ?>" class="rounded tt img_margin"
+                             height="24" width="24" alt="24x24" data-src="holder.js/24x24"
+                             style="width: 24px; height: 24px;"
+                             data-bs-toggle="tooltip" data-placement="top" title=""
+                             data-bs-title="<?php echo Html::encode($friend->displayName); ?>">
+                    </a>
+                <?php endforeach; ?>
+            </div>
             <?php if ($totalCount > $limit): ?>
-                <br/>
-                <br/>
-                <?= Html::a(Yii::t('FriendshipModule.base', 'Show all friends'), ['/friendship/list/popup', 'userId' => $user->id], ['class' => 'btn btn-sm', 'data-bs-target' => '#globalModal']) ?>
+                <?= Html::a(Yii::t('FriendshipModule.base', 'Show all friends'), ['/friendship/list/popup', 'userId' => $user->id], ['class' => 'btn btn-sm mt-2', 'data-bs-target' => '#globalModal']) ?>
             <?php endif; ?>
         </div>
     </div>
