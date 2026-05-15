a8bda12da73a64cdfe9ed5a9b66327a59f9f98d8
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2026-05-15 14:01:20 +0300

    Topic sidebar widget

--- protected/humhub/modules/topic/config.php
+++ protected/humhub/modules/topic/config.php
@@ -8,10 +8,12 @@
  */
 
 use humhub\modules\content\widgets\WallEntryControls;
+use humhub\modules\dashboard\widgets\Sidebar as DashboardSidebar;
 use humhub\modules\topic\Module;
 use humhub\modules\user\widgets\AccountSettingsMenu;
 use humhub\modules\topic\Events;
 use humhub\modules\space\modules\manage\widgets\DefaultMenu;
+use humhub\modules\space\widgets\Sidebar as SpaceSidebar;
 
 return [
     'id' => 'topic',
@@ -21,5 +23,7 @@ return [
         ['class' => WallEntryControls::class, 'event' => WallEntryControls::EVENT_INIT, 'callback' => [Events::class, 'onWallEntryControlsInit']],
         ['class' => DefaultMenu::class, 'event' => DefaultMenu::EVENT_INIT, 'callback' => [Events::class, 'onSpaceSettingMenuInit']],
         ['class' => AccountSettingsMenu::class, 'event' => AccountSettingsMenu::EVENT_INIT, 'callback' => [Events::class, 'onProfileSettingMenuInit']],
+        ['class' => DashboardSidebar::class, 'event' => DashboardSidebar::EVENT_INIT, 'callback' => [Events::class, 'onDashboardSidebarInit']],
+        ['class' => SpaceSidebar::class, 'event' => SpaceSidebar::EVENT_INIT, 'callback' => [Events::class, 'onSpaceSidebarInit']],
     ],
 ];
