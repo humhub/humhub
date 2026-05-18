7feb7cea9ff7dda40c82192841efb7e983a0206d
Marc Farré <contact@marc.fun>
2026-05-18 16:05:01 +0100

    Display media types in separated rows

--- protected/humhub/modules/file/widgets/views/showFiles.php
+++ protected/humhub/modules/file/widgets/views/showFiles.php
@@ -29,79 +29,92 @@ foreach ($files as $file) {
     }
 }
 
-$nbFiles = count($files);
 $isFluid = ThemeHelper::isFluid();
 
-// Image height is defined in file.less and matches this template
-$bsColumns = 6;
-$bsColumnsMd = $isFluid ? 4 : 6;
-$bsColumnsLg = $isFluid ? 3 : 4;
-if ($nbFiles === 1) {
-    $bsColumns = 12;
-    $bsColumnsMd = $isFluid ? 6 : 12;
-    $bsColumnsLg = $isFluid ? 4 : 6;
-}
-if ($nbFiles === 2) {
-    $bsColumnsMd = 6;
-    $bsColumnsLg = $isFluid ? 4 : 6;
-}
-if ($videos) {
-    // When media contains videos, make the previews bigger
-    $bsColumnsLg = $isFluid ? 4 : 6;
+$getColumnClass = static function (int $nbFiles, bool $enlarge = false) use ($isFluid): string {
+    // Image and Video heights are defined in file.scss and matches this template
+    $bsColumns = 6;
+    $bsColumnsMd = $isFluid ? 4 : 6;
+    $bsColumnsLg = $isFluid ? 3 : 4;
     if ($nbFiles === 1) {
-        $bsColumnsLg = 12;
+        $bsColumns = 12;
+        $bsColumnsMd = $isFluid ? 6 : 12;
+        $bsColumnsLg = $isFluid ? 4 : 6;
     }
-}
-$fullWidthColumnClass = 'col-media col-12';
-$galleryColumnClass = 'col-media col-' . $bsColumns . ' col-lg-' . $bsColumnsMd . ' col-xl-' . $bsColumnsLg;
+    if ($nbFiles === 2) {
+        $bsColumnsMd = 6;
+        $bsColumnsLg = $isFluid ? 4 : 6;
+    }
+
+    if ($enlarge) {
+        $bsColumnsLg = $isFluid ? 4 : 6;
+        if ($nbFiles === 1) {
+            $bsColumnsLg = 12;
+        }
+    }
+
+    return 'col-media col-' . $bsColumns . ' col-lg-' . $bsColumnsMd . ' col-xl-' . $bsColumnsLg;
+};
+
+
 
 $videoTypes = [
     'webm' => 'video/webm',
-    'mp4'  => 'video/mp4',
-    'ogv'  => 'video/ogg',
-    'mov'  => 'video/quicktime',
+    'mp4' => 'video/mp4',
+    'ogv' => 'video/ogg',
+    'mov' => 'video/quicktime',
 ];
 ?>
 
-<?php if ($nbFiles > 0): ?>
+<?php if ($files): ?>
     <!-- hideOnEdit mandatory since 1.2 -->
     <div class="hideOnEdit">
         <!-- Show Medias as Thumbnails -->
         <?php if ($showPreview): ?>
-            <div class="post-files d-flex flex-wrap justify-content-center" id="post-files-<?= $object->getUniqueId() ?>">
+            <div class="post-files" id="post-files-<?= $object->getUniqueId() ?>">
                 <?php if (!empty($audios)): ?>
-                    <div class="<?= $fullWidthColumnClass ?>">
-                        <?= JPlayerPlaylistWidget::widget(['playlist' => $audios]) ?>
+                    <div class="post-files-audio d-flex flex-wrap justify-content-center">
+                        <div class="col-media col-12">
+                            <?= JPlayerPlaylistWidget::widget(['playlist' => $audios]) ?>
+                        </div>
                     </div>
                 <?php endif; ?>
 
-                <?php foreach ($videos as $video):
-                    $ext = FileHelper::getExtension($video->file_name);
-                    if (!isset($videoTypes[$ext])) {
-                        continue;
-                    } ?>
-                    <div class="<?= $galleryColumnClass ?>">
-                        <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>"
-                           href="<?= $video->getUrl() ?>#.<?= $ext ?>"
-                           title="<?= Html::encode($video->file_name) ?>"
-                           class="d-flex align-items-center justify-content-center h-100 w-100"
-                        >
-                            <video src="<?= $video->getUrl() ?>#t=0.001"
-                                   type="<?= $videoTypes[$ext] ?>"
-                                   controls preload="metadata" height="130"></video>
-                        </a>
+                <?php if ($videos): ?>
+                    <div class="post-files-videos d-flex flex-wrap justify-content-center">
+                        <?php foreach ($videos as $video):
+                            $ext = FileHelper::getExtension($video->file_name);
+                            if (!isset($videoTypes[$ext])) {
+                                continue;
+                            } ?>
+                            <div class="<?= $getColumnClass(count($videos), true) ?>">
+                                <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId() ?>"
+                                   href="<?= $video->getUrl() ?>#.<?= $ext ?>"
+                                   title="<?= Html::encode($video->file_name) ?>"
+                                   class="d-flex align-items-center justify-content-center h-100 w-100"
+                                >
+                                    <video src="<?= $video->getUrl() ?>#t=0.001"
+                                           type="<?= $videoTypes[$ext] ?>"
+                                           controls preload="metadata" height="130"></video>
+                                </a>
+                            </div>
+                        <?php endforeach ?>
                     </div>
-                <?php endforeach ?>
+                <?php endif; ?>
 
-                <?php foreach ($images as $image): ?>
-                    <?php $previewImage->applyFile($image) ?>
-                    <div class="<?= $galleryColumnClass ?>">
-                        <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId(); ?>"
-                           href="<?= $image->getUrl() ?>#.jpeg" title="<?= Html::encode($image->file_name) ?>">
-                            <?= $previewImage->render() ?>
-                        </a>
+                <?php if ($images): ?>
+                    <div class="post-files-images d-flex flex-wrap justify-content-center">
+                        <?php foreach ($images as $image): ?>
+                            <?php $previewImage->applyFile($image) ?>
+                            <div class="<?= $getColumnClass(count($images)) ?>">
+                                <a data-ui-gallery="<?= 'gallery-' . $object->getUniqueId(); ?>"
+                                   href="<?= $image->getUrl() ?>#.jpeg" title="<?= Html::encode($image->file_name) ?>">
+                                    <?= $previewImage->render() ?>
+                                </a>
+                            </div>
+                        <?php endforeach; ?>
                     </div>
-                <?php endforeach; ?>
+                <?php endif; ?>
             </div>
         <?php endif; ?>
 
