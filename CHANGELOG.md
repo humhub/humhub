c0475f7a859f071055fd498a2d54c8840beae68f
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2026-06-02 10:54:56 +0300

    Improve permalink error handling for guest users

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ HumHub Changelog
 - Fix #8189: Scroll to highlighted comment when navigating from Last Activities
 - Enh #8187: Update composer package symfony/mime
 - Fix #8188: AutoContrast and Sign In Footer Links
+- Fix #8195: Improve permalink error handling for guest users
 
 1.18.3 (May 18, 2026)
 ---------------------
--- protected/humhub/modules/content/controllers/PermaController.php
+++ protected/humhub/modules/content/controllers/PermaController.php
@@ -11,8 +11,10 @@ namespace humhub\modules\content\controllers;
 use humhub\components\behaviors\AccessControl;
 use humhub\components\Controller;
 use humhub\modules\content\models\Content;
+use humhub\modules\user\helpers\AuthHelper;
 use Yii;
-use yii\web\HttpException;
+use yii\web\NotFoundHttpException;
+use yii\web\UnauthorizedHttpException;
 
 /**
  * PermaController is used to create permanent links to content.
@@ -45,7 +47,7 @@ class PermaController extends Controller
         $commentId = (int)Yii::$app->request->get('commentId');
 
         $content = Content::findOne(['id' => $id]);
-        if ($content !== null) {
+        if ($content?->canView()) {
             $highlight = Yii::$app->request->get('highlight');
             if ($highlight !== null) {
                 Yii::$app->session->set('contentHighlight', $highlight);
@@ -66,6 +68,14 @@ class PermaController extends Controller
             }
         }
 
-        throw new HttpException(404, Yii::t('ContentModule.base', 'Could not find requested content!'));
+        if (Yii::$app->user->isGuest) {
+            if (AuthHelper::isGuestAccessEnabled()) {
+                throw new UnauthorizedHttpException(Yii::t('ContentModule.base', 'You need to login to view this content!'));
+            } else {
+                return Yii::$app->user->loginRequired();
+            }
+        }
+
+        throw new NotFoundHttpException(Yii::t('ContentModule.base', 'Could not find requested content!'));
     }
 }
