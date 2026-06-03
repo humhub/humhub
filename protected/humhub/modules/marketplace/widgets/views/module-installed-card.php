9acfe5bfee23911fb417d12766d1aa6b6c1c1de5
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2026-06-03 11:22:35 +0300

    WCAG A: Marketplace browse page
    # Conflicts:
    #	protected/humhub/modules/marketplace/views/purchase/list.php
    #	protected/humhub/modules/user/widgets/views/userPicker.php

--- protected/humhub/modules/marketplace/widgets/views/module-installed-card.php
+++ protected/humhub/modules/marketplace/widgets/views/module-installed-card.php
@@ -10,26 +10,19 @@ use humhub\modules\marketplace\models\Module;
 use humhub\modules\marketplace\widgets\ModuleControls;
 use humhub\modules\marketplace\widgets\ModuleInstalledActionButtons;
 use humhub\modules\marketplace\widgets\ModuleStatus;
-use humhub\modules\ui\icon\widgets\Icon;
 
 /* @var $module Module */
 ?>
 <div class="card-panel">
     <?= ModuleStatus::widget(['module' => $module]) ?>
     <div class="card-header">
-        <?= $module->marketplaceLink(Html::img($module->getImage(), [
-            'class' => 'rounded',
-            'data-src' => 'holder.js/94x94',
-            'alt' => '94x94',
-            'style' => 'width:94px;height:94px',
-        ])) ?>
+        <?= $module->marketplaceImage() ?>
         <?= ModuleControls::widget(['module' => $module]) ?>
     </div>
     <div class="card-body">
-        <div
-            class="card-title"><?= $module->marketplaceLink($module->name) . ($module->featured ? ' ' . Icon::get('star')->color('accent') : '') ?></div>
+        <div class="card-title"><?= $module->marketplaceName() ?></div>
         <div><?= $module->getInstalledVersion() ?></div>
-        <div><?= $module->marketplaceLink($module->description) ?></div>
+        <div><?= Html::encode($module->description) ?></div>
     </div>
     <?= ModuleInstalledActionButtons::widget(['module' => $module]) ?>
 </div>
