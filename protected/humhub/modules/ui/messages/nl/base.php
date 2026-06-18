d1a821be36aee81db0c6976fafae6af372bade02
HumHub Translations <hello@humhub.com>
2026-06-18 03:00:03 +0000

    Enh: Updated Translations (translate.humhub.org)

--- protected/humhub/modules/ui/messages/nl/base.php
+++ protected/humhub/modules/ui/messages/nl/base.php
@@ -1,10 +1,9 @@
 <?php
-
 return [
-    'Collapse' => 'Samenvouwen',
-    'Read more' => 'Lees verder',
-    'Reset filters' => 'Filters resetten',
-    'Select' => 'Kies',
-    'User Interface' => 'Gebruikersomgeving',
-    'Toggle filters' => '',
+  'Collapse' => 'Samenvouwen',
+  'Read more' => 'Lees verder',
+  'Reset filters' => 'Filters resetten',
+  'Select' => 'Kies',
+  'Toggle filters' => 'Filters in-/uitschakelen',
+  'User Interface' => 'Gebruikersomgeving',
 ];
