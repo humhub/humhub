humhub/humhub

commit 77b183538fdf3c86c1ee07e32d862cd22fcf95c0

Ref: Activities (#7933)
* First Prototype

* Refactor Mailing

* Improve migrations

* Removed Stream usages, Unit Tests

* Disabled ActvityLinkCest

* Fixed Merged Syntax Error

* Test Fixes

* Fixed Removed Controller

* Fixed Tests, Created ActivityManager

* Added ActivityBox Controller

* Activity getUrl

* Added "lastActivityId" to ActivityBoxController

* Added Grouping prototype

* Grouping Improvements

* Fix ActivityManager call

* Fix tests

* Improved grouping

* Fix Tests

* Activity Refactoring - Add endless scroll (#7965)

* Switched GroupingKey to Integer

* Remove Caching from Render Controller

* Rename Content placeholders

* Disable Grouping Tests

---------

Co-authored-by: Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>

protected/humhub/modules/activity/components/BaseActivity.php
@@ -1,193 +1,127 @@
 <?php
 
-/**
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
- */
-
 namespace humhub\modules\activity\components;
 
-use humhub\modules\content\components\ContentContainerActiveRecord;
-use yii\base\InvalidConfigException;
-use yii\base\Exception;
-use yii\db\ActiveRecord;
-use humhub\components\SocialActivity;
+use humhub\helpers\Html;
 use humhub\modules\activity\models\Activity;
-use humhub\modules\content\components\ContentActiveRecord;
-use humhub\modules\content\components\ContentAddonActiveRecord;
-use humhub\modules\content\models\Content;
+use humhub\modules\activity\models\Activity as ActivityRecord;
+use humhub\modules\activity\services\GroupingService;
+use humhub\modules\content\models\ContentContainer;
+use humhub\modules\user\models\User;
+use Yii;
+use yii\base\BaseObject;
 
 /**
- * BaseActivity is the base class for all activities.
- *
- * @property Activity $record
- * @author luke
+ * @property-read User[] $groupedUsers
  */
-abstract class BaseActivity extends SocialActivity
+abstract class BaseActivity extends BaseObject
 {
-    /**
-     * Default content visibility of this Activity.
-     * @var int
-     */
-    public $visibility = Content::VISIBILITY_PRIVATE;
+    public readonly ActivityRecord $record;
 
-    /**
-     * @inheritdoc
-     */
-    public $recordClass = Activity::class;
+    public readonly ContentContainer $contentContainer;
 
-    /**
-     * @var bool
-     */
-    public $clickable = true;
+    public readonly User $user;
 
-    /**
-     * @inheritdoc
-     */
-    public function init()
+    public readonly string $createdAt;
+    public readonly int $groupCount;
+    public ?int $groupingThreshold = null;
+    public int $groupingTimeBucketSeconds = 900;
+
+    protected GroupingService $groupingService;
+
+    public function __construct(ActivityRecord $record, $config = [])
     {
-        if ($this->viewName == '') {
-            throw new InvalidConfigException('Missing viewName!');
-        }
+        parent::__construct($config);
 
-        parent::init();
+        $this->contentContainer = $record->contentContainer;
+        $this->user = $record->createdBy;
+        $this->createdAt = $record->created_at;
+        $this->record = $record;
+        $this->groupCount = $this->record->group_count ?? 1;
+        $this->groupingService = new GroupingService($this);
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function getViewParams($params = [])
-    {
-        $params['clickable'] = $this->clickable;
+    abstract protected function getMessage(array $params): string;
 
-        return parent::getViewParams($params);
+    final public function asText(): string
+    {
+        return $this->getMessage($this->getMessageParamsText());
     }
 
-    /**
-     * Creates an activity model and determines the contentContainer/visibility
-     *
-     * @return static
-     * @throws Exception
-     */
-    public function create()
+    final public function asHtml(): string
     {
-        if (empty($this->moduleId)) {
-            throw new InvalidConfigException('No moduleId given!');
-        }
-
-        if (!$this->source instanceof ActiveRecord) {
-            throw new InvalidConfigException('Invalid source object given!');
-        }
-
-        $this->saveModelInstance();
-
-        return $this;
+        return $this->getMessage($this->getMessageParamsHtml());
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function from($originator)
+    final public function asHtmlMail(): string
     {
-        parent::from($originator);
-        $this->record->content->created_by = $originator->id;
-
-        return $this;
+        return $this->getMessage($this->getMessageParamsHtmlMail());
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function about($source)
+    public function getUrl(bool $scheme = true): ?string
     {
-        parent::about($source);
-        $this->record->content->visibility = $this->getContentVisibility();
-        if (!$this->record->content->container && $this->getContentContainer()) {
-            $this->container($this->getContentContainer());
-        }
-
-        return $this;
+        return $this->contentContainer->polymorphicRelation->getUrl($scheme);
     }
 
-    /**
-     * Builder function for setting ContentContainerActiveRecord
-     *
-     * @param ContentContainerActiveRecord $container
-     * @return BaseActivity
-     */
-    public function container($container)
+    protected function getMessageParamsText(): array
     {
-        $this->record->content->container = $container;
-        return $this;
+        return [
+            'displayName' => $this->user->displayName,
+            'displayNames' => $this->formatDisplayNames(fn($dn) => $dn),
+            'groupCount' => $this->groupCount,
+        ];
     }
 
-    /**
-     * Saves the underlying Activity model record.
-     *
-     * @throws InvalidConfigException
-     * @throws Exception
-     */
-    private function saveModelInstance()
+    protected function getMessageParamsHtml(): array
     {
-        $this->record->setPolymorphicRelation($this->source);
-        $this->record->content->visibility = $this->getContentVisibility();
-
-        if (!$this->record->content->container && $this->getContentContainer()) {
-            $this->record->content->container = $this->getContentContainer();
-        }
-
-        $this->record->content->created_by = $this->getOriginatorId();
-
-        if ($this->record->content->created_by == null) {
-            throw new InvalidConfigException('Could not determine originator for activity!');
-        }
-
-        if (!$this->record->save()) {
-            throw new Exception('Could not save activity!' . $this->record->getErrors());
-        }
+        return array_merge($this->getMessageParamsText(), [
+            'displayName' => Html::strong(Html::encode($this->user->displayName)),
+            'displayNames' => $this->formatDisplayNames(fn($dn) => Html::strong(Html::encode($dn))),
+        ]);
     }
 
-    /**
-     * Stores the activity in database
-     *
-     * @return bool
-     */
-    public function save()
+    protected function getMessageParamsHtmlMail(): array
     {
-        return $this->record->save();
+        return array_merge($this->getMessageParamsHtml(), [
+            'displayName' => Html::strong(Html::encode($this->user->displayName)),
+            'displayNames' => $this->formatDisplayNames(fn($dn) => Html::strong(Html::encode($dn))),
+        ]);
     }
 
-    /**
-     * Returns the visibility of the content
-     *
-     * @return int the visibility
-     */
-    protected function getContentVisibility()
+    protected function formatDisplayNames(callable $formatter): string
     {
-        return $this->hasContent() ? $this->getContent()->visibility : $this->visibility;
+        $groupedUsers = $this->groupingService->getGroupedUsers();
+
+        if (count($groupedUsers) === 2) {
+            return Yii::t(
+                'ActivityModule.base',
+                '{displayName1} and {displayName2}',
+                [
+                    'displayName1' => $formatter($groupedUsers[0]->displayName),
+                    'displayName2' => $formatter($groupedUsers[1]->displayName),
+                ],
+            );
+        } elseif (count($groupedUsers) > 2) {
+            return Yii::t(
+                'ActivityModule.base',
+                '{displayName1}, {displayName2} and {count} more',
+                [
+                    'displayName1' => $formatter($groupedUsers[0]->displayName),
+                    'displayName2' => $formatter($groupedUsers[2]->displayName),
+                    'count' => count($groupedUsers) - 2,
+                ],
+            );
+        }
+
+        return '';
     }
 
-    /**
-     * Returns the user id of the originator of this activity
-     *
-     * @return int user id
-     */
-    protected function getOriginatorId()
+    public function findGroupedQuery(): ActiveQueryActivity
     {
-        if ($this->originator !== null) {
-            return $this->originator->id;
-        }
-
-        if ($this->source instanceof ContentActiveRecord) {
-            return $this->source->content->created_by;
-        }
-
-        if ($this->source instanceof ContentAddonActiveRecord) {
-            return $this->source->created_by;
-        }
-
-        return null;
+        return Activity::find()
+            ->timeBucket($this->groupingTimeBucketSeconds, $this->createdAt)
+            ->andWhere(['activity.class' => static::class])
+            ->andWhere(['activity.contentcontainer_id' => $this->contentContainer->id])
+            ->andWhere(['activity.created_by' => $this->user->id]);
     }
-
 }

protected/humhub/modules/activity/models/Activity.php
@@ -1,133 +1,65 @@
 <?php
 
-/**
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
- */
-
 namespace humhub\modules\activity\models;
 
-use humhub\modules\activity\components\BaseActivity;
-use Yii;
-use yii\base\Exception;
-use yii\base\InvalidConfigException;
-use yii\db\ActiveRecord;
-use humhub\modules\content\components\ContentActiveRecord;
-use humhub\components\behaviors\PolymorphicRelation;
-use yii\db\IntegrityException;
-use humhub\modules\activity\widgets\Activity as ActivityStreamEntryWidget;
+use humhub\modules\activity\components\ActiveQueryActivity;
+use humhub\modules\activity\services\ActivityManager;
+use humhub\modules\activity\services\GroupingService;
+use humhub\modules\content\models\Content;
+use humhub\modules\content\models\ContentContainer;
+use yii\db\ActiveQuery;
 
 /**
  * This is the model class for table "activity".
  *
  * @property int $id
  * @property string $class
- * @property string $module
- * @property string $object_model
- * @property int $object_id
+ * @property int $contentcontainer_id
+ * @property int $content_id
+ * @property int $content_addon_record_id
+ * @property string $grouping_key
+ * @property int $created_by
+ * @property string $created_at
  *
- * @mixin PolymorphicRelation
+ * @property-read int $group_count
+ * @property-read Content $content
+ * @property-read ContentContainer $contentContainer
  */
-class Activity extends ContentActiveRecord
+class Activity extends \humhub\components\ActiveRecord
 {
-    /**
-     * @inheritdoc
-     */
-    public $wallEntryClass = ActivityStreamEntryWidget::class;
+    public ?int $group_count = null;
 
-    /**
-     * @inheritdoc
-     */
-    public $autoFollow = false;
-
-    /**
-     * @inheritdoc
-     */
-    protected $streamChannel = 'activity';
-
-    /**
-     * @inheritdoc
-     */
-    public $silentContentCreation = true;
-
-    /**
-     * @inheritdoc
-     */
-    public function behaviors()
-    {
-        return [
-            [
-                'class' => PolymorphicRelation::class,
-                'strict' => true,
-                'mustBeInstanceOf' => [
-                    ActiveRecord::class,
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
     public static function tableName()
     {
         return 'activity';
     }
 
-    /**
-     * @inheritdoc
-     */
-    public function rules()
+    public function afterSave($insert, $changedAttributes)
     {
-        return [
-            [['object_id'], 'integer'],
-            [['class'], 'string', 'max' => 100],
-            [['module', 'object_model'], 'string', 'max' => 100],
-        ];
-    }
-
-    /**
-     * Returns the related BaseActivity object of this Activity record.
-     *
-     * @return BaseActivity
-     * @throws Exception
-     * @throws InvalidConfigException
-     * @throws IntegrityException
-     */
-    public function getActivityBaseClass()
-    {
-        if (!class_exists($this->class)) {
-            throw new Exception('Could not find BaseActivity ' . $this->class . ' for Activity Record.');
+        if ($insert) {
+            $this->updateAttributes(['grouping_key' => $this->id]);
         }
-
-        $result = Yii::createObject([
-            'class' => $this->class,
-            'originator' => $this->content->createdBy,
-            'source' => $this->getSource(),
-        ]);
-        $result->record = $this; // If we include the record in createObject, it somehow loses activerecord data (id etc...)
-        return $result;
+        parent::afterSave($insert, $changedAttributes);
     }
 
-    /**
-     * Returns the source object which belongs to this Activity.
-     *
-     * @return mixed
-     * @throws IntegrityException
-     * @see BaseActivity
-     */
-    public function getSource()
+    public function beforeDelete()
     {
-        return $this->getPolymorphicRelation();
+        (new GroupingService(ActivityManager::load($this)))->beforeDelete();
+        return parent::beforeDelete();
     }
 
-    /**
-     * @return bool|int
-     */
-    public function delete()
+    public function getContent(): ActiveQuery
     {
-        // Always hard delete activities
-        return $this->hardDelete();
+        return $this->hasOne(Content::class, ['id' => 'content_id']);
+    }
+
+    public function getContentContainer(): ActiveQuery
+    {
+        return $this->hasOne(ContentContainer::class, ['id' => 'contentcontainer_id']);
+    }
+
+    public static function find(): ActiveQueryActivity
+    {
+        return new ActiveQueryActivity(static::class);
     }
 }

protected/humhub/modules/activity/views/layouts/mail.php
@@ -1,26 +1,17 @@
 <?php
 
-/**
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
- */
-
-use humhub\components\View;
 use humhub\helpers\Html;
-use humhub\modules\content\components\ContentContainerActiveRecord;
 use humhub\modules\space\models\Space;
 use humhub\helpers\MailStyleHelper;
-use humhub\modules\user\models\User;
 
-/* @var $this View */
-/* @var $space Space */
+/* @var $user \humhub\modules\user\models\User */
 /* @var $url string */
-/* @var $contentContainer ContentContainerActiveRecord */
-/* @var $html string */
-/* @var $text string */
-/* @var $originator User */
-/* @var $content string */
+/* @var $content \humhub\modules\content\models\Content */
+/* @var $contentAddon \humhub\modules\content\interfaces\ContentProvider */
+/* @var $contentContainer \humhub\modules\content\models\ContentContainer */
+/* @var $createdAt string */
+/* @var $message string */
+
 ?>
 
 <!-- START NOTIFICATION/ACTIVITY -->
@@ -60,9 +51,9 @@ use humhub\modules\user\models\User;
 
                                                     <td valign="top" align="left" style="padding-right:20px">
                                                         <!-- START: USER IMAGE -->
-                                                        <a href="<?= $originator->createUrl('/user/profile', [], true) ?>">
+                                                        <a href="<?= $user->createUrl('/user/profile', [], true) ?>">
                                                             <img
-                                                                src="<?= $originator->getProfileImage()->getUrl('', true) ?>"
+                                                                src="<?= $user->getProfileImage()->getUrl('', true) ?>"
                                                                 width="50"
                                                                 alt=""
                                                                 style="max-width:50px; display:block !important; border-radius: 4px;"
@@ -83,13 +74,13 @@ use humhub\modules\user\models\User;
                                                                     <div
                                                                         style="width:480px;overflow:hidden;text-overflow:ellipsis;font-size: 13px; line-height: 22px; font-family: <?= MailStyleHelper::getFontFamily() ?>; color:<?= MailStyleHelper::getTextColorMain() ?>; font-weight:300; text-align:left">
                                                                         <!-- content output-->
-                                                                        <?= $content ?>
+                                                                        <?= $message ?>
 
                                                                         <!-- check if activity object has a space -->
-                                                                        <?php if ($space !== null): ?>
+                                                                        <?php if ($contentContainer->polymorphicRelation instanceof Space): ?>
                                                                             <?= Html::a(
-                                                                                $space->displayName,
-                                                                                $space->createUrl(null, [], true),
+                                                                                $contentContainer->polymorphicRelation->displayName,
+                                                                                $contentContainer->polymorphicRelation->createUrl(null, [], true),
                                                                                 [
                                                                                     'style' => 'text-decoration: none; color: ' . MailStyleHelper::getTextColorMain(),
                                                                                 ],

protected/humhub/modules/activity/views/layouts/mail_plaintext.php
@@ -1,32 +1,25 @@
 <?php
 
-/**
- * @link https://www.humhub.org/
- * @copyright Copyright (c) 2017 HumHub GmbH & Co. KG
- * @license https://www.humhub.com/licences
- */
-
-/* @var $this View */
-/* @var $space Space */
+/* @var $user \humhub\modules\user\models\User */
 /* @var $url string */
-/* @var $contentContainer ContentContainerActiveRecord */
-/* @var $html string */
-/* @var $text string */
+/* @var $content \humhub\modules\content\models\Content */
+/* @var $contentAddon \humhub\modules\content\interfaces\ContentProvider */
+/* @var $contentContainer \humhub\modules\content\models\ContentContainer */
+/* @var $createdAt string */
 
-/* @var $originator User */
+/* @var $message string */
 
-use humhub\modules\content\components\ContentContainerActiveRecord;
-use yii\web\View;
+use humhub\modules\space\models\Space;
 
 ?>
 
 ---
 
-<?= $content ?>
-<?php if (!empty($space)) : ?>
-    (<?= Yii::t('ActivityModule.base', 'via') ?> <?= $space->displayName ?>)
+<?= $message ?>
+<?php if ($contentContainer->polymorphicRelation instanceof Space) : ?>
+    (<?= Yii::t('ActivityModule.base', 'via') ?> <?= $contentContainer->polymorphicRelation->displayName ?>)
 <?php endif; ?>
 
 <?php if ($url != '') : ?>
-    <?= Yii::t('ActivityModule.base', 'See online:') ?> <?= urldecode((string) $url) ?>
+    <?= Yii::t('ActivityModule.base', 'See online:') ?> <?= urldecode($url) ?>
 <?php endif; ?>

commit 2843835d0aaa7cb2952e56f3a53baabb21eaa11f

Fix TypeError for activities on global content
BaseActivity::$contentContainer was typed non-nullable, but content
without a container (global content) has no contentcontainer_id, so
e.g. commenting on global content crashed in NewCommentActivity.
Make the property nullable to match the nullable DB column and handle
the missing container in getUrl(), the grouping query and the
activity layouts.

Regression from #7933.

protected/humhub/modules/activity/components/BaseActivity.php
@@ -16,7 +16,7 @@ use yii\base\BaseObject;
 abstract class BaseActivity extends BaseObject
 {
     public readonly ActivityRecord $record;
-    public readonly ContentContainer $contentContainer;
+    public readonly ?ContentContainer $contentContainer;
     public readonly User $user;
     public readonly string $createdAt;
     public readonly int $groupCount;
@@ -57,7 +57,7 @@ abstract class BaseActivity extends BaseObject
 
     public function getUrl(bool $scheme = true): ?string
     {
-        return $this->contentContainer->polymorphicRelation->getUrl($scheme);
+        return $this->contentContainer?->polymorphicRelation?->getUrl($scheme);
     }
 
     protected function getMessageParamsMailText(): array

protected/humhub/modules/activity/models/Activity.php
@@ -14,15 +14,15 @@ use yii\db\ActiveQuery;
  *
  * @property int $id
  * @property string $class
- * @property int $contentcontainer_id
+ * @property int|null $contentcontainer_id
  * @property int $content_id
  * @property int $content_addon_record_id
  * @property string $grouping_key
  * @property int $created_by
  * @property string $created_at
  *
- * @property-read Content $content
- * @property-read ContentContainer $contentContainer
+ * @property-read Content|null $content
+ * @property-read ContentContainer|null $contentContainer
  */
 class Activity extends \humhub\components\ActiveRecord
 {

protected/humhub/modules/activity/views/layouts/mail.php
@@ -77,7 +77,7 @@ use humhub\helpers\MailStyleHelper;
                                                                         <?= $message ?>
 
                                                                         <!-- check if activity object has a space -->
-                                                                        <?php if ($contentContainer->polymorphicRelation instanceof Space): ?>
+                                                                        <?php if ($contentContainer?->polymorphicRelation instanceof Space): ?>
                                                                             <?= Html::a(
                                                                                 $contentContainer->polymorphicRelation->displayName,
                                                                                 $contentContainer->polymorphicRelation->createUrl(null, [], true),

protected/humhub/modules/activity/views/layouts/mail_plaintext.php
@@ -16,7 +16,7 @@ use humhub\modules\space\models\Space;
 ---
 
 <?= $message ?>
-<?php if ($contentContainer->polymorphicRelation instanceof Space) : ?>
+<?php if ($contentContainer?->polymorphicRelation instanceof Space) : ?>
     (<?= Yii::t('ActivityModule.base', 'via') ?> <?= $contentContainer->polymorphicRelation->displayName ?>)
 <?php endif; ?>
 

protected/humhub/modules/activity/views/layouts/web.php
@@ -25,7 +25,7 @@ use yii\helpers\Url;
             <?= $user->getProfileImage()->render(32, ['link' => false]) ?>
 
             <!-- Show space image, if you are outside from a space -->
-            <?php if (!Yii::$app->controller instanceof ContentContainerController && $contentContainer->polymorphicRelation instanceof Space) : ?>
+            <?php if (!Yii::$app->controller instanceof ContentContainerController && $contentContainer?->polymorphicRelation instanceof Space) : ?>
                 <?= Image::widget([
                     'space' => $contentContainer->polymorphicRelation,
                     'width' => 20,

protected/humhub/modules/content/activities/ContentCreatedActivity.php
@@ -68,7 +68,7 @@ final class ContentCreatedActivity extends BaseContentActivity implements Config
     {
         return Activity::find()
             ->andWhere(['activity.class' => self::class])
-            ->andWhere(['activity.contentcontainer_id' => $this->contentContainer->id])
+            ->andWhere(['activity.contentcontainer_id' => $this->contentContainer?->id])
             ->andWhere(['activity.created_by' => $this->user->id])
             ->andWhere(['content.object_model' => $this->record->content->object_model])
             ->andWhere(['content.visibility' => $this->record->content->visibility])

protected/humhub/modules/space/components/BaseSpaceActivity.php
@@ -18,7 +18,7 @@ abstract class BaseSpaceActivity extends BaseActivity
     {
         parent::__construct($record, $config);
 
-        if (!$record->contentContainer->polymorphicRelation instanceof Space) {
+        if (!$record->contentContainer?->polymorphicRelation instanceof Space) {
             throw new InvalidValueException('Space activity content container must implement space');
         }
 

protected/humhub/modules/user/activities/FollowActivity.php
@@ -19,7 +19,7 @@ final class FollowActivity extends BaseActivity implements ConfigurableActivityI
     {
         parent::__construct($record, $config);
 
-        if (!$this->contentContainer->polymorphicRelation instanceof User) {
+        if (!$this->contentContainer?->polymorphicRelation instanceof User) {
             throw new InvalidValueException('Invalid user record!');
         }
 

protected/humhub/modules/activity/tests/codeception/unit/ActivityTest.php
@@ -5,7 +5,10 @@ namespace humhub\modules\activity\tests\codeception\unit;
 use Codeception\Specify;
 use humhub\modules\activity\models\Activity;
 use humhub\modules\activity\services\ActivityManager;
+use humhub\modules\activity\services\RenderService;
 use humhub\modules\activity\tests\codeception\activities\TestActivity;
+use humhub\modules\comment\activities\NewCommentActivity;
+use humhub\modules\comment\models\Comment;
 use humhub\modules\post\models\Post;
 use tests\codeception\_support\HumHubDbTestCase;
 use Yii;
@@ -30,6 +33,34 @@ class ActivityTest extends HumHubDbTestCase
         $this->assertEquals(TestActivity::class, $testActivity::class);
         $this->assertEquals($record->content->polymorphicRelation->id, $post->id);
         $this->assertEquals($record->contentcontainer_id, $post->content->contentcontainer_id);
+    }
+
+    public function testCreateActivityOnGlobalContent()
+    {
+        $this->becomeUser('User2');
+
+        $post = new Post(['message' => 'Global content']);
+        $this->assertTrue($post->save());
+        $this->assertNull($post->content->contentcontainer_id);
+
+        // Commenting on global content dispatches NewCommentActivity without a container
+        $comment = new Comment([
+            'message' => 'Comment on global content',
+            'content_id' => $post->content->id,
+        ]);
+        $this->assertTrue($comment->save());
+
+        $record = Activity::findOne(['class' => NewCommentActivity::class]);
+        $this->assertNotNull($record, 'Activity record persisted');
+        $this->assertNull($record->contentcontainer_id);
+
+        $activity = ActivityManager::load($record);
+        $this->assertNull($activity->contentContainer);
+
+        $renderService = new RenderService($record);
+        $this->assertNotEmpty($renderService->getWeb());
+        $this->assertNotEmpty($renderService->getMailText());
+        $this->assertNotEmpty($renderService->getMailHtml());
     }
 
     public function testDeleteRecord()