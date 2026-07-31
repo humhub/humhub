b545655847c875cb2842ccd464a4f7146bc5ffc3
Yuriy Bakhtin <yurabakhtin@users.noreply.github.com>
2026-07-31 17:23:50 +0300

    Fix the upload button hiding on create a comment

--- protected/humhub/modules/file/widgets/views/uploadButton.php
+++ protected/humhub/modules/file/widgets/views/uploadButton.php
@@ -1,11 +1,13 @@
 <?php
 
 use humhub\helpers\Html;
-use humhub\modules\ui\icon\widgets\Icon;
 
 /* @var $label string */
 /* @var $input string */
-/* @var $options array */
+
 ?>
-<?= Html::tag('button', Icon::get('cloud-upload') . ' ' . Html::encode($label), $options) ?>
+
+<?= Html::beginTag('span', $options) ?>
+<i class="fa fa-cloud-upload" aria-hidden="true"></i> <?= $label ?>
 <?= $input ?>
+<?= Html::endTag('span') ?>
